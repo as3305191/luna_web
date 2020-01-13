cc09470b19731f28aa83b9917fa41ab073b67d9c
jamie <jamie91200@gmail.com>
2020-01-13 18:37:20 +0800

    update find_last_record

--- application/controllers/api/Record.php
+++ application/controllers/api/Record.php
@@ -117,6 +117,7 @@ class Record extends MY_Base_Controller {
 				$muscle =  $m->weight/1000 * $m->muscle_rate/100;
 				$bone_mass =  $m->weight/1000 * $m->bone_mass_rate/100;
 				$skeletal_muscle =  $m->weight/1000 * $m->skeletal_muscle_rate/100;
+				$fat_best =  $m->weight/1000 * $m->body_fat_best/100;
 
 				// $m->weight =sprintf("%.2f",$weight_kg);
 				$m -> weight = number_format($weight_kg,1);
@@ -127,6 +128,7 @@ class Record extends MY_Base_Controller {
 				$m -> muscle = number_format($muscle,1);
 				$m -> skeletal_muscle = number_format($skeletal_muscle,1);
 				$m -> bone_mass = number_format($bone_mass,1);
+				$m -> fat_best = number_format($fat_best,1);
 
 				$res['record'] = $m;
 
