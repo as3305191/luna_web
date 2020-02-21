5c8189a026dfabf3813052275df29d4c8124aa0c
steve yeh <aapplle123@gmail.com>
2020-02-21 15:38:51 +0800

    20200221153851

--- application/controllers/api/Record.php
+++ application/controllers/api/Record.php
@@ -450,7 +450,7 @@ class Record extends MY_Base_Controller {
 		}
 		$lowWater = $rec -> weight / 1000 * $water_0;
 		$hightWater = $rec -> weight / 1000 * $water_1;
-
+		$w_weight = $rec -> weight / 1000 * $wrate; 
 		$td -> water -> title = $msg["tip.water"];
 		$td -> water -> value = number_format($wrate, 2) . " kg";
 		$td -> water -> explain = $msg["tip.waterv"];
