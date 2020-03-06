86e1f1db0f9ea5ef43630c8339339a7fba987b71
jamie <jamie91200@gmail.com>
2020-03-06 17:48:09 +0800

    uupdate find_all_by_ym

--- application/models/Records_dao.php
+++ application/models/Records_dao.php
@@ -178,9 +178,9 @@ class Records_dao extends MY_Model {
 
 		$this -> db -> where("( _m.create_date like '{$ym}-%' )");
 		$this -> db -> where("_m.member_id", $member_id);
-		$this -> db -> where("_m.pos", 1);
+		// $this -> db -> where("_m.pos", 1);
 		$this -> db -> where('_m.is_delete', 0);
-
+		$this -> db -> group_by('_m.create_date');
 
 		$list = $this -> db -> get() -> result();
 		return $list;
