bffed59179813ff89ed25240d180f3eec8bcfb67
jamie <jamie91200@gmail.com>
2020-03-12 12:18:11 +0800

    create Verify

---
+++ application/controllers/api/Verify.php
@@ -0,0 +1,32 @@
+<?php
+class Verify extends MY_Base_Controller {
+
+	function __construct() {
+		parent::__construct();
+
+
+	}
+
+	function index() {
+		echo "index";
+	}
+
+	public function check_ver(){
+		$url = 'http://itunes.apple.com/lookup?bundleId=com.jie.heng.HelloThev';
+		$urls = $url.'&t='.mt_rand();
+		$string = file_get_contents($urls);
+
+		$a = strstr($string,'version');
+		$a = strstr($a,',',true);
+		$a = strstr($a,':');
+		$a = str_replace('"','',$a);
+		$a = str_replace(':','',$a);
+		$data = array();
+		$data['version'] = $a;
+		$this -> to_json($data);
+
+	}
+
+
+}
+?>
