b0eb3582f10beb20e212f2c19139aadbbbea70fc
steve yeh <aapplle123@gmail.com>
2020-03-17 11:55:00 +0800

    push for all

--- application/controllers/api/Push.php
+++ application/controllers/api/Push.php
@@ -24,30 +24,46 @@ class Push extends MY_Base_Controller {
 		$date = date('Y-m-d H:i:s');
 		if(!empty($title) && !empty($message) && !empty($target)) {
 
-			$m = $this -> members_dao -> find_by_account($target);
-			// $m = $this -> members_dao -> find_by_value(array('account'=> $target));
-			if(!empty($m)){
-				$res['success'] = TRUE;
-
-				$update_data = array(
-												'title' => $title,
-												'member_id' => $m->id,
-												'content' => $message
-											);
-				$this -> news_private_dao -> insert($update_data);
-
-				$code = $this -> app_reg_code_dao -> find_by_member($m->id);
-				$res['msg_id'] = time();
-				$action = array('1');
-				$token = $code -> token;
-				$title = $title;
-				$msg = $message;
-				$this -> send_gcm_notification($token, $title, $msg , $action);
-			}else{
-				$res['error_code'][] = "account_not_found";
-				$res['error_message'][] = "查無此帳號";
+			if($target == "ALL") {
+				$link = $this -> get_post("link");
+				if(!empty($link)) {
+					$action = array(
+						"link" => $link // link
+					);
+					$title = $title;
+					$msg = $message;
+					$result = $this -> send_gcm_notification("", $title, $msg , $action, "'thev' in topics");
+				} else {
+					$res['error_code'][] = "columns_required";
+					$res['error_message'][] = "缺少必填欄位link";
+				}
+			} else {
+				$m = $this -> members_dao -> find_by_account($target);
+				// $m = $this -> members_dao -> find_by_value(array('account'=> $target));
+				if(!empty($m)){
+					$res['success'] = TRUE;
+	
+					$update_data = array(
+													'title' => $title,
+													'member_id' => $m->id,
+													'content' => $message
+												);
+					$np_id = $this -> news_private_dao -> insert($update_data);
+	
+					$code = $this -> app_reg_code_dao -> find_by_member($m->id);
+					$res['msg_id'] = time();
+					$action = array(
+						"np_id" => $np_id // new privafte id
+					);
+					$token = $code -> token;
+					$title = $title;
+					$msg = $message;
+					$this -> send_gcm_notification($token, $title, $msg , $action);
+				}else{
+					$res['error_code'][] = "account_not_found";
+					$res['error_message'][] = "查無此帳號";
+				}
 			}
-
 		} else {
 			$res['error_code'][] = "columns_required";
 			$res['error_message'][] = "缺少必填欄位";
@@ -56,13 +72,12 @@ class Push extends MY_Base_Controller {
 		$this -> to_json($res);
 	}
 
-	public function send_gcm_notification($token, $title, $msg , $action = array()) {
+	public function send_gcm_notification($token, $title, $msg , $action = array(), $condition = "") {
 		$action_str = implode("#",$action);
 
 		$url = 'https://fcm.googleapis.com/fcm/send';
 
 		$fields = array (
-				'to' => $token,
 				"priority" => "high",
 				'notification' => array(
 						"title" => $title,
@@ -73,6 +88,12 @@ class Push extends MY_Base_Controller {
 					'action' => $action_str
 				)
 		);
+
+		if(!empty($condition)) {
+			$fields['condition'] = $condition;
+		} else {
+			$fields['to'] = $token;
+		}
 		$fields = json_encode($fields);
 	//	echo $fields;
 		$headers = array (
@@ -91,6 +112,8 @@ class Push extends MY_Base_Controller {
 		$result = curl_exec ( $ch );
 
 		curl_close ( $ch );
+
+		return $result;
 	}
 
 
