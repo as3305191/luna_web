0ff3b5219bc9e226d287c4270463d60d25c3bfa4
jamie <jamie91200@gmail.com>
2020-03-30 17:23:39 +0800

    update

--- application/models/Members_dao.php
+++ application/models/Members_dao.php
@@ -53,7 +53,7 @@ class Members_dao extends MY_Model {
 
 	function find_with_token(){
 		$this -> db -> from("$this->table_name as _m");
-		$this -> db -> join("app_reg_code app", "k.member_id = _m.id", "left");
+		$this -> db -> join("app_reg_code app", "app.member_id = _m.id", "left");
 
 		$this -> db -> select('_m.*');
 		$this -> db -> select('app.token');
