8a3a8dc0b6e1febd5dd88d094178e374d5c56646
jamie <jamie91200@gmail.com>
2020-04-16 07:51:49 +0800

    update find_range

--- application/controllers/api/Record.php
+++ application/controllers/api/Record.php
@@ -187,9 +187,12 @@ class Record extends MY_Base_Controller {
 				$data1 = $dataList[0];
 				$data2 = end($dataList);
 
-				$res['max_weight'] = $data2->weight;
-				$res['min_weight'] = $data1->weight;
-				$res['diff_weight'] = $data2->weight-$data1->weight;
+				$max_weight = number_format($data2->weight/1000,2);
+				$min_weight = number_format($data1->weight/1000,2);
+
+				$res['max_weight'] = $max_weight;
+				$res['min_weight'] = $min_weight;
+				$res['diff_weight'] = $max_weight - $min_weight;
 
 				$res['max_bmi'] = $data2->bmi;
 				$res['min_bmi'] = $data1->bmi;
