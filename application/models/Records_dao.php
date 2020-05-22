0cabe26f25a75d60677a110707aaccd781991122
jamie <jamie91200@gmail.com>
2020-05-22 11:49:18 +0800

    update test

--- application/models/Records_dao.php
+++ application/models/Records_dao.php
@@ -302,8 +302,9 @@ class Records_dao extends MY_Model {
 	}
 
 	function find_all_by_ym_update($member_id, $ym) {
-		$sql = "SELECT * FROM records WHERE id IN (SELECT MAX(id) FROM records WHERE member_id = $member_id AND is_delete = 0 GROUP BY create_date) order by id asc";
+		$sql = "SELECT * FROM records WHERE id IN (SELECT MAX(id) FROM records WHERE member_id = $member_id AND is_delete = 0 GROUP BY create_date) order by id desc";
 		$query = $this -> db -> query($sql)-> result();
+		return $query;
 		// return $query;
 		// $this -> db -> from("$this->table_name as _m");
 		//
