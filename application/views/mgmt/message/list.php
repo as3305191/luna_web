40bcbcec966d21ceb327b06a1846a05178560136
江官駿 <as3305191@gmail.com>
2020-08-26 10:18:42 +0800

    update

--- application/views/mgmt/message/list.php
+++ application/views/mgmt/message/list.php
@@ -239,9 +239,9 @@ A={
             var obj=false,c=false;
             if(da.type=='add'){
                 if(da.users[i].me_id>0){
-                        var obj=A.$$('<p me_id="'+da.users[i].me_id+'" onclick="reload_chat_room('+da.users[i].me_id+')">'+da.users[i].name+'</p>');
+                        var obj=A.$$('<p me_id="'+da.me_id+'" onclick="reload_chat_room('+da.me_id+')">'+da.users[i].name+'</p>');
                     } else{
-                        var obj=A.$$('<p me_id="0" onclick="reload_chat_room(0)">'+da.users[i].name+'</p>');
+                        var obj=A.$$('<p me_id="0" onclick="reload_chat_room(0)">'+da.name+'</p>');
                     }
                 lus.appendChild(obj);
                 cuser(obj,da.code);
