d23250080078c6a344b2d12061efdad6cd1e7d92
江官駿 <as3305191@gmail.com>
2020-08-26 10:16:32 +0800

    update

--- application/views/mgmt/message/list.php
+++ application/views/mgmt/message/list.php
@@ -238,7 +238,11 @@ A={
             eval('var da='+msg.data);
             var obj=false,c=false;
             if(da.type=='add'){
-                var obj=A.$$('<p me_id="'+da.users[i].me_id+'" onclick="reload_chat_room('+da.users[i].me_id+')">'+da.name+'</p>');
+                if(da.users[i].me_id>0){
+                        var obj=A.$$('<p me_id="'+da.users[i].me_id+'" onclick="reload_chat_room('+da.users[i].me_id+')">'+da.users[i].name+'</p>');
+                    } else{
+                        var obj=A.$$('<p me_id="0" onclick="reload_chat_room(0)">'+da.users[i].name+'</p>');
+                    }
                 lus.appendChild(obj);
                 cuser(obj,da.code);
                 obj=A.$$('<p><span>['+da.time+']</span>歡迎<a>'+da.name+'</a>加入</p>');
