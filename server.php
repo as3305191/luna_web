76985ab23d09511f8f0a8397290da87c3085b32a
江官駿 <as3305191@gmail.com>
2020-09-09 16:09:35 +0800

    update

--- server.php
+++ server.php
@@ -274,7 +274,7 @@ class Sock{
             $sql2="SELECT now_online FROM `user_online` WHERE id='1'";
 
             $all_online_user=mysqli_query($link,$sql2);
-            foreach($all_online_user as $each=>$each_value){
+            foreach($all_online_user[0] as $each_value){
                 $this->online_user[] = $each_value;
             }
             if($me_id>0){
