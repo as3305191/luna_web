f093ab8ea80ed467582c99b976a5f80e91d3bfe0
江官駿 <as3305191@gmail.com>
2020-09-14 10:57:33 +0800

    update

--- application/views/mgmt/message/list.php
+++ application/views/mgmt/message/list.php
@@ -60,7 +60,11 @@ input{width:103%; height:30px; padding:2px; line-height:20px; outline:none; bord
 </body>
 <?php $this -> load -> view('mgmt/message/message_script'); ?>
 <script>
-    
+if(location.href.indexOf('#reloaded')==-1){
+    location.href=location.href+"#reloaded";
+    location.reload(); 
+}
+
 (function(){
     var key='all',mkey;
     var users={};
@@ -77,8 +81,6 @@ input{width:103%; height:30px; padding:2px; line-height:20px; outline:none; bord
         if(!n){
             return ;   
         }
-       
-      
 
         //創socket，注意URL的格式：ws://ip:端口
         so=new WebSocket(url);
