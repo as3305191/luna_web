adfcab441b877e9b3675de126b89b58f78ee8886
unknown <as3305191@gmail.com>
2020-09-16 14:13:09 +0800

    update

--- application/views/mgmt/old_ktx/list.php
+++ application/views/mgmt/old_ktx/list.php
@@ -1,75 +1,34 @@
-<div class="tab-content">
-	<div class="tab-pane active" id="list_page">
-
-		<!-- widget grid -->
-		<section id="widget-grid" class="">
-
-			<!-- row -->
-			<div class="row">
-
-				<!-- NEW WIDGET START -->
-				<article class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-
-					<!-- Widget ID (each widget will need unique ID)-->
-					<div class="jarviswidget">
-						<!-- <header>
-							<div class="widget-toolbar pull-left">
-								<div class="btn-group">
-									<button onclick="currentApp.doEdit(0)" class="btn dropdown-toggle btn-xs btn-success" data-toggle="dropdown">
-										<i class="fa fa-plus"></i>新增
-									</button>
-								</div>
-							</div>
-						</header> -->
-
-						<!-- widget div-->
-						<div>
-
-							<!-- widget edit box -->
-							<div class="jarviswidget-editbox">
-								<!-- This area used as dropdown edit box -->
-
-							</div>
-							<!-- end widget edit box -->
-
-							<!-- widget content -->
-							<div class="widget-body no-padding">
-
-						 	 <a href="192.168.1.211/login.aspx" class="list-group-item list-group-item-action justify-content-between"> 
-
-
-							</div>
-							<!-- end widget content -->
-
-						</div>
-						<!-- end widget div -->
-
-					</div>
-					<!-- end widget -->
-
-				</article>
-				<!-- WIDGET END -->
-
-			</div>
-
-			<!-- end row -->
-
-		</section>
-		<!-- end widget grid -->
-	</div>
-
-	<!-- <div class="tab-pane animated fadeIn" id="edit_page">
-		<section class="">
-			<div class="row">
-				<article class="col-xs-12 col-sm-12 col-md-12 col-lg-12" id="edit-modal-body">
-
-				</article>
-			</div>
-		</section>
-	</div> -->
+<style>
+/* *{
+	margin:0;
+} */
+html,body{
+	margin:0;
+	padding:0;
+	height:100%;
+}
+body{
+	margin:0;
+	padding:0;
+	height:100%;
+	font:100%;
+}
+</style>
+<div  style="position:relative;width:100%;height:100%;">
+	<iframe id="iframe_ktx" name="iframe_ktx" src="//192.168.1.211/index.aspx" style=" width:100%; height:1000px" frameborder="0" scrolling="auto"></iframe>
 </div>
 
-<?php $this -> load -> view('general/delete_modal'); ?>
+
 <script type="text/javascript">
+var account='<?= $login_user->account?>';
+var password='<?= $login_user->password?>';
+// var iframe_index = ($('#iframe_ktx').contentDocument||$('#iframe_ktx').contentWindow);
+	// $(document).ready(function(){
+		
+	
+	// })
+	// $('#iframe_ktx').contents().find('#TxbAccount').text(account);
+	// iframe_index.contents().find('#TxbPassword').text(account);
+	// iframe_index.getElementById('TxbPassword').value=password;
 
 </script>
