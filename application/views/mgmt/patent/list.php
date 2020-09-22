81c24efdbdbd704e26ca271fb1ea075d92205b48
江官駿 <as3305191@gmail.com>
2020-09-22 09:53:13 +0800

    update

--- application/views/mgmt/patent/list.php
+++ application/views/mgmt/patent/list.php
@@ -1,275 +1,201 @@
-<style>
-.none {
-	display: none;
-}
-.btn_1 {
-    background-color: #FFD22F !important;
-    color: #F57316 !important;
-  }
-
-.mask {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 531px;
-    height: 354px;
-    background: rgba(101, 101, 101, 0.6);
-    color: #ffffff;
-    opacity: 0;
-}
-
-.mask h3 {
-    text-align: center;
-}
-
-.img_div a:hover .mask {
-    opacity: 1;           
-}
-</style>
-
-<?php $this -> load -> view("mgmt/patent/patent_head")  ?>    
-        <div class="tab-content">
+<div class="tab-content">
 	<div class="tab-pane active" id="list_page">
+
 		<!-- widget grid -->
-        <section class="tab-pane padding-10 no-padding-bottom" id="s_template" style="padding: 17px 0px">
-        <header>
-            <div class="widget-toolbar pull-left">
-                <div class="btn-group">
-                    <button onclick="currentApp.doEdit(0)" class="btn dropdown-toggle btn-xs btn-success" data-toggle="dropdown">
-                        <i class="fa fa-plus"></i>新增
-                    </button>
-                </div>
-            </div>
-        </header>
-            <div class="col-sm-12">
-                <div class="form-group">
-                    <fieldset>
-                        <div class="form-group">
-                            <label for="" class="col-md-2 control-label">項目類別</label>
-                            <div class="col-md-3">
-                                <select id="fix_type" required class="form-control change-name">
-                                    <option value="1">電腦</option>
-                                    <option value="2">軟體</option>
-                                    <option value="3">硬體</option>
-                                </select>
-                            </div>
-                            <div class="col-md-3">
-                                <select id="fix_type" required class="form-control change-name">
-                                    <option value="1">電腦</option>
-                                    <option value="2">軟體</option>
-                                    <option value="3">硬體</option>
-                                </select>
-                            </div>
-                            <div class="col-md-3">
-                                <select id="fix_type" required class="form-control change-name">
-                                    <option value="1">電腦</option>
-                                    <option value="2">軟體</option>
-                                    <option value="3">硬體</option>
-                                </select>
-                            </div>
-                        </div>
-                    </fieldset>	
-                </div>
-            </div>
-            <div class="col-sm-12">
-                <fieldset>
-                    <div class="form-group">
-                        <label for="" class="col-md-2 control-label">申請人</label>
-                        <div class=" col-md-3">
-                            <input class="form-control" id="h_s_name" placeholder="請輸入軟硬體名稱">
-                        </div>
-                        <label for="" class="col-md-2  control-label">申請號</label>
-                        <div class=" col-md-3">
-                            <input class="form-control" id="h_s_name" placeholder="請輸入軟硬體名稱">
-                        </div>
-                    </div>
-                   
-                </fieldset>	
-            </div>
-            <div class="col-sm-12">
-                <fieldset>
-                    <div class="form-group">
-                        <label for="" class="col-md-2 control-label">發明人</label>
-                        <div class=" col-md-3">
-                            <input class="form-control" id="h_s_name" placeholder="請輸入軟硬體名稱">
-                        </div>
-                        <label for="" class="col-md-2  control-label">公開號</label>
-                        <div class=" col-md-3">
-                            <input class="form-control" id="h_s_name" placeholder="請輸入軟硬體名稱">
-                        </div>
-                    </div>
-                   
-                </fieldset>	
-            </div>
-            <div class="col-sm-12">
-                <fieldset>
-                    <div class="form-group">
-                        <label for="" class="col-md-2 control-label">關鍵字</label>
-                        <div class=" col-md-3">
-                            <input class="form-control" id="keyword" placeholder="請輸入關鍵字">
-                        </div>
-                        <label for="" class="col-md-2  control-label">專利號</label>
-                        <div class=" col-md-3">
-                            <input class="form-control" id="h_s_name" placeholder="請輸入軟硬體名稱">
-                        </div>
-                    </div>
-                </fieldset>	
-            </div>
-            <div class="col-sm-12">
-                <fieldset>
-                    <div class="form-group">
-                        <label for="" class="col-md-2 control-label">摘要搜尋</label>
-                        <div class=" col-md-3 " >
-                            <input class="form-control" id="h_s_name" placeholder="請輸入軟硬體名稱">
-                        </div>
-                    </div>
-                    <div class="col-sm-6 form-group pull-right">
-                            <input type="radio" id="huey" name="drone" value="1">
-                            <label for="huey">專利已核准</label>
-                            <input type="radio" id="huey" name="drone" value="1">
-                            <label for="huey">專利審查中</label>
-                            <input type="radio" id="huey" name="drone" value="1">
-                            <label for="huey">專利放棄/核駁審定</label>
-                    </div>
-                </fieldset>	
-            </div>
-            <hr>
-            <div class="col-sm-12">
-                <section class="koko-hot-product">
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <h3>搜尋結果:</h3>
-                        </div>
-                    </div>
-                    <div class="owl-carousel owl-theme" id="patent_product">
-                    </div>
-                </section>
-            </div>			
-        </section>
+		<section id="widget-grid" class="">
+
+			<!-- row -->
+			<div class="row">
+
+				<!-- NEW WIDGET START -->
+				<article class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+
+					<!-- Widget ID (each widget will need unique ID)-->
+					<div class="">
+
+
+						<!-- widget div-->
+						<div>
+						    <div class="col-md-12 col-xs-12 col-sm-12" style="padding:10px 20px 10px 12px;">
+						        <div class="col-md-12 col-xs-12 col-sm-12 no-padding" style="margin-bottom: 10px;">
+						            <span style="font-size: 16pt; color:#0d0d56">督導日報管理列表</span>
+												<?php if ($dp -> export_manager_daily_report == 1): ?>
+													<button id="manager_daily_report_export" class="btn" style="float:right;background-color:#FF9030;color:white;">匯出督導日報資料</button>
+												<?php endif; ?>
+
+						        </div>
+						    </div>
+						    <!-- $$$$$$$$$$$$$$$$$ -->
+								<div class="col-md-12 col-xs-12 col-sm-12">
+									<div class="inline" style="padding:0px 12px 0px 0px ;">專案名稱</div>
+									<div class="inline">
+										<input id="project_filter" type="text" class="form-control" value="" placeholder="請輸入名稱"/>
+									</div>
+
+									<!-- <div class="inline">日期區間</div>
+									<div class="inline" style="position:relative">
+										<input class="form-control inline date-input dt_picker" id="start_time_filter" placeholder="YYYY-MM-DD" name="start_time_filter"/>
+										<img src="<?=base_url('img/proj_new/cala.svg')?>" style="height:16px;position:absolute;right:20px;top:9px;z-index:3">
+									</div>
+									<div class="inline">~</div>
+									<div class="inline" style="position:relative">
+										<input class="form-control inline date-input dt_picker" id="end_time_filter" placeholder="YYYY-MM-DD" name="end_time_filter"/>
+										<img src="<?=base_url('img/proj_new/cala.svg')?>" style="height:16px;position:absolute;right:20px;top:9px;z-index:3">
+									</div> -->
+
+									<div class="inline" style="padding:0px 12px;">完成狀態</div>
+									<div class="inline">
+										<select class="form-control" required name="check_status" id="is_finish_filter">
+											<option value="all">全部</option>
+											<option value="0">未完成</option>
+											<option value="1">已完成</option>
+										</select>
+									</div>
+
+									<div class="inline" style="line-height:32px;vertical-align:middle">
+											<div class="input-group">
+													<button type="button" class="btn btn-primary" id="searchlist2Btn">Go!</button>
+											</div>
+									</div>
+								</div>
+						    <hr class="clearfix"/>
+								<section class="col-md-12 col-xs-12 col-sm-12" style="padding: 17px 17px;border:1px solid #ccc">
+										<table id="dt_list" class="table display datatable">
+												<thead>
+													<tr>
+															<th class="no-sort">專案名稱</th>
+															<th class="no-sort">日報模板</th>
+															<th class="no-sort">完成狀態</th>
+															<th class="no-sort">專案完成</th>
+															<th class="no-sort"></th>
+															<th class="no-sort"></th>
+
+
+													</tr>
+														<tr class="searchbox">
+																<th class="no-sort"></th>
+																<th class="no-sort"></th>
+																<th class="no-sort"></th>
+																<th class="min100 no-sort">應</th>
+																<th class="min100 no-sort">已</th>
+																<th class="min100 no-sort">未</th>
+
+														</tr>
+												</thead>
+												<tbody>
+												</tbody>
+												<tfoot>
+													<tr class="searchbox">
+															<th class="no-sort"></th>
+															<th class="no-sort"></th>
+															<th class="no-sort"><span style="float:right">合計</span></th>
+															<th class="no-sort" id="all_count"><?=$all_count?></th>
+															<th class="no-sort" id="finish_count"><?=$finish_count?></th>
+															<th class="no-sort" id="unfinish_count"><?=$unfinish_count?></th>
+													</tr>
+												</tfoot>
+										</table>
+								</section>
+						    <!-- <div class="col-sm-12">
+						        <button type="button" class="btn btn-warning" onclick="javascript:void(0)">新增任務</button>
+						    </div> -->
+						</div>
+						<!-- end widget div -->
+
+					</div>
+					<!-- end widget -->
+
+				</article>
+				<!-- WIDGET END -->
+
+			</div>
+
+
+			<!-- end row -->
+
+		</section>
 		<!-- end widget grid -->
 	</div>
+
 	<div class="tab-pane animated fadeIn" id="edit_page">
 		<section class="">
 			<!-- row -->
 			<div class="row">
 				<article class="col-xs-12 col-sm-12 col-md-12 col-lg-12" id="edit-modal-body">
+
 				</article>
 			</div>
 		</section>
 	</div>
 </div>
-
 <?php $this -> load -> view('general/delete_modal'); ?>
-<script src="<?= base_url() ?>patent_vendor/ScrollToFixed-master/jquery-scrolltofixed.js"></script>
-<script src="<?= base_url() ?>patent_vendor/OwlCarousel2-2.3.4/dist/owl.carousel.min.js"></script>
-<script src="<?= base_url() ?>patent_vendor/mega-dropdown-master/js/jquery.menu-aim.js"></script>
-<script src="<?= base_url() ?>patent_vendor/mega-dropdown-master/js/mega-dropdown.js"></script>
-<script src="<?= base_url() ?>patent_vendor/fancybox/jquery.fancybox.min.js"></script>
-<script src="<?= base_url() ?>patent_js/main.min.js"></script>
 
 <script type="text/javascript">
-var baseUrl = '<?= base_url(); ?>';
+	loadScript(baseUrl + "js/class/BaseAppClass.js", function(){
+		loadScript(baseUrl + "js/app/manager_daily_report/list.js?<?=time()?>", function(){
+			currentAppm = new MdrAppClass(new BaseAppClass({}));
+			// currentApp.doEdit($('#login_user_id').val());
+		});
+	});
+</script>
 
-var data = {};
+<script type="text/javascript">
+	// date-picker
+	$(".dt_picker").datetimepicker({
+		format : 'YYYY-MM-DD'
+	});
 
-$(".dt_picker").datetimepicker({
-    format : 'YYYY-MM-DD'
-}).on('dp.change',function(event){
+	$(".dt_picker_t").datetimepicker({
+		format : 'HH:mm'
+	});
 
-});
+</script>
+<script>
+	$('td').click(function(){
+    id = $('#dt_list tr').index($(this).closest('tr'));
 
-loadScript(baseUrl + "js/class/BaseAppClass.js", function(){
-    loadScript(baseUrl + "js/app/patent/list.js", function(){
-        currentApp = new patentAppClass(new BaseAppClass({}));
+      var loading = $('<h1 class="ajax-loading-animation"><i class="fa fa-cog fa-spin"></i> Loading...</h1>')
+        .appendTo($('#edit-modal-body').empty());
+    $("#btn-submit-edit").prop( "disabled", true);
+    $('.tab-pane').removeClass('active'); $('#edit_page').addClass('active');
+    $('#edit-modal-body').load(baseUrl + 'mgmt/arrange_time_search/show_detail/' + id, function(){
+      $("#btn-submit-edit").prop( "disabled", false);
+      loading.remove();
     });
-});
+  })
 
-var timeoutId = 0;
-
-$('#keyword').keyup(function(){ 
-	clearTimeout(timeoutId);
-	var keyword = $('#keyword').val();
-	if(keyword.length<1){
-		return;
-	}
-	timeoutId = setTimeout(function () {
-        data.keyword = $('#keyword').val();
-        find_patent();
-	}, 1000);
-});
-
-function find_patent(){
-    	$.ajax({
-			url: baseUrl + 'mgmt/patent/find_patent',
+	$('#searchlist2Btn').click(function() {
+		currentAppm.tableReload();
+		$.ajax({
+			url: '<?= base_url() ?>' + 'mgmt/manager_daily_report/recount_all',
 			type: 'POST',
 			data: {
-				data:data,
+				project_filter : $('#project_filter').val(),
+				is_finish_filter : $('#is_finish_filter').val()
 			},
 			dataType: 'json',
 			success: function(d) {
-				if(d) {
-                    // $('#patent_product').append('123');          
-				    var _body = $("#patent_product").empty();
-
-                    $.each(d.items, function() {
-                        var me = this
-                        $('<figure class="koko-card">'+
-                           '<div class="box-up">'+
-                                '<div class="img_div">'+
-                                    '<img class="owl-lazy" data-src="'+ baseUrl+'api/images/get/'+me.img_id+'/thumb" alt="productnames">'+
-                                    '<a href="#">'+
-                                        '<div class="mask">'+
-                                            '<h3>'+me.patent_name+'</h3>'+
-                                        '</div>'+
-                                    '</a>'+
-                                '</div>'+
-                                '<div class="info-inner">'+
-                                    '<p class="p-name">'+me.patent_name+'</p>'+
-                                '</div>'+
-                           '</div>'+
-                        '</figure>').appendTo(_body);
-                    });
-                    
-                    $("#patent_product").owlCarousel({
-                        stagePadding: 150,
-                        margin: 20,
-                        dots: true,
-                        loop: false,
-                        merge: true,
-                        lazyLoad: true,
-                        stagePadding:2,
-                        responsive: {
-                            0: {
-                            items: 2
-                            },
-                            768: {
-                            items: 3
-                            },
-                            992: {
-                            items: 4
-                            },
-                            1046: {
-                            items: 5
-                            },
-                            1200: {
-                            items: 6
-                            },
-                            1400: {
-                            items: 7
-                            },
-                            1600: {
-                            items: 8
-                            },
-                        },
-                    });
-				}
+				$('#all_count').text(d.all_count);
+				$('#finish_count').text(d.finish_count);
+				$('#unfinish_count').text(d.unfinish_count);
+				$('a[data-dt-idx="1"]').click();
 			},
 			failure:function(){
 				alert('faialure');
 			}
 		});
-}
+	})
 
+	$('#manager_daily_report_export').click(function(){
+		var id = '';
+		layer.open({
+			type:2,
+			title:'',
+			closeBtn:0,
+			area:['1000px','500px'],
+			shadeClose:true,
+			content:'<?=base_url('mgmt/manager_daily_report/manager_daily_report_export/')?>'
+		})
+	})
 
-  
 </script>
