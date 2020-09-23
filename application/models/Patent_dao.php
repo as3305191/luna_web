be2b6e1c4bfe7b4654d4378836984c2a6d288281
江官駿 <as3305191@gmail.com>
2020-09-23 10:46:10 +0800

    update

--- application/models/Patent_dao.php
+++ application/models/Patent_dao.php
@@ -37,5 +37,48 @@ class Patent_dao extends MY_Model {
 		return count($list);
 	}
 
+	function query_ajax($data) {
+		$start = $data['start'];
+		$limit = $data['length'];
+		$columns = $data['columns'];
+		$search = $data['search'];
+		$order = $data['order'];
+
+		// select
+		$this -> db -> select('_m.*');
+
+		// join
+		$this -> ajax_from_join();
+
+		// search always
+		$this -> search_always($data);
+
+		// search
+		$this -> ajax_column_setup($columns, $search, $this -> alias_map);
+
+		// order
+		$this -> ajax_order_setup($order, $columns, $this -> alias_map);
+		$this -> db -> order_by('id', 'desc');
+
+		// limit
+		$this -> db -> limit($limit, $start);
+
+		// query results
+		$query = $this -> db -> get();
+		return $query -> result();
+	}
+
+	function search_always($data) {
+		
+	}
+
+	function ajax_from_join() {
+		// join
+		$this -> db -> from("$this->table_name as _m");
+	
+		// $this -> db -> join("roles r", "r.id = _m.role_id", "left");
+	}
+
+
 }
 ?>
