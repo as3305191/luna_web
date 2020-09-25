1421a2751dba56f0ce882091d4cf438eb3b6d716
江官駿 <as3305191@gmail.com>
2020-09-25 16:36:49 +0800

    update

--- application/controllers/api/Import.php
+++ application/controllers/api/Import.php
@@ -27,26 +27,41 @@ class Import extends MY_Base_Controller {
 				$patent_name_eng = $worksheet->getCellByColumnAndRow(1, $row)->getValue();
 				$patent_name = $worksheet->getCellByColumnAndRow(2, $row)->getValue();
 				$application_num = $worksheet->getCellByColumnAndRow(3, $row)->getValue();
-				$empid = $worksheet->getCellByColumnAndRow(4, $row)->getValue();
-				$empid = $worksheet->getCellByColumnAndRow(5, $row)->getValue();
-				$empid = $worksheet->getCellByColumnAndRow(6, $row)->getValue();
-				$empid = $worksheet->getCellByColumnAndRow(7, $row)->getValue();
-				$empid = $worksheet->getCellByColumnAndRow(8, $row)->getValue();
-				$empid = $worksheet->getCellByColumnAndRow(9, $row)->getValue();
-				$del_date = $worksheet->getCellByColumnAndRow(10, $row)->getValue();
-				$del_date = $worksheet->getCellByColumnAndRow(11, $row)->getValue();
-				$del_date = $worksheet->getCellByColumnAndRow(12, $row)->getValue();
-				$del_date = $worksheet->getCellByColumnAndRow(13, $row)->getValue();
-				$del_date = $worksheet->getCellByColumnAndRow(14, $row)->getValue();
+				$application_date = $worksheet->getCellByColumnAndRow(6, $row)->getValue();
+				$public_num = $worksheet->getCellByColumnAndRow(4, $row)->getValue();
+				$public_date = $worksheet->getCellByColumnAndRow(7, $row)->getValue();
+				$patnet_num = $worksheet->getCellByColumnAndRow(5, $row)->getValue();
+				$announcement_date = $worksheet->getCellByColumnAndRow(8, $row)->getValue();
+				$patent_start_dt = $worksheet->getCellByColumnAndRow(9, $row)->getValue();
+				$patent_end_dt = $worksheet->getCellByColumnAndRow(10, $row)->getValue();
+				$patent_finish_date = $worksheet->getCellByColumnAndRow(11, $row)->getValue();
+				$patnet_status = $worksheet->getCellByColumnAndRow(12, $row)->getValue();
+				$patnet_note = $worksheet->getCellByColumnAndRow(13, $row)->getValue();
+				$patent_range = $worksheet->getCellByColumnAndRow(14, $row)->getValue();
 				$del_date = $worksheet->getCellByColumnAndRow(15, $row)->getValue();
 				$del_date = $worksheet->getCellByColumnAndRow(16, $row)->getValue();
 				$del_date = $worksheet->getCellByColumnAndRow(17, $row)->getValue();
 
 				if($del_date=='NULL'){
 					$data = array(
-						'account'  =>$account,
-						'password' =>$password,
+						'patent_name_eng'  =>$patent_name_eng,
+						'patent_name' =>$patent_name,
+						'application_num' =>$application_num,
+						'application_date' =>$application_date,
+						'public_num' =>$public_num,
+						'public_date' =>$public_date,
+						'patnet_num' =>$patnet_num,
+						'announcement_date' =>$announcement_date,
+						'patent_start_dt' =>$patent_start_dt,
+						'patent_end_dt' =>$patent_end_dt,
+						'patent_finish_date' =>$patent_finish_date,
+						'patnet_status' =>$patnet_status,
+						'patnet_note' =>$patnet_note,
+						'patent_range' =>$patent_range,
+						'empid' =>$empid,
+						'empid' =>$empid,
 						'empid' =>$empid
+
 					);
 					$this->users_copy_dao->insert($data);
 				}
