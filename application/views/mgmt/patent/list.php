b2206714af6fe3d472a7a7985a2600578c41f2f4
江官駿 <as3305191@gmail.com>
2020-10-05 09:52:21 +0800

    update

--- application/views/mgmt/patent/list.php
+++ application/views/mgmt/patent/list.php
@@ -252,7 +252,7 @@
 	}	
 	load_category();
 	
-    var _ctype = $('#category select.change').data("val");
+    var _ctype = $('#category select').find(":selected").data("val");
 	$('#category_' + _ctype).change(function() {
 		// if(this.val()!=='all'){
 		// 	$.ajax({
