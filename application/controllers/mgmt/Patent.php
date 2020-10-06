657558b273c9d8a191fe78839429f5e452982c1e
江官駿 <as3305191@gmail.com>
2020-10-06 10:23:00 +0800

    update

--- application/controllers/mgmt/Patent.php
+++ application/controllers/mgmt/Patent.php
@@ -310,9 +310,9 @@ class Patent extends MY_Mgmt_Controller {
 		$patent= $this -> dao -> find_by_id($item_id);
 		$last_child_category = $this -> patent_category_dao -> find_by_id($patent->patnet_status);
 		$last_level = $last_child_category->level;
-		for($i==$last_level;$i>=0;$i--){
+		for($i=$last_level;$i>=0;$i--){
 			$last_child = $this -> patent_category_dao -> find_by_id($last_child_category->id);
-			if($i=$last_level){
+			if($i==$last_level){
 				$res['category_'.$i] = $item_id;
 			} else{
 				$j = $i+1;
