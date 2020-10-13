73149f9b0cfbc88941eea11984b32d3b465d6eb8
unknown <as3305191@gmail.com>
2020-10-13 10:54:21 +0800

    up

--- application/views/mgmt/patent/list.php
+++ application/views/mgmt/patent/list.php
@@ -163,7 +163,7 @@
 		data : 'img_id',
 		render: function(d,t,r){
 			if(d>0){
-				var html = '<div style="min-height:150px; object-fit: cover;background-image: url('+baseUrl+'api/images/get/'+d+'/thumb);" ></div>';
+				var html = '<div style="max-height:150px;max-width:150px;background-image: url('+baseUrl+'api/images/get/'+d+'/thumb);" ></div>';
 				return html;
 			} else{
 				return '';
