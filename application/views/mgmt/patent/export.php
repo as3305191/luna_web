88f1645bf186c6b1545702bd0846c4529db3f31f
江官駿 <as3305191@gmail.com>
2020-10-29 11:14:52 +0800

    udpate

--- application/views/mgmt/patent/export.php
+++ application/views/mgmt/patent/export.php
@@ -4,16 +4,7 @@
 		<?php $this->load->view('layout/head'); ?>
 		<script>
 			var baseUrl = '<?= base_url(); ?>';
-			var currentApp;
 
-
-			/* utilities */
-			function numberWithCommas(x) {
-			  if(!x) {
-			  	return 0;
-			  }
-			  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-			}
 		</script>
 	</head>
 <style>
