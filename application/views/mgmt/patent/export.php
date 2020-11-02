b6c70a7186e29c5ed78fc868a76a8fb7ebfdc85f
江官駿 <as3305191@gmail.com>
2020-11-02 10:37:38 +0800

    update

--- application/views/mgmt/patent/export.php
+++ application/views/mgmt/patent/export.php
@@ -17,24 +17,20 @@
 
 </style>
 <!-- Widget ID (each widget will need unique ID)-->
-<div class="jarviswidget" id="wid-id-7" data-widget-colorbutton="false"	data-widget-editbutton="false" data-widget-deletebutton="false" data-widget-sortable="false">
-
+<div class="jarviswidget" id="wid-id-7" data-widget-colorbutton="false"	data-widget-editbutton="false" data-widget-deletebutton="false" data-widget-sortable="false" style="padding:50px 50px 50px 50px">
 
 	<table id="export_table_list" class="table table-striped table-bordered table-hover" width="70%">
 		<thead>
 			<tr>
-				<th class="min100">專利名稱</th>
-				<th class="min100">代表圖</th>
-				<th class="min100">申請人</th>
-				<th class="min100">發明人</th>
-				<th class="min100">申請號</th>
-				<th class="min100">公開號</th>
-				<th class="min100">申請日</th>
-				<th class="min100">公告日</th>
-				<th class="min100">更新日</th>
+				<th class="min100" colspan="5" style="text-align:center;vertical-align:middle;">專利詳細資訊</th>
+				<th class="min100"></th>
+				<th class="min100"></th>
+				<th class="min100"></th>
+				<th class="min100"></th>
 			</tr>
 		</thead>
 		<tbody>
+			
 		</tbody>
 	</table>
 
