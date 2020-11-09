c5c15c1a8bfa4fca67257e86ca3ab3789a70f86f
江官駿 <as3305191@gmail.com>
2020-11-09 10:20:27 +0800

    update

---
+++ application/controllers/mgmt/News_img.php
@@ -0,0 +1,132 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * @brief 參數管理->條款
+ */
+class News_img extends MY_Base_Controller {
+
+	function __construct() {
+		parent::__construct();
+		$this -> load -> model('Terms_of_service_dao', 'dao');
+		// $this -> load -> model('About_articles_images_dao', 'aimg_dao');
+		$this -> load -> model('Images_dao', 'img_dao');
+	}
+
+	public function index()
+	{
+		$data = array();
+		$this -> setup_user_data($data);
+		$data['id'] = $this -> dao -> find_max_id();
+
+		$this->load->view('mgmt/news_img/list', $data);
+	}
+
+	/**
+	 * @brief 取得資料
+	 */
+
+	public function get_data() {
+		$res = array();
+		$data = $this -> get_posts(array(
+			'length',
+			'start',
+			'columns',
+			'search',
+			'order',
+			'company_id'
+		));
+
+		$items = $this -> dao -> query_ajax($data);
+		// foreach($items as $item) {
+		// 	if(!empty($item -> image_id)) {
+		// 		$item -> img_url = get_img_url($item -> image_id);
+		// 	}
+		// }
+		$res['items'] = $items;
+		$res['recordsFiltered'] = $this -> dao -> count_ajax($data);
+		$res['recordsTotal'] = $this -> dao -> count_all_ajax($data);
+		$this -> to_json($res);
+	}
+
+	/**
+	 * @brief 條款編輯
+	 */
+	public function edit($id) {
+		$data = array();
+		$data['id'] = $id;
+
+		$item = NULL;
+
+			$item = $this -> dao -> find_all();
+			// find images
+			// $item -> images = $this -> aimg_dao -> find_image_by_product_id($id);
+
+			$data['item'] = $item;
+
+
+
+
+		$this->load->view('mgmt/news_img/edit', $data);
+	}
+	/**
+	 * @brief 條款更新
+	 */
+
+	public function insert() {
+		$res = array();
+		$id = $this -> get_post('id');
+		$data = array();
+		$data_tos = array();
+		$data_ur = array();
+		$desc_tos = $this -> get_post('desc_tos');
+		$data_tos['desc'] = $desc_tos;
+		$desc_ur = $this -> get_post('desc_ur');
+		$data_ur['desc'] = $desc_ur;
+		if(!empty($desc_tos)){
+			$this -> dao -> update_by($data_tos,'rule_type',1);
+		}
+
+		if(!empty($desc_ur)){
+			$this -> dao -> update_by($data_ur,'rule_type',2);
+		}
+
+
+		// $end_time = $this -> get_post('end_time');
+		// if(empty($end_time)) {
+		// 	$data['end_time'] = NULL;
+		// } else {
+		// 	$data['end_time'] = $end_time;
+		// }
+
+		// $data['hidden'] = (!empty($this -> get_post('hidden')) ? 1 : 0);
+
+		// if(empty($id)) {
+		// 	// insert
+		// 	$id = $this -> dao -> insert($data);
+		// } else {
+		// 	// update
+		// 	$this -> dao -> update($data, $id);
+		// }
+
+
+
+		// add images
+		// $img_id_str = $this -> get_post('img_id_list');
+		// $img_id_list = explode(',', $img_id_str);
+		// $this -> aimg_dao -> add_imgs($id, $img_id_list);
+
+		$res['success'] = TRUE;
+		$res['id'] = $id;
+ 		$this -> to_json($res);
+	}
+
+	/**
+	 * @brief 移除
+	 */
+
+	public function delete($id) {
+		$res['success'] = TRUE;
+		$this -> dao -> delete_status($id, $this -> session -> userdata('user_id'));
+		$this -> to_json($res);
+	}
+}
---
+++ js/app/news_img/list.js
@@ -0,0 +1,124 @@
+var newsimgAppClass = (function(app) {
+	app.basePath = "mgmt/news_img/";
+	app.init = function() {
+		app.mDtTable = $('#dt_list').DataTable($.extend(app.dtConfig,{
+
+			ajax : {
+				url : baseUrl + app.basePath + '/get_data',
+				data : function(d) {
+
+				},
+				dataSrc : 'items',
+				dataType : 'json',
+				type : 'post'
+			},
+
+			columns : [{
+				data : 'article_name'
+			},  {
+				data : 'abstract'
+			}, {
+				data : 'create_time'
+			}],
+
+			order : [[2, "desc"]],
+			columnDefs : [{
+				targets : 0,
+				data : null,
+				defaultContent : '<a href="javascript:void(0);" style="margin-right: 5px;"><i class="fa fa-pencil fa-lg"></i></a>'
+							   + '<a href="#deleteModal" role="button" data-toggle="modal" style="margin-right: 5px;"><i class="fa fa-trash fa-lg"></i></a>'
+							   + '<a href="#copyModal" role="button" data-toggle="modal"><i class="fa fa-copy fa-lg"></i></a>',
+				searchable : false,
+				orderable : false,
+				width : "5%",
+				className : ''
+			}, {
+				"targets" : 0,
+				"orderable" : false
+			}, {
+				"targets" : 1,
+				"orderable" : false
+			}, {
+				"targets" : 2,
+				"orderable" : false
+			}],
+		}));
+
+		// data table actions
+		app.dtActions();
+
+		// get year month list
+		app.tableReload();
+
+		app.doSubmit = function() {
+			//if(!$('#app-edit-form').data('bootstrapValidator').validate().isValid()) return;
+
+			// sync ckeditor first
+			for ( instance in CKEDITOR.instances )
+		        CKEDITOR.instances[instance].updateElement();
+
+			var url = baseUrl + app.basePath + 'insert'; // the script where you handle the form input.
+			var imgIdList = app.getImgIdList();
+			$.ajax({
+				type : "POST",
+				url : url,
+				data : $("#app-edit-form").serialize()
+						+ '&' + $("#app-edit-form-s3").serialize()
+						// + '&' + $("#app-edit-form-s4").serialize()
+						// + '&spec_list=' + encodeURIComponent(JSON.stringify(specStore))
+						+ '&img_id_list=' + imgIdList.join(','),
+				success : function(data) {
+					app.tableReload();
+					currentApp.doEdit(data.id);
+					$('input[name="id"]').val(data.id);
+					// $('#editModal').modal('hide');
+				}
+			});
+		};
+		app.getImgIdList = function() {
+			var idList = [];
+			$('.kv-file-content img').each(function() {
+				if($(this).attr('src').indexOf('http') == 0) {
+					var _id = $(this).attr('src').split('/').pop();
+					idList.push(_id);
+				}
+			});
+			return idList;
+		};
+
+		// image operations
+		app.addImg = function(id) {
+			if(!app.imgIds) {
+				app.imgIds = [];
+			}
+			app.imgIds.push(id);
+		};
+
+		app.delImg = function(id) {
+			if(app.imgIds && app.imgIds.length > 0) {
+				for(var i = app.imgIds.length - 1; i >= 0; i--) {
+				    if(app.imgIds[i] === id) {
+				       app.imgIds.splice(i, 1);
+				    }
+				}
+			}
+		};
+
+		app.getImgs = function() {
+			if(!app.imgIds) {
+				app.imgIds = [];
+			}
+			return app.imgIds;
+		};
+
+		app.clearImgs = function() {
+			app.imgIds = [];
+		};
+
+
+
+		return app;
+	};
+
+	return app.init();
+	});
