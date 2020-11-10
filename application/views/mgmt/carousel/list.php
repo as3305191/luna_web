ff5a22d073a27d46a4bbe267f771401f3ce1d2eb
江官駿 <as3305191@gmail.com>
2020-11-10 11:43:50 +0800

    update

--- application/views/mgmt/carousel/list.php
+++ application/views/mgmt/carousel/list.php
@@ -1,101 +1,159 @@
-<style>
-.slide-container {
-    width: 500px;
-    height: 300px;
-  	overflow: hidden;
-}
-.middle {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-}
-.slide img {
-    width: 100%;
-    height: 100%;
-}
+<head>
+  
+    <style>
+        .slide-container {
+            width: 500px;
+            height: 300px;
+            overflow: hidden;
+        }
 
-.navigation {
-    position: absolute;
-    left: 50%;
-    bottom: 30px;
-    transform: translateX(-50%);
-    display: flex;
-}
+        .slide-wrap {
+            display: flex;
+            width: 500%;
+            height: 100%;
+        }
 
-.bar {
-    width: 20px;
-    height: 10px;
-    border: 3px white solid;
-    margin-left: 10px;
-  	cursor: pointer;
-		transition: 0.5s;
-}
+        .middle {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+        }
 
-.bar:hover {
-    background: white;
-}
+        .slide {
+            width: 20%;
+            height: 100%;
+            transition: 1s;
+        }
 
-input[name="r"] {
-    position: absolute;
-    visibility: hidden;
-}
+        .slide img {
+            width: 100%;
+            height: 100%;
+        }
 
+        .navigation {
+            position: absolute;
+            left: 50%;
+            bottom: 30px;
+            transform: translateX(-50%);
+            display: flex;
+        }
 
-.s1 {
-    animation: loop 12s linear infinite;
-}
+        .bar {
+            width: 20px;
+            height: 10px;
+            border: 3px white solid;
+            margin-left: 10px;
+            cursor: pointer;
+            transition: 0.5s;
+        }
 
-@keyframes loop {
-    0% {
-        margin-left: 0;
-    }
-    15% {
-        margin-left: 0;
-    }
-    /* 停留1500ms */
-    20% {
-        margin-left: -20%;
-    }
-    /* 切换500ms 位移-20% */
-    35% {
-        margin-left: -20%;
-    }
-    40% {
-        margin-left: -40%;
-    }
-    55% {
-        margin-left: -40%;
-    }
-    60% {
-        margin-left: -60%;
-    }
-    75% {
-        margin-left: -60%;
-    }
-    80% {
-        margin-left: -80%;
-    }
-    95% {
-        margin-left: -80%;
-    }
-    100% {
-        margin-left: 0;
-    }
-}
-</style>
+        .bar:hover {
+            background: white;
+        }
 
-<div class="slide-container middle">
-    <div class="slide-wrap">
-        <div class="slide">
-            <img src="<?= base_url('img/ktx_img/logo.jpg') ?>" alt="">
+        input[name="r"] {
+            position: absolute;
+            visibility: hidden;
+        }
+
+        #r1:checked~.s1 {
+            margin-left: 0;
+        }
+
+        #r2:checked~.s1 {
+            margin-left: -20%;
+        }
+
+        #r3:checked~.s1 {
+            margin-left: -40%;
+        }
+
+        #r4:checked~.s1 {
+            margin-left: -60%;
+        }
+
+        #r5:checked~.s1 {
+            margin-left: -80%;
+        }
+
+        .s1 {
+            animation: loop 12s linear infinite;
+        }
+
+        @keyframes loop {
+            0% {
+                margin-left: 0;
+            }
+            15% {
+                margin-left: 0;
+            }
+            /* 停留1500ms */
+            20% {
+                margin-left: -20%;
+            }
+            /* 切换500ms 位移-20% */
+            35% {
+                margin-left: -20%;
+            }
+            40% {
+                margin-left: -40%;
+            }
+            55% {
+                margin-left: -40%;
+            }
+            60% {
+                margin-left: -60%;
+            }
+            75% {
+                margin-left: -60%;
+            }
+            80% {
+                margin-left: -80%;
+            }
+            95% {
+                margin-left: -80%;
+            }
+            100% {
+                margin-left: 0;
+            }
+            /* 复位到第一张图片 */
+        }
+    </style>
+</head>
+
+    <div class="slide-container middle">
+        <div class="slide-wrap">
+            <input type="radio" name="r" id="r1" checked>
+            <input type="radio" name="r" id="r2">
+            <input type="radio" name="r" id="r3">
+            <input type="radio" name="r" id="r4">
+            <input type="radio" name="r" id="r5">
+            <div class="slide s1">
+                <img src="<?= base_url('img/ktx_img/logo.jpg') ?>" alt="">
+            </div>
+            <div class="slide">
+                <img src="<?= base_url('img/ktx_img/logo.jpg') ?>" alt="">
+            </div>
+            <div class="slide">
+                <img src="<?= base_url('img/ktx_img/logo.jpg') ?>" alt="">
+            </div>
+            <div class="slide">
+                <img src="<?= base_url('img/ktx_img/logo.jpg') ?>" alt="">
+            </div>
+            <div class="slide">
+                <img src="<?= base_url('img/ktx_img/logo.jpg') ?>" alt="">
+            </div>
+
+            <div class="navigation">
+                <label class="bar" for="r1"></label>
+                <label class="bar" for="r2"></label>
+                <label class="bar" for="r3"></label>
+                <label class="bar" for="r4"></label>
+                <label class="bar" for="r5"></label>
+            </div>
         </div>
-        <div class="slide">
-            <img src="<?= base_url('img/ktx_img/logo.jpg') ?>" alt="">
-        </div>
-        
     </div>
-</div>
 
 <input type="radio" class="s1" name="r" id="r1" checked>
 <input type="radio" class="s1" name="r" id="r2">
