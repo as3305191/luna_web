37d038b32ad764b8688a6e1a12162b6a9e6881ac
江官駿 <as3305191@gmail.com>
2020-11-12 15:59:51 +0800

    update

--- application/controllers/mgmt/Carousel.php
+++ application/controllers/mgmt/Carousel.php
@@ -18,7 +18,7 @@ class Carousel extends MY_Mgmt_Controller {
 		$data = $this -> setup_user_data($data);
 		
 		$data['login_user'] = $this -> users_dao -> find_by_id($data['login_user_id']);
-		$content = file_get_contents('https://tw.rter.info/capi.php');
+		$content = file_get_contents('https://openapi.taifex.com.tw/v1/DailyForeignExchangeRates.php');
 		$currency = json_decode($content);
 		$data['currency'] = $currency;
 		$this -> to_json($data['currency']);
