90c9c79ba26fe7d14b5331f85ac7ba1e7df3e100
江官駿 <as3305191@gmail.com>
2020-11-12 14:00:38 +0800

    update

--- js/app/carousel/list.js
+++ js/app/carousel/list.js
@@ -2,43 +2,26 @@ var CarouselClass = (function(app) {
 	app.basePath = "mgmt/carousel/";
 
 	app.init = function() {
-		app = $('#news_container');
-		var wOnResize = function(){
+		app.mDtTable = $('#dt_list').DataTable($.extend(app.dtConfig,{
+			ajax : {
+				url : baseUrl + app.basePath + '/get_data',
+				data : function(d) {
+					// d.role_id = $('#role_id').val();
+				},
+				dataSrc : 'items',
+				dataType : 'json',
+				type : 'post',
+			
+			},
 
-			var currentListElId = "#news_container";
-			if(currentApp && currentApp.dtListId) {
-				currentListElId = currentApp.dtListId;
-			}
-			if($(window).height() < 700) {
-				$('.t-box').css('height', ($(currentListElId).height()) + 'px');
-			} else {
-				$('.t-box').css('max-height', ($(window).height() - 320) + 'px');
-			}
-			$('#widget-grid').width($('#content').width() - 13);
-		};
+		}));
+
 		// data table actions
-		app.dtActions = function() {
-			// search box
-			$(app.dtListId + " thead th input[type=text]").on('change keyup', function() {
-				var me = this;
-	
-				setTimeout(function(){
-					app.settings()[0].jqXHR.abort();
-					app.column($(me).parent().index() + ':visible').search(me.value).draw();
-				}, 100);
-			});
-	
-			// trigger on resize when draw datatable
-			$(app.dtListId).on('draw.dt', function(){
-				wOnResize();
-			});
-		};
-		// get year month list
-		app.Reload = function() {
-			location.reload();
-		};
 		app.dtActions();
-		// app.Reload();
+
+		// get year month list
+		app.tableReload();
+
 		return app;
 	};
 
