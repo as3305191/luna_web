1f8eb83bc251bab386032607d80c6b01e662dc8b
江官駿 <as3305191@gmail.com>
2020-11-20 10:55:40 +0800

    up

--- js/app/news_img/list.js
+++ js/app/news_img/list.js
@@ -38,37 +38,37 @@ var NewsimgClass = (function(app) {
 							});
 					});
 
-					//img post
-					$(nRow).find('.product-post').eq(0).click(function(){
-						var me = $(this).attr('id');
-						$.ajax({
-							url: baseUrl + app.basePath + '/img_post',
-							data: {
-							  'img_id': me
-							},
-							error: function() {
-							},
-							dataType: 'json',
-							success: function(data) {
-								if(data.success_msg){
-									$.smallBox({
-										title: data.success_msg,
-										content: "<i class='fa fa-clock-o'></i> <i>1 seconds ago...</i>",
-										color: "#5F895F",
-										iconSmall: "fa fa-check bounce animated",
-										timeout: 4000
-									});
+					// //img post
+					// $(nRow).find('.product-post').eq(0).click(function(){
+					// 	var me = $(this).attr('id');
+					// 	$.ajax({
+					// 		url: baseUrl + app.basePath + '/img_post',
+					// 		data: {
+					// 		  'img_id': me
+					// 		},
+					// 		error: function() {
+					// 		},
+					// 		dataType: 'json',
+					// 		success: function(data) {
+					// 			if(data.success_msg){
+					// 				$.smallBox({
+					// 					title: data.success_msg,
+					// 					content: "<i class='fa fa-clock-o'></i> <i>1 seconds ago...</i>",
+					// 					color: "#5F895F",
+					// 					iconSmall: "fa fa-check bounce animated",
+					// 					timeout: 4000
+					// 				});
 
-									app.tableReload();
+					// 				app.tableReload();
 
-								}
-								if(data.message){
-									layer.msg(data.message);
-								}
-							},
-							type: 'POST'
-						})
-					})
+					// 			}
+					// 			if(data.message){
+					// 				layer.msg(data.message);
+					// 			}
+					// 		},
+					// 		type: 'POST'
+					// 	})
+					// })
 
 				if(app.fnRowCallbackExt) {
 					app.fnRowCallbackExt(nRow, aData, iDisplayIndex, iDisplayIndexFull);
@@ -141,34 +141,34 @@ var NewsimgClass = (function(app) {
 				targets : 0,
 				data : null,
 				render:function ( data, type, row ) {
-					var input = '';
+					// var input = '';
 					// if(row.status == 1){
 					// 	input = '<input type="checkbox"  class="product-post onoffswitch-checkbox" checked id="'+row.id+'" >'
 					// }else{
 					// 	input = '<input type="checkbox"  class="product-post onoffswitch-checkbox" id="'+row.id+'" >'
 					// }
 
-					if(row.status == 1){
-						input = '<input type="checkbox"  class="product-post onoffswitch-checkbox" checked id="'+row.id+'" >'
-						var html = '<span class="onoffswitch" style="margin-top: 10px;">'
-						+input
-						+'<label class="onoffswitch-label" for="'+row.id+'">'
-							+'<span class="onoffswitch-inner" data-swchon-text="預設" data-swchoff-text="非預設"></span>'
-							+'<span class="onoffswitch-switch"></span>'
-						+'</label>'
-					+'</span>';
-					}else{
-						input = '<input type="checkbox"  class="product-post onoffswitch-checkbox" id="'+row.id+'" >'
-						var html = '<span class="onoffswitch" style="margin-top: 10px;">'
-						+input
-						+'<label class="onoffswitch-label" for="'+row.id+'">'
-							+'<span class="onoffswitch-inner" data-swchon-text="預設" data-swchoff-text="非預設"></span>'
-							+'<span class="onoffswitch-switch"></span>'
-						+'</label>'
-					+'</span>'
-					+ '<a href="#deleteModal" role="button" data-toggle="modal" style="margin-left: 10px;"><i class="fa fa-trash fa-lg"></i></a>';
-					}
-					return html;
+					// if(row.status == 1){
+					// 	input = '<input type="checkbox"  class="product-post onoffswitch-checkbox" checked id="'+row.id+'" >'
+					// 	var html = '<span class="onoffswitch" style="margin-top: 10px;">'
+					// 	+input
+					// 	+'<label class="onoffswitch-label" for="'+row.id+'">'
+					// 		+'<span class="onoffswitch-inner" data-swchon-text="預設" data-swchoff-text="非預設"></span>'
+					// 		+'<span class="onoffswitch-switch"></span>'
+					// 	+'</label>'
+					// +'</span>';
+					// }else{
+					// 	input = '<input type="checkbox"  class="product-post onoffswitch-checkbox" id="'+row.id+'" >'
+					// 	var html = '<span class="onoffswitch" style="margin-top: 10px;">'
+					// 	+input
+					// 	+'<label class="onoffswitch-label" for="'+row.id+'">'
+					// 		+'<span class="onoffswitch-inner" data-swchon-text="預設" data-swchoff-text="非預設"></span>'
+					// 		+'<span class="onoffswitch-switch"></span>'
+					// 	+'</label>'
+					// +'</span>'
+					// + '<a href="#deleteModal" role="button" data-toggle="modal" style="margin-left: 10px;"><i class="fa fa-trash fa-lg"></i></a>';
+					// }
+					// return html;
 		    },
 				searchable : false,
 				orderable : false,
@@ -263,137 +263,6 @@ var NewsimgClass = (function(app) {
 		$('#s_img_style').on('change', function(){
 			app.tableReload();
 		});
-
-		return app;
-	};
-
-	// return self
-	return app.init();
-});
-
-
-var NewsEditAppClass = (function(app) {
-	app.basePath = "mgmt/news_edit/";
-
-	app.init = function() {
-		app.mDtTable = $('#dt_list').DataTable($.extend(app.dtConfig,{
-			ajax : {
-				url : baseUrl + app.basePath + '/get_data',
-				data : function(d) {
-					// d.role_id = $('#role_id').val();
-				},
-				dataSrc : 'items',
-				dataType : 'json',
-				type : 'post'
-			},
-
-			columns : [null, {
-				data : 'title'
-			}, {
-				data : 'news_style'
-			}, {
-				data : 'create_time'
-			}],
-			order : [[3, "desc"]],
-			columnDefs : [{
-				targets : 0,
-				data : null,
-
-				defaultContent : app.defaultContent,
-				searchable : false,
-				orderable : false,
-				width : "5%",
-				className : ''
-			}, {
-				"targets" : 0,
-				"orderable" : false
-			}, {
-				"targets" : 1,
-				"orderable" : false
-			}, {
-				"targets" : 2,
-				"orderable" : false
-			}]
-
-
-		}));
-
-		// data table actions
-		app.dtActions();
-
-		// get year month list
-		app.tableReload();
-
-		app.doSubmit = function() {
-			//if(!$('#app-edit-form').data('bootstrapValidator').validate().isValid()) return;
-
-			// sync ckeditor first
-			for ( instance in CKEDITOR.instances )
-		        CKEDITOR.instances[instance].updateElement();
-
-
-			var url = baseUrl + app.basePath + 'insert'; // the script where you handle the form input.
-			var imgIdList = app.getImgIdList();
-			$.ajax({
-				type : "POST",
-				url : url,
-				data : $("#app-edit-form").serialize()
-						+ '&' + $("#app-edit-form-s3").serialize()
-						// + '&' + $("#app-edit-form-s4").serialize()
-						// + '&spec_list=' + encodeURIComponent(JSON.stringify(specStore))
-						+ '&img_id_list=' + imgIdList.join(','),
-				success : function(data) {
-					app.tableReload();
-					currentApp.doEdit(data.id);
-					$('input[name="id"]').val(data.id);
-					// $('#editModal').modal('hide');
-				}
-			});
-		};
-
-	app.getImgIdList = function() {
-		var idList = [];
-		$('.kv-file-content img').each(function() {
-			if($(this).attr('src').indexOf('http') == 0) {
-				var _id = $(this).attr('src').split('/').pop();
-				idList.push(_id);
-			}
-		});
-		return idList;
-	};
-
-	// image operations
-	app.addImg = function(id) {
-		if(!app.imgIds) {
-			app.imgIds = [];
-		}
-		app.imgIds.push(id);
-	};
-
-	app.delImg = function(id) {
-		if(app.imgIds && app.imgIds.length > 0) {
-			for(var i = app.imgIds.length - 1; i >= 0; i--) {
-				if(app.imgIds[i] === id) {
-				   app.imgIds.splice(i, 1);
-				}
-			}
-		}
-	};
-
-	app.getImgs = function() {
-		if(!app.imgIds) {
-			app.imgIds = [];
-		}
-		return app.imgIds;
-	};
-
-	app.clearImgs = function() {
-		app.imgIds = [];
-	};
-
-	$('#s_news_style').on('change', function(){
-		app.tableReload();
-	});
 
 		return app;
 	};
