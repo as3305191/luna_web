2ecd23691ab94eec78d3394cf748924e582de522
江官駿 <as3305191@gmail.com>
2020-11-24 09:06:26 +0800

    up

--- application/controllers/mgmt/Carousel.php
+++ application/controllers/mgmt/Carousel.php
@@ -18,14 +18,33 @@ class Carousel extends MY_Mgmt_Controller {
 		$data['items']= $items;
 		
 		$today = date("Y-m-d");
+		$this_year_first_day = date("Y")."01-01";
 		$during_now = date('z', strtotime($today)); 
 		$today_h = date("H");
 		$today_m = date("i");
 		$today_s = date("s");
-		$data['during_now_s']= ($during_now*86400)+($today_h*3600)+($today_m*60)+$today_s;
+		$total_weekends = $this -> get_weekend_days($this_year_first_day,$today);
+		// $data['during_now_s']= ($during_now*86400)+($today_h*3600)+($today_m*60)+$today_s;
+		$data['during_now_s']= $during_now-$total_weekends;
+
 		$this -> to_json($data);
 		$this->load->view('mgmt/carousel/list', $data);
 	}
 
+	public function get_weekend_days($start_date,$end_date){
 
+		if (strtotime($start_date) > strtotime($end_date)) list($start_date, $end_date) = array($end_date, $start_date);
+		
+		$start_reduce = $end_add = 0;
+		
+		$start_N = date('N',strtotime($start_date));
+		$start_reduce = ($start_N == 7) ? 1 : 0;
+		
+		$end_N = date('N',strtotime($end_date));
+		in_array($end_N,array(6,7)) && $end_add = ($end_N == 7) ? 2 : 1;
+		
+		$days = abs(strtotime($end_date) - strtotime($start_date))/86400 + 1;
+		
+		return floor(($days + $start_N - 1 - $end_N) / 7) * 2 - $start_reduce + $end_add;
+	}
 }
