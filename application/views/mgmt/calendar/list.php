0ea1179a7b80a89131824466f7ae467d78b2fdc6
江官駿 <as3305191@gmail.com>
2020-11-24 10:12:49 +0800

    up

--- application/views/mgmt/calendar/list.php
+++ application/views/mgmt/calendar/list.php
@@ -8,7 +8,15 @@
 <script type="text/javascript">
     $(document).on('ready', function () {
         $('#calendar').fullCalendar({
-        //相關參數
+            header: { // 頂部排版
+                left: "prev,next today", // 左邊放置上一頁、下一頁和今天
+                center: "title", // 中間放置標題
+                right: "month,basicWeek,basicDay" // 右邊放置月、周、天
+            },
+            userLang    : 'zh-TW',
+            americanMode: false,
+            editable: true,
+
         })
     });
 
