a335ee9743879bbab82e03d87c750d676b30129d
江官駿 <as3305191@gmail.com>
2020-11-24 14:52:13 +0800

    up

--- application/views/mgmt/carousel/list.php
+++ application/views/mgmt/carousel/list.php
@@ -90,8 +90,10 @@
     //     } 
     // }, 1000); 
 
-    var counter = $('#counter');
+    var counter = '<?= $during_now_s?>';
+    var counter_add = parseFloat(counter*29.01);
+
     setInterval(function() { 
-        counter.text(counter.text()+=29.01); 
+        $('#counter').text(counter_add+=29.01); 
     }, 1000); 
 </script>
