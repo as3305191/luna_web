90b932e9f8d6963ebe1d23c54c5805d95fd299ff
江官駿 <as3305191@gmail.com>
2020-11-24 10:08:32 +0800

    up

---
+++ application/controllers/mgmt/Calendar.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Calendar extends MY_Mgmt_Controller {
+
+	function __construct() {
+		parent::__construct();
+
+		$this -> load -> model('Img_style_dao', 'img_style_dao');
+		$this -> load -> model('News_dao', 'news_dao');
+		$this -> load -> model('News_style_dao', 'news_style_dao');		
+	}
+
+	public function index()
+	{
+		$data = array();
+		
+		// $this -> to_json($data);
+		$this->load->view('mgmt/calendar/list', $data);
+	}
+
+}
---
+++ application/views/mgmt/calendar/list.php
@@ -0,0 +1,15 @@
+<head>
+
+</head>
+<div id="calendar">
+  
+</div>
+<script src="<?= base_url("js/plugin/fullcalendar/-jquery.fullcalendar.min.js") ?>" type="text/javascript"></script>
+<script type="text/javascript">
+    $(document).on('ready', function () {
+        $('#calendar').fullCalendar({
+        //相關參數
+        })
+    });
+
+</script>
