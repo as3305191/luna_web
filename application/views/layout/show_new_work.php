54bcf712015301b1ec278bab8de09f13e14b7d69
江官駿 <as3305191@gmail.com>
2020-11-24 10:31:25 +0800

    update

--- application/views/layout/show_new_work.php
+++ application/views/layout/show_new_work.php
@@ -12,7 +12,7 @@
 </style>
 <div class="col-xs-12" style="padding:20px">
     <div class="col-xs-12">
-        <span style="font-size:12pt">新增事項</span>
+        <span style="font-size:12pt"><?=$parent -> name?>新增事項</span>
     </div>
     <hr/>
     <input type="hidden" name="parent_id" id="parent_id" value="<?=$parent -> id?>" />
