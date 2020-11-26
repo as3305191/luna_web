46af231da0f70827067391c136a28ad95bb1f1c5
江官駿 <as3305191@gmail.com>
2020-11-26 14:27:16 +0800

    update

--- application/controllers/mgmt/Carousel.php
+++ application/controllers/mgmt/Carousel.php
@@ -16,7 +16,6 @@ class Carousel extends MY_Mgmt_Controller {
 		$data = array();
 		$items= $this -> news_dao -> find_carousel();
 		$data['items']= $items;
-		
 		$today = date("Y-m-d");
 		$this_year_first_day = date("Y")."-01-01";
 		$this_month_first_day = date("Y-m")."-01";
@@ -32,8 +31,7 @@ class Carousel extends MY_Mgmt_Controller {
 		$data['during_now_s'] = ((intval($during_now-$total_years_weekends)*28800)+(($today_h-8)*3600)+($today_m*60)+$today_s)*$cost;
 		$data['during_m_now_s'] =(((intval(round((strtotime($today)-strtotime($this_month_first_day)))/3600/24)-$total_month_weekends)*28800)+(($today_h-8)*3600)+($today_m*60)+$today_s)*$cost;
 		$data['during_today_now_s'] = ((($today_h-8)*3600)+($today_m*60)+$today_s)*$cost;
-		// $data['this_year_first_day']= $this_year_first_day;
-		// $data['today']= $today;
+	
 		// $this -> to_json($data);
 		$this->load->view('mgmt/carousel/list', $data);
 	}
@@ -41,17 +39,12 @@ class Carousel extends MY_Mgmt_Controller {
 	public function get_weekend_days($start_date,$end_date){
 
 		if (strtotime($start_date) > strtotime($end_date)) list($start_date, $end_date) = array($end_date, $start_date);
-		
 		$start_reduce = $end_add = 0;
-		
 		$start_N = date('N',strtotime($start_date));
 		$start_reduce = ($start_N == 7) ? 1 : 0;
-		
 		$end_N = date('N',strtotime($end_date));
 		in_array($end_N,array(6,7)) && $end_add = ($end_N == 7) ? 2 : 1;
-		
 		$days = abs(strtotime($end_date) - strtotime($start_date))/86400 + 1;
-		
 		return floor(($days + $start_N - 1 - $end_N) / 7) * 2 - $start_reduce + $end_add;
 	}
 }
