29334f3f3bfbc4b048fba4ce54e1826c7cbde169
江官駿 <as3305191@gmail.com>
2020-12-03 16:24:27 +0800

    update

--- application/controllers/mgmt/Product.php
+++ application/controllers/mgmt/Product.php
@@ -99,10 +99,6 @@ class Product extends MY_Mgmt_Controller {
  		$this -> to_json($res);
 	}
 
-	public function copy() {
-
-	}
-
 	public function delete($id) {
 		$res['success'] = TRUE;
 		//$this -> dao -> delete_status($id, $this -> session -> userdata('user_id'));
