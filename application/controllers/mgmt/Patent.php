990f5d8b52ccffe744f1e08224cdef8d77b91aed
江官駿 <as3305191@gmail.com>
2020-12-09 08:34:42 +0800

    update

--- application/controllers/mgmt/Patent.php
+++ application/controllers/mgmt/Patent.php
@@ -66,6 +66,7 @@ class Patent extends MY_Mgmt_Controller {
 	public function edit($id) {
 		$data = array();
 		$u_data = array();
+	
 		$data['id'] = $id;
 		if(!empty($id)) {
 			$q_data = $this -> get_posts(array(
@@ -119,12 +120,12 @@ class Patent extends MY_Mgmt_Controller {
 			}
 
 			$data['item'] = $item;
+			$this -> session -> set_userdata('patent_data', $data);
 		}
+		
 		$u_data = $this -> setup_user_data($u_data);
 		$data['login_user'] = $this -> users_dao -> find_by_id($u_data['login_user_id']);
 		$data['country'] = $this -> country_dao -> find_all();
-		$this -> session -> set_userdata('patent_data', $data);
-
 		// $this -> to_json($data);
 		$this->load->view('mgmt/patent/edit', $data);
 	}
