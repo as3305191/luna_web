f4ba8bea6e05c39e95bfe9b9c3d97a4ce6b7fd2b
江官駿 <as3305191@gmail.com>
2021-01-08 11:56:31 +0800

    update

--- application/views/mgmt/members_login/list.php
+++ application/views/mgmt/members_login/list.php
@@ -87,13 +87,10 @@
 	}];
 
 	var mOrderIdx = 0;
-
 	var defaultContent = '<a href="#deleteModal" role="button" data-toggle="modal" style="margin-right: 5px;"><i class="fa fa-trash fa-lg"></i></a>';
-
 	var mColDefs = [{
 		targets : 0,
 		data : null,
-
 		defaultContent : defaultContent,
 		searchable : false,
 		orderable : false,
