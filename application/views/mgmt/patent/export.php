115447f2ca2fee43382a3354c14c308240ce1427
江官駿 <as3305191@gmail.com>
2021-01-08 13:41:56 +0800

    update

--- application/views/mgmt/patent/export.php
+++ application/views/mgmt/patent/export.php
@@ -32,9 +32,9 @@
 			<tr>
 				<td class="min200">專利項目</td>
 				<td colspan="3"></td>
-				<?php if($item -> patnet_status==1): ?>
+				<?php if(!empty($item->image) && $item->image !==''): ?>
 					<td rowspan="5"><img src="<?= base_url("api/images/get/".$item->image[0]."/thumb") ?>" style="height:500%;width:100%" ></td>
-				<?php elseif($item -> patnet_status==2): ?>
+				<?php else : ?>
 					<td rowspan="5">沒有圖片</td>
 				<?php endif?>
 			</tr>
