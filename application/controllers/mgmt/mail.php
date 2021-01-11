325ac706cfaaffd0325287953a7f6408927651ac
江官駿 <as3305191@gmail.com>
2021-01-11 13:57:05 +0800

    update

--- application/controllers/mgmt/mail.php
+++ application/controllers/mgmt/mail.php
@@ -22,4 +22,9 @@ class Mail extends MY_Mgmt_Controller {
 		$login_user = $this -> users_dao -> find_by_id($s_data['login_user_id']);
 		$this->mail($login_user->role_id,$to,$subject,$text,$cc);
 	}
+
+	public function load_department_mail(){
+
+	}
+
 }
