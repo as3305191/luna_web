5fdc7d704df0938bb8e01cef267f419ddc3be761
江官駿 <as3305191@gmail.com>
2021-02-02 11:22:06 +0800

    update

--- application/controllers/mgmt/Patent.php
+++ application/controllers/mgmt/Patent.php
@@ -132,13 +132,8 @@ class Patent extends MY_Mgmt_Controller {
 	public function new_patent_family() {
 		$res = array();
 		$count_num = $this -> dao -> find_by_all_today_add();
-		if($count_num=='0'){
-			$last_num = $count_num+1;
-			$family_num = date('Ymd').'0'.$last_num;
-		} else{
-			$last_num = $count_num+1;
-			$family_num = date('Ymd').'0'.$last_num;
-		}
+		$last_num = $count_num+1;
+		$family_num = date('Ymd').'0'.$last_num;
 		$res['family_num'] = $family_num;		
 		$res['success'] = TRUE;
 		$this -> to_json($res);
