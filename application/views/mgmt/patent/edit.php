cfa98b4304de157a2f3cf33d3002b6696df4463f
江官駿 <as3305191@gmail.com>
2021-02-05 09:55:41 +0800

    update

--- application/views/mgmt/patent/edit.php
+++ application/views/mgmt/patent/edit.php
@@ -842,7 +842,6 @@ function do_save() {
 											'<option value="all">全部</option>'+
 										'</select>'+
 									'</div>';
-								
 							} else{
 								html+='<div class="widget-toolbar pull-left">'+
 											'<select id="patnet_status_'+i+'" class="p_patnet_status form-control" data-val="'+i+'" disabled>'+
