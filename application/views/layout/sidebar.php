e3ba8b0c1b7359636cc42fd9df6c66fffa135b39
pony <as3305191@gmail.com>
2021-02-05 11:28:41 +0800

    up

--- application/views/layout/sidebar.php
+++ application/views/layout/sidebar.php
@@ -7,7 +7,6 @@
   
   <div class="list-group list-group-border-0 g-mb-40" id="user_sidebar">
     <!-- Overall -->
-
     <!-- <a href="<?= base_url("coach/coach_home") ?>" class="list-group-item justify-content-between coach_home">
       <span><i class="icon-home g-pos-rel g-top-1 g-mr-8"></i> 主頁</span>
       <span class="u-label g-font-size-11 g-bg-white g-color-main g-rounded-20 g-px-10">2</span>
