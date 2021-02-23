a1b04c6fa38160009c3add19c6580fef86375dd5
江官駿 <as3305191@gmail.com>
2021-02-23 10:22:21 +0800

    up

--- application/views/mgmt/patent/edit.php
+++ application/views/mgmt/patent/edit.php
@@ -855,7 +855,7 @@ function do_save() {
 						$.each(d.category, function(){
 							var level = this.level;
 							if($('#role_id').val()=='9'||$('#role_id').val()=='11'||$('#role_id').val()=='28'){
-								if(current_app[0].indexOf('patnet_status_'+level)>0){
+								if(current_app[0].indexOf('patnet_status_'+level)>=0){
 									if(current_app[0]['patnet_status_'+level] &&current_app[0]['patnet_status_'+level]==this.id){
 										$('<option />', {
 											'value': this.id,
