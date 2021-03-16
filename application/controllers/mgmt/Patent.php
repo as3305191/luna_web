d293cd417cf675276376af22d9bbc8af7de1fb8b
江官駿 <as3305191@gmail.com>
2021-03-16 13:15:11 +0800

    up

--- application/controllers/mgmt/Patent.php
+++ application/controllers/mgmt/Patent.php
@@ -48,10 +48,10 @@ class Patent extends MY_Mgmt_Controller {
 
 		$patent_status = $this->get_post('patent_status');
 
-		if (!empty($patent_status)) {
-			$new_patent_status = explode(",", str_replace('#', ',', $patent_status));
-			$data['patent_status'] = $new_patent_status;
-		}
+		// if (!empty($patent_status)) {
+		// 	$new_patent_status = explode(",", str_replace('#', ',', $patent_status));
+		// 	$data['patent_status'] = $new_patent_status;
+		// }
 
 		$s_data = $this -> setup_user_data(array());
 		$login_user = $this -> dao -> find_by_id($s_data['login_user_id']);
