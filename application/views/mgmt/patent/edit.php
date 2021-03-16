bc38c2f6a44a9dfd490f4f625cb3de1baea09cd9
江官駿 <as3305191@gmail.com>
2021-03-16 16:59:02 +0800

    up

--- application/views/mgmt/patent/edit.php
+++ application/views/mgmt/patent/edit.php
@@ -859,7 +859,7 @@ function do_save() {
 							
 						}
 						$(html).appendTo($category);
-						if($("#item_id").val()>0){
+						// if($("#item_id").val()>0){
 							$.each(d.category, function(){
 								category_level = this.level;
 								if(current_app[0].hasOwnProperty('patnet_status_'+category_level) && current_app[0]['patnet_status_'+category_level]==this.id){
@@ -880,15 +880,15 @@ function do_save() {
 							}else{
 								$('#patnet_status_'+category_level).attr("disabled",true);
 							}
-						} else{
-							$.each(d.category, function(){
-								category_level = this.level;
-								$('<option />', {
-									'value': this.id,
-									'text': this.name,
-								}).appendTo($('#patnet_status_'+category_level));
-							});
-						}
+						// } else{
+						// 	$.each(d.category, function(){
+						// 		category_level = this.level;
+						// 		$('<option />', {
+						// 			'value': this.id,
+						// 			'text': this.name,
+						// 		}).appendTo($('#patnet_status_'+category_level));
+						// 	});
+						// }
 					
 						$('.p_patnet_status').on('change', function(){
 							now_patent_status = [];
