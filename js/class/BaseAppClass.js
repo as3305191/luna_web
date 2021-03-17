8003c2e748e8a0a86a3b188f746930e9dc3dd9a5
江官駿 <as3305191@gmail.com>
2021-03-17 13:06:10 +0800

    up

--- js/class/BaseAppClass.js
+++ js/class/BaseAppClass.js
@@ -140,7 +140,7 @@ var BaseAppClass = (function(app) {
 		$('.tab-pane').removeClass('active'); $('#edit_page').addClass('active');
 
 
-			location.hash = baseUrl + app.basePath + 'edit/' + id;
+			location.hash = app.basePath + 'edit/' + id;
 		
 			$('#edit-modal-body').load(baseUrl + app.basePath + 'edit/' + id, function(){
 				$("#btn-submit-edit").prop( "disabled", false);
