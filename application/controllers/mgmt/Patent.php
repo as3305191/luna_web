5b63415d3c251669bbfe6d82b10b17ae96ca7a0e
江官駿 <as3305191@gmail.com>
2021-03-18 16:54:12 +0800

    up

--- application/controllers/mgmt/Patent.php
+++ application/controllers/mgmt/Patent.php
@@ -67,11 +67,7 @@ class Patent extends MY_Mgmt_Controller {
 			foreach($patent_country_list as $each_country_list){
 				$patent_country_name[] = $each_country_list->patent_country_name;
 			}
-			if(count($patent_country_name)>1){
-				$each -> total_country = explode(",", $patent_country_name);
-			} else{
-				$each -> total_country = $patent_country_name[0];
-			}
+			$each -> total_country = $patent_country_name[0];
 		}
 
 		$res['items'] = $items;
