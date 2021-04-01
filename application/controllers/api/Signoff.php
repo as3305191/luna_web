8e087a24397ad43c6d694e5f9a4ddbbeeee27b5f
江官駿 <as3305191@gmail.com>
2021-04-01 13:46:35 +0800

    up

--- application/controllers/api/Signoff.php
+++ application/controllers/api/Signoff.php
@@ -3,7 +3,12 @@ class Signoff extends MY_Base_Controller
 {
     public function public_sign()
     {
-        
+        // $data = array();
+		// $data = $this -> setup_user_data($data);
+		// $login_user = $this -> users_dao -> find_by_id($data['login_user_id']);
+        // if($login_user->role==''){
+
+        // }
     }
 
 }
