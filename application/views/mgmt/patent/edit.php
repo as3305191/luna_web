619cbdb3c0e35cff22cee708e07c35729c8ec82f
江官駿 <as3305191@gmail.com>
2021-04-15 17:15:16 +0800

    up

--- application/views/mgmt/patent/edit.php
+++ application/views/mgmt/patent/edit.php
@@ -411,7 +411,7 @@ $(document).ready(function() {
 				$('#patent_family').val(d.family_num);
 			}
 		});
-		$('#patnet_status_0').find('option:nth-child[0]').prop('checked','ture');
+		// $('#patnet_status_0').find('option:nth-child[0]').prop('checked','ture');
 
 	} 
 	$('#checkboxGroup input').click(function(){
@@ -1064,11 +1064,11 @@ function do_save() {
 					alert('faialure');
 				}
 		});
-		if($('#item_id').val()==0){
+		// if($('#item_id').val()==0){
 			
-			$('#patnet_status_0').find('option:nth-child[1]').prop('checked','ture');
+		// 	$('#patnet_status_0').find('option:nth-child[1]').prop('checked','ture');
 
-		} 
+		// } 
 	}	
 	load_edit_category();
 	
