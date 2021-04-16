c2e7cdcca0a9d1a733103d25cff1d06aa0cb9268
江官駿 <as3305191@gmail.com>
2021-04-16 15:48:09 +0800

    up

--- application/views/mgmt/patent/export.php
+++ application/views/mgmt/patent/export.php
@@ -98,8 +98,8 @@
 				<td><?= isset($item) ? $item -> assignee : '' ?></td>
 				<td>專利狀態</td>
 				<td colspan="2">
-					<?php if($item -> patnet_category=="3"): ?>
-						<?php if($patnet_fail_status->id !=="5"): ?>
+					<?php if($item -> patnet_category==3): ?>
+						<?php if($patnet_fail_status->id !==5): ?>
 							<?= isset($patnet_type_name) ? $patnet_type_name.'('.$patnet_fail_status->name.')' : '' ?>
 						<?php else: ?>
 							<?= isset($patnet_type_name) ? $patnet_type_name.'('.$patnet_fail_status->name.'：'.$item->patent_fail_person.')' : '' ?>
