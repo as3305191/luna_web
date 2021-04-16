da28329362e5dbbfe13bca0191b8c2e18d48b645
江官駿 <as3305191@gmail.com>
2021-04-16 16:28:59 +0800

    up

--- application/views/mgmt/patent/export.php
+++ application/views/mgmt/patent/export.php
@@ -132,11 +132,9 @@
 				<td>專利分析相關文件</td>
 				<td colspan="4"><pre style="border-style:none;background-color:transparent;color:#666666;font:14px Helvetica Neue,Helvetica,PingFang SC,\5FAE\8F6F\96C5\9ED1,Tahoma,Arial,sans-serif;">
 					<?php if(!empty($item->files) && $item->files !==''): ?>
-						<?php foreach ($item->files as $each) : ?>
-							<?= $each -> file_name.' ' ?>
-						<?php endforeach ?>
+						
+						<?= $item->files.' ' ?>
 					<?php endif?>	
-
 				</pre></td>
 			</tr>
 		</tbody>
