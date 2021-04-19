3b26710d3d2a4ba8a0f3cb3209eb77c1818f1cb5
江官駿 <as3305191@gmail.com>
2021-04-19 16:42:45 +0800

    up

--- application/views/mgmt/patent/edit.php
+++ application/views/mgmt/patent/edit.php
@@ -432,10 +432,7 @@ $(document).ready(function() {
 		}
 	});
 
-	if($('#role_id').val()==9||$('#role_id').val()==11||$('#role_id').val()==28){
-	} else{
-		$('.kv-file-zoom').attr("disabled",false);
-	}
+	$('.kv-file-zoom').attr("disabled",false);
 });
 
 $('#app-edit-form').bootstrapValidator({
