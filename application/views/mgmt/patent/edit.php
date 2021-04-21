679fb25b14c9415f65656516521ac55610d5887a
江官駿 <as3305191@gmail.com>
2021-04-21 14:12:23 +0800

    up

--- application/views/mgmt/patent/edit.php
+++ application/views/mgmt/patent/edit.php
@@ -38,15 +38,14 @@
 			</a>
 		</div>
 		<?php endif?>
-		<?php if( $login_user->role_id==9 || $login_user->role_id==11 || $login_user->role_id==28): ?>
-			<div class="widget-toolbar pull-right">
-				<div class="btn-group">
-					<button onclick="currentApp.doExportAll(<?=isset($item->id) && $item->id>0? $item -> id : ''?>)" class="btn dropdown-toggle btn-xs btn-warning" data-toggle="dropdown">
-						<i class="fa fa-save"></i>匯出
-					</button>
-				</div>
+	
+		<div class="widget-toolbar pull-right">
+			<div class="btn-group">
+				<button onclick="currentApp.doExportAll(<?=isset($item->id) && $item->id>0? $item -> id : ''?>)" class="btn dropdown-toggle btn-xs btn-warning" data-toggle="dropdown">
+					<i class="fa fa-save"></i>匯出
+				</button>
 			</div>
-		<?php endif?>
+		</div>
 	</header>
 	<!-- widget div-->
 	<div>
