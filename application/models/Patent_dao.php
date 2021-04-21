9ed2118ba8d505b322af5b425fdb4d41e773a02b
江官駿 <as3305191@gmail.com>
2021-04-21 14:52:50 +0800

    up

--- application/models/Patent_dao.php
+++ application/models/Patent_dao.php
@@ -127,13 +127,12 @@ class Patent_dao extends MY_Model {
 			$key_search = $data['key_search'];
 			$new_key_search= explode(",", str_replace(' ', ',', $key_search));
 			$new_key_sql='';
-			for($i=0;$i<=count($new_key_search);$i++){
+			for($i=0;$i<count($new_key_search);$i++){
 				if($i>0){
 					$new_key_sql+= "or _m.patent_key like %$new_key_search[$i]%";
 				}
 			}
 			$this -> db -> where("_m.patent_key like '%$new_key_search[0]%' $new_key_sql");
-
 			
 		}
 		if(!empty($data['patent_family_search'])){
