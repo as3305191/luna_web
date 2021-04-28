7fa998b6a8ecf228f2f4371d97375e3c9c65765b
江官駿 <as3305191@gmail.com>
2021-04-28 14:37:24 +0800

    up

--- application/views/mgmt/swot/export.php
+++ application/views/mgmt/swot/export.php
@@ -80,7 +80,7 @@ function line(header,line_width,line_color,line_number){
 		switch(line_number){
 			case 1:
 				ctx.moveTo(0,0);
-				ctx.lineTo(xpos/2,ypos);
+				ctx.lineTo(xpos/2,ypos*2);
 				break;
 			default:
 			return 0;	
