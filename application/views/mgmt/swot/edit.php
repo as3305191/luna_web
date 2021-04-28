e3ba2364e8480063dc15ce05db390e4dcf845f06
江官駿 <as3305191@gmail.com>
2021-04-28 09:05:25 +0800

    up

--- application/views/mgmt/swot/edit.php
+++ application/views/mgmt/swot/edit.php
@@ -53,16 +53,18 @@
 												<option value="<?= $each -> id?>"><?=  $each -> name ?></option>
 											<?php endif?>
 										<?php endforeach ?>	
-									
-
 									<?php endforeach ?>
 								</select>
 							</div>
 						</div>
 					</fieldset>
 				<?php else: ?>
-					<input type="hidden" id="department" value="0" />
+					<input type="hidden" id="department" value="<?= $login_user_role_array[0] ?>" />
 				<?php endif?>
+			<?php else: ?>
+
+				<input type="hidden" id="department" value="0" />
+
 			<?php endif?>
 			<fieldset>
 				<div class="form-group">
