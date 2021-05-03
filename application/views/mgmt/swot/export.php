c3263ff26866694635f8b868e5ea75fb866f7bf9
江官駿 <as3305191@gmail.com>
2021-05-03 08:51:34 +0800

    update

--- application/views/mgmt/swot/export.php
+++ application/views/mgmt/swot/export.php
@@ -29,7 +29,6 @@
 			</tr>
 			<tr>
 				<td ><div>sw</div><div>ot</div></td>
-
 				<td ><span valign="top">s</span><span style="border-style:none;background-color:transparent;color:#666666;font:14px Helvetica Neue,Helvetica,PingFang SC,\5FAE\8F6F\96C5\9ED1,Tahoma,Arial,sans-serif;word-wrap:break-word;white-space:pre-wrap;"><?= isset($item) ? $item -> m_swot_s : '' ?></span></td>
 				<td ><span valign="top">w</span><span style="border-style:none;background-color:transparent;color:#666666;font:14px Helvetica Neue,Helvetica,PingFang SC,\5FAE\8F6F\96C5\9ED1,Tahoma,Arial,sans-serif;word-wrap:break-word;white-space:pre-wrap;"><?= isset($item) ? $item -> m_swot_w : '' ?></span></td>
 			</tr>
