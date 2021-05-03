f29f690b2477c155147bdc16e203db8c007a237d
江官駿 <as3305191@gmail.com>
2021-05-03 09:00:10 +0800

    up

--- application/views/mgmt/swot/edit.php
+++ application/views/mgmt/swot/edit.php
@@ -276,8 +276,15 @@
 				if(data.error_msg) {
 					layer.msg(data.error_msg);
 				} else {
-					currentApp.mDtTable.ajax.reload(null, false);
-					currentApp.back(<?= isset($item) ? $item -> id : '0' ?>);
+					var this_id = <?= isset($item) ? $item -> id : '0' ?>;
+					if(this_id>0){
+						currentApp.mDtTable.ajax.reload(null, false);
+						currentApp.back(<?= isset($item) ? $item -> id : '0' ?>);
+					} else{
+						currentApp.mDtTable.ajax.reload(null, false);
+						currentApp.backTo();
+					}
+				
 				}
 			}
 		});
