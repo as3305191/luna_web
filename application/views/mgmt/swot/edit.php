b56b56d7963f4a69d9415bdc45f49d2ed199851e
江官駿 <as3305191@gmail.com>
2021-05-04 16:36:14 +0800

    up

--- application/views/mgmt/swot/edit.php
+++ application/views/mgmt/swot/edit.php
@@ -96,7 +96,7 @@
 						</select>
 					</div>
 					<?php if(!empty($login_user) ): ?>
-						<?php if($login_user->role_id==17 || $login_user->role_id==6 || $login_user->role_id==16): ?>
+						<?php if($login_user->role_id==17 || $login_user->role_id==6 || $login_user->role_id==16 || $login_user->role_id=9): ?>
 							<div class="col-md-2">
 								<button type="button" class="btn btn-sm btn-primary" id="add_swot"><i class="fa fa-plus-circle fa-lg"></i></button>
 							</div>
