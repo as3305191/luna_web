f7994602022a057940611ffaf5ad9ad3f4ea7b5b
江官駿 <as3305191@gmail.com>
2021-05-06 15:08:20 +0800

    up

--- application/controllers/mgmt/Swot.php
+++ application/controllers/mgmt/Swot.php
@@ -92,12 +92,12 @@ class Swot extends MY_Mgmt_Controller {
 				foreach($list as $each){
 					$s.= substr_replace($each->m_swot_s,$each->d_or_c_name ,'</p>', -1);
 					$w.=$each->m_swot_w;
-					$o+=$each->m_swot_o;
-					$t+=$each->m_swot_t;
-					$s_o+=$each->m_swot_s_o;
-					$w_o+=$each->m_swot_w_o;
-					$s_t+=$each->m_swot_s_t;
-					$w_t+=$each->m_swot_w_t;
+					$o.=$each->m_swot_o;
+					$t.=$each->m_swot_t;
+					$s_o.=$each->m_swot_s_o;
+					$w_o.=$each->m_swot_w_o;
+					$s_t.=$each->m_swot_s_t;
+					$w_t.=$each->m_swot_w_t;
 				}
 				$item['s'] = $s;
 				$item['w'] = $w;
