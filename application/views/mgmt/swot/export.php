031b3417c12f551b3aa64f0b80d2433e343779fa
江官駿 <as3305191@gmail.com>
2021-05-06 09:02:50 +0800

    up

--- application/views/mgmt/swot/export.php
+++ application/views/mgmt/swot/export.php
@@ -33,7 +33,7 @@ tbody tr td {
 				<td class="min100" colspan="3" style="text-align:center;vertical-align:middle;">SWOT分析表-<?= date('Ymd')?></td>
 			</tr>
 			<tr>
-				<td style=""><img src="<?= base_url("api/images/get/603")?>" style="width:100%;height:100%;object-fit:cover;" ></td>
+				<td style="position:relative;overflow:hidden;"><img src="<?= base_url("api/images/get/603")?>" style="position: absolute;top: 50%;left: 50%;display: block;min-width:100%;min-height: 100%;transform:translate(-50%,-50%);" ></td>
 				<td valign="top">
 					<table class="swot_table" width="100%" height="100%"  align="center">
 						<tr >
