9ca8974e8f045847e29d44a54c9f51f006345209
江官駿 <as3305191@gmail.com>
2021-05-06 08:43:01 +0800

    up

--- application/models/Swot_dao.php
+++ application/models/Swot_dao.php
@@ -22,7 +22,7 @@ class Swot_dao extends MY_Model {
 		// select
 		$this -> db -> select('_m.*');
 		$this -> db -> select('s_s.swot_name as s_style_name');
-		$this -> db -> select('s_t.name as s_title_name');
+		$this -> db -> select('s_t.swot_title as s_title_name');
 		$this -> db -> select('d.name as d_or_c_name');
 		// join
 		$this -> ajax_from_join();
@@ -51,7 +51,7 @@ class Swot_dao extends MY_Model {
 		// select
 		$this -> db -> select('_m.*');
 		$this -> db -> select('s_s.swot_name as s_style_name');
-		$this -> db -> select('s_t.name as s_title_name');
+		$this -> db -> select('s_t.swot_title as s_title_name');
 		$this -> db -> select('d.name as d_or_c_name');
 		// join
 		$this -> ajax_from_join();
