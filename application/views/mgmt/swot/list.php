9b27ab7a9fe844b86f995463e723c4323649578a
江官駿 <as3305191@gmail.com>
2021-05-07 17:01:53 +0800

    up

--- application/views/mgmt/swot/list.php
+++ application/views/mgmt/swot/list.php
@@ -61,17 +61,17 @@ thead tr th {
                            
 						</header>
 						<input type="hidden" name="l_user_id" id="l_user_id" value="<?= isset($login_user->role_id) ? $login_user->role_id: '' ?>" />
-
+						<div class=" pull-left">
+							<button onclick="currentApp.do_remove();" class="btn btn-xs btn-success" data-toggle="dropdown">
+								<i class="fa fa-refresh"></i>一鍵清除自己使用中檔案
+							</button>
+						</div>
 						<!-- widget div-->
 						<div>
 							<!-- widget edit box -->
 							<div class="jarviswidget-editbox">
 								<!-- This area used as dropdown edit box -->
-								<div class=" pull-left">
-									<button onclick="currentApp.do_remove();" class="btn btn-xs btn-success" data-toggle="dropdown">
-										<i class="fa fa-refresh"></i>一鍵清除自己使用中檔案
-									</button>
-								</div>
+								
 							</div>
 							<!-- end widget edit box -->
 
