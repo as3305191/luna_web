d8284466000f4cc91bc7580fa01283e21d751555
江官駿 <as3305191@gmail.com>
2021-05-07 13:42:34 +0800

    up

--- application/views/mgmt/swot/export.php
+++ application/views/mgmt/swot/export.php
@@ -129,7 +129,7 @@
 				</td>
 				<td valign="top" class="td_table" style="text-align:right;border:none;" >
 					<span class="col-sm-4" style="text-align:left;">製表：</span>
-					<span class="col-sm-4" style="text-align:right;">RA2602-1</span>
+					<div class="col-sm-4" style="text-align:right;">RA2602-1</div>
 				</td>
 			</tr>
 		</tbody>
