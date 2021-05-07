cd81fcf80e1a3c1c692fbf9e9e69c6233d636347
江官駿 <as3305191@gmail.com>
2021-05-07 16:59:26 +0800

    up

--- application/views/mgmt/swot/list.php
+++ application/views/mgmt/swot/list.php
@@ -33,7 +33,7 @@ thead tr th {
 							<div class="widget-toolbar pull-left">
 								<?php if(!empty($login_user) ): ?>
 									<?php if($login_user->role_id==17 || $login_user->role_id==6 || $login_user->role_id==16 || $login_user->role_id=9): ?>
-										<label class="col-md-3 control-label">部門:</label>
+										<label class="col-md-3 control-label">部:</label>
 										<select class="col-md-8 control-label" id="d_or_c" >
 											<option value="0">請選擇</option>
 											<option value="3">寬仕</option>
@@ -47,8 +47,7 @@ thead tr th {
 							<div class="widget-toolbar pull-left">
 								<?php if(!empty($login_user) ): ?>
 									<?php if($login_user->role_id==17 || $login_user->role_id==6 || $login_user->role_id==16 || $login_user->role_id=9): ?>
-										<label class="col-md-3 control-label">合併：</label>
-										<select class="col-md-3 control-label" id="list_title"  >
+										<select class="col-md-5 control-label" id="list_title"  >
 											<!-- option from javascript -->
 										</select>
 										<button type="button" class=" btn btn-sm btn-primary btn-group" id="add_title"><i class="fa fa-plus-circle fa-lg"></i></button>
@@ -177,7 +176,7 @@ thead tr th {
 				if(d) {
 					// console.log(d);
 					$swot_title = $('#list_title').empty();
-					$("#list_title").prepend("<option value='0'>請選擇</option>");
+					$("#list_title").prepend("<option value='0'>請選擇標題</option>");
 					$.each(d.swot, function(){
 						$('<option/>', {
 							'value': this.id,
