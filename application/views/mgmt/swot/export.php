789363baf864cf26414a929bfcc89865dcd1173a
江官駿 <as3305191@gmail.com>
2021-05-07 13:14:16 +0800

    up

--- application/views/mgmt/swot/export.php
+++ application/views/mgmt/swot/export.php
@@ -24,11 +24,11 @@ tbody tr td {
 		</thead>
 		<tbody class="">
 			<tr>
-				<td class="min100" colspan="3" style="text-align:center;vertical-align:middle;">寬仕工業股份有限公司</td>
+				<td class="min100" colspan="3" style="text-align:center;vertical-align:middle;font-weight:bold;font-size:2em;">寬仕工業股份有限公司</td>
 			</tr>
 			<tr>
 				<td class="min100" colspan="3" style="text-align:center;vertical-align:middle;">
-					<?= isset($item) ? $item -> d_or_c_name : '' ?>-<?= isset($item) ? $item -> s_title_name : '' ?>-<?= date('Ymd')?>
+					<span style="text-align:center;vertical-align:middle;font-weight:bold;font-size:60px;"><?= isset($item) ? $item -> d_or_c_name : '' ?>-<?= isset($item) ? $item -> s_title_name : '' ?>-<?= date('Ymd')?></span>
 					<div style="text-align:right;">文件種類：<?= isset($item) ? $item -> s_style_name : ''?></div>
 				</td>
 			</tr>
