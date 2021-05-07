56baf0dd7d53df916f706e1ba886422e1f3c5200
江官駿 <as3305191@gmail.com>
2021-05-07 16:19:36 +0800

    up

--- application/controllers/mgmt/Swot.php
+++ application/controllers/mgmt/Swot.php
@@ -41,7 +41,7 @@ class Swot extends MY_Mgmt_Controller {
 			if(!empty($each->class_id)){
 				$d_or_c_level = $this-> d_dao-> find_by_id($each->class_id);
 				if($d_or_c_level -> level==4){
-					$each->department_list = $this-> d_dao-> find_by_id($d_or_c_level->id);
+					$each->department_list = $this-> d_dao-> find_by_id($d_or_c_level->parent_id);
 				}
 			}
 
