8ad894e777b3b9d4ee1096cef47ac927a0cb03b6
江官駿 <as3305191@gmail.com>
2021-05-10 14:21:43 +0800

    up

--- js/plugin/ckeditor/plugins/font/plugin.js
+++ js/plugin/ckeditor/plugins/font/plugin.js
@@ -221,8 +221,8 @@ CKEDITOR.config.font_names = 'Arial/Arial, Helvetica, sans-serif;' +
 	'Lucida Sans Unicode/Lucida Sans Unicode, Lucida Grande, sans-serif;' +
 	'Tahoma/Tahoma, Geneva, sans-serif;' +
 	'Times New Roman/Times New Roman, Times, serif;' +
-	'Trebuchet MS/Trebuchet MS, Helvetica, sans-serif;Verdana/Verdana, Geneva, sans-serif;新細明體;標楷體;微軟正黑體';
-	
+	'Trebuchet MS/Trebuchet MS, Helvetica, sans-serif;' +
+	'Verdana/Verdana, Geneva, sans-serif;新細明體;標楷體;微軟正黑體';
 
 
 /**
