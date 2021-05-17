585b930cc43ca557923f6a72c0f72e3f277dbbf3
江官駿 <as3305191@gmail.com>
2021-05-17 08:48:56 +0800

    up

--- application/views/mgmt/swot/export.php
+++ application/views/mgmt/swot/export.php
@@ -12,8 +12,8 @@
 
 @page {
   /* size: portrait; 直向 */
-  size: landscape; 
-  size: A4; /* 紙張大小 */
+  /* size: landscape;  */
+  /* size: A4; 紙張大小 */
   /* size: A4 portrait; 混合使用 */
   
   margin: 0; /* 邊界與內容的距離 */
