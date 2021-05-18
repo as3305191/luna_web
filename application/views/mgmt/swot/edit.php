16ccddb1405f6acf15788d724bc7a5c9c9145a1c
江官駿 <as3305191@gmail.com>
2021-05-18 17:28:39 +0800

    up

--- application/views/mgmt/swot/edit.php
+++ application/views/mgmt/swot/edit.php
@@ -21,7 +21,8 @@
 
 			<?php if(!empty($item -> id) && $item -> is_use==1 ): ?>
 				<?php if($item -> is_use_user_id== $login_user->id): ?>
-					<?php if(!empty($item -> is_lock) && $item -> is_lock!==1 ): ?>
+					<?php if(!empty($item -> is_lock) && $item -> is_lock==1 ): ?>
+					<?php else: ?>
 						<div class="widget-toolbar pull-left">
 							<a href="javascript:void(0);"  onclick="do_save();" class="btn btn-default btn-danger">
 								<i class="fa fa-save"></i>存檔
