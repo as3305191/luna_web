90885af007e18b5d633beece5f45952ccba722cd
江官駿 <as3305191@gmail.com>
2021-05-18 15:15:50 +0800

    up

--- application/views/mgmt/swot/edit.php
+++ application/views/mgmt/swot/edit.php
@@ -209,13 +209,6 @@
 							<!-- option from javascript -->
 						</select>
 					</div>
-					<?php if(!empty($login_user) ): ?>
-						<?php if($login_user->role_id==17 || $login_user->role_id==6 || $login_user->role_id==16 || $login_user->role_id=9): ?>
-							<div class="col-md-2">
-								<button type="button" class="btn btn-sm btn-primary" id="add_swot"><i class="fa fa-plus-circle fa-lg"></i></button>
-							</div>
-						<?php endif?>
-					<?php endif?>
 				</div>
 			</fieldset>
 			<fieldset>
