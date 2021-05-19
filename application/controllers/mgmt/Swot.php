03cbae3c065c1f8e1a49c10c3a1c3ee4808aec95
江官駿 <as3305191@gmail.com>
2021-05-19 11:55:07 +0800

    up

--- application/controllers/mgmt/Swot.php
+++ application/controllers/mgmt/Swot.php
@@ -369,11 +369,11 @@ class Swot extends MY_Mgmt_Controller {
 		$new_str='';
 		for($i=0;$i<=$total_num;$i++){
 			$strbetween_p_p = $total_array[$i];
-			$check = strstr($strbetween_p_p,'<span');
+			$check = strstr($strbetween_p_p,'style');
 			if($check>0){
-				$the_num_in_p_p = $this->get_between($strbetween_p_p, '">', '.');
+				$the_num_in_p_p = $this->get_between($strbetween_p_p, 'style', '.');
 				$j=$i+1;
-				$new_str.=str_replace('&nbsp;','',str_replace('">'.$the_num_in_p_p.'.','">'.$j.'.',trim($strbetween_p_p)));
+				$new_str.=str_replace('&nbsp;','',str_replace('>'.$the_num_in_p_p.'.','>'.$j.'.',trim($strbetween_p_p)));
 			}else{
 				$the_num_in_p_p = $this->get_between($strbetween_p_p, '<p>', '.');
 				$j=$i+1;
