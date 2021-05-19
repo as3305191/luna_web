0ca12eadeed521947dbcbe4f8edb7b4007342a1f
江官駿 <as3305191@gmail.com>
2021-05-19 12:28:29 +0800

    up

--- application/views/mgmt/swot/edit.php
+++ application/views/mgmt/swot/edit.php
@@ -520,23 +520,23 @@
 			success: function(d) {
 				if(d) {
 				
-					CKEDITOR.instances.m_swot_s.empty();
-					CKEDITOR.instances.m_swot_w.empty();
-					CKEDITOR.instances.m_swot_o.empty();
-					CKEDITOR.instances.m_swot_t.empty();
-					CKEDITOR.instances.m_swot_s_o.empty();
-					CKEDITOR.instances.m_swot_w_o.empty();
-					CKEDITOR.instances.m_swot_s_t.empty();
-					CKEDITOR.instances.m_swot_w_t.empty();
+					CKEDITOR.instances.m_swot_s.setData('');
+					CKEDITOR.instances.m_swot_w.setData('');
+					CKEDITOR.instances.m_swot_o.setData('');
+					CKEDITOR.instances.m_swot_t.setData('');
+					CKEDITOR.instances.m_swot_s_o.setData('');
+					CKEDITOR.instances.m_swot_w_o.setData('');
+					CKEDITOR.instances.m_swot_s_t.setData('');
+					CKEDITOR.instances.m_swot_w_t.setData('');
 
-					CKEDITOR.instances.m_swot_s.text(d.new_swot_s);
-					CKEDITOR.instances.m_swot_w.text(d.new_swot_w);
-					CKEDITOR.instances.m_swot_o.text(d.new_swot_o);
-					CKEDITOR.instances.m_swot_t.text(d.new_swot_t);
-					CKEDITOR.instances.m_swot_s_o.text(d.new_swot_s_o);
-					CKEDITOR.instances.m_swot_w_o.text(d.new_swot_w_o);
-					CKEDITOR.instances.m_swot_s_t.text(d.new_swot_s_t);
-					CKEDITOR.instances.m_swot_w_t.text(d.new_swot_w_t);
+					CKEDITOR.instances.m_swot_s.setData(d.new_swot_s);
+					CKEDITOR.instances.m_swot_w.setData(d.new_swot_w);
+					CKEDITOR.instances.m_swot_o.setData(d.new_swot_o);
+					CKEDITOR.instances.m_swot_t.setData(d.new_swot_t);
+					CKEDITOR.instances.m_swot_s_o.setData(d.new_swot_s_o);
+					CKEDITOR.instances.m_swot_w_o.setData(d.new_swot_w_o);
+					CKEDITOR.instances.m_swot_s_t.setData(d.new_swot_s_t);
+					CKEDITOR.instances.m_swot_w_t.setData(d.new_swot_w_t);
 	
 				}
 			},
