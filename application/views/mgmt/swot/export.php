07aadf91544bdca1d826ae1207363886919e5074
江官駿 <as3305191@gmail.com>
2021-05-20 13:22:13 +0800

    UP

--- application/views/mgmt/swot/export.php
+++ application/views/mgmt/swot/export.php
@@ -58,7 +58,7 @@
 </style>
 <!-- Widget ID (each widget will need unique ID)-->
 <div class="ex_page " style="padding:20px 20px 20px 30px;">
-	<table class="layui-table pageDiv layui-table1" width="70%"  style="border-color:#000;" >
+	<table class="layui-table pageDiv layui-table1" width="70%"  style="border-color:#000;padding:20px 20px 20px 20px;" >
 		<thead>
 		</thead>
 		<tbody class="">
