785965ebb61a27930442d38427a8839ceba47624
江官駿 <as3305191@gmail.com>
2021-05-24 15:20:03 +0800

    up

--- js/app/swot/list.js
+++ js/app/swot/list.js
@@ -33,7 +33,7 @@ var SwotAppClass = (function(app) {
 			$('.tab-pane').removeClass('active');
 			$('#edit_page').addClass('active');	
 				$('#edit-modal-body').load(baseUrl + app.basePath + 'edit/' + id+'?title='+title+'&style='+style, function(){
-					// window.location.hash = app.basePath + 'edit/' + id;
+					window.location.hash = app.basePath + 'edit/' + id;
 					$("#btn-submit-edit").prop( "disabled", false);
 					loading.remove();
 
