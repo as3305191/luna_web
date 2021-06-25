84638b18554a26a42ba96dee11d8a31f00f5fe88
江官駿 <as3305191@gmail.com>
2021-06-25 11:58:27 +0800

    up

--- application/views/mgmt/swot/export.php
+++ application/views/mgmt/swot/export.php
@@ -10,6 +10,45 @@
 <meta http-equiv="expires" content="0">
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
+<!-- <link rel="stylesheet" type="text/css" media="all" href="<?= base_url('css/bootstrap.min.css') ?>"> -->
+<link rel="stylesheet" type="text/css" media="all" href="<?= base_url('css/font-awesome.min.css') ?>">
+
+<!-- bootstrap fileinput -->
+<link href="<?= base_url('js/plugin/bootstrap-fileinput/css/fileinput.css') ?>" media="all" rel="stylesheet" type="text/css" />
+
+<!-- bootstrap datetimepicker -->
+<link href="<?= base_url('js/plugin/bootstrap-datetimepicker/css/bootstrap-datetimepicker.min.css') ?>" media="all" rel="stylesheet" type="text/css" />
+
+<!-- datatables -->
+<link href="<?= base_url('js/plugin/datatable-scroller/css/scroller.bootstrap.min.css') ?>" media="all" rel="stylesheet" type="text/css" />
+
+<!--  -->
+<link rel="stylesheet" type="text/css" media="all" href="<?= base_url('css/smartadmin-production-plugins.min.css') ?>">
+<link rel="stylesheet" type="text/css" media="all" href="<?= base_url('css/smartadmin-production.min.css') ?>">
+<link rel="stylesheet" type="text/css" media="all" href="<?= base_url('css/smartadmin-skins.min.css') ?>">
+
+<!-- SmartAdmin RTL Support -->
+<link rel="stylesheet" type="text/css" media="screen" href="<?= base_url('css/smartadmin-rtl.min.css') ?>">
+
+<link rel="stylesheet" type="text/css" media="screen" href="<?= base_url('css/my.css') ?>">
+
+<!-- #FAVICONS -->
+<link rel="icon" type="image/png" href="<?= base_url('img/ktx_img/logo_1.png')  ?>" />
+
+<!-- #APP SCREEN / ICONS -->
+<!-- Specifying a Webpage Icon for Web Clip
+Ref: https://developer.apple.com/library/ios/documentation/AppleApplications/Reference/SafariWebContent/ConfiguringWebApplications/ConfiguringWebApplications.html -->
+<link rel="apple-touch-icon" href="<?= base_url('img/splash/sptouch-icon-iphone.png')?>">
+<link rel="apple-touch-icon" sizes="76x76" href="<?= base_url('img/splash/touch-icon-ipad.png') ?>">
+<link rel="apple-touch-icon" sizes="120x120" href="<?= base_url('img/splash/touch-icon-iphone-retina.png') ?>">
+<link rel="apple-touch-icon" sizes="152x152" href="<?= base_url('img/splash/touch-icon-ipad-retina.png') ?>">
+
+
+<!-- iOS web-app metas : hides Safari UI Components and Changes Status Bar Appearance -->
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="black">
+
+
 <link rel="stylesheet" href="<?= base_url('js/layui/dist/css/layui.css') ?>">
 <style>
 .layui-table1{ 
