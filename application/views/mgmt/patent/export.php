a5a10ccca5e5eccf839e205d722bf3ca0dc5c301
江官駿 <as3305191@gmail.com>
2021-06-25 11:35:10 +0800

    up

--- application/views/mgmt/patent/export.php
+++ application/views/mgmt/patent/export.php
@@ -127,12 +127,12 @@
 			</tr>
 			<tr>
 				<td>專利範圍</td>
-				<td colspan="4"><span style="border-style:none;background-color:transparent;color:#666666;font:14px Helvetica Neue,Helvetica,PingFang SC,\5FAE\8F6F\96C5\9ED1,Tahoma,Arial,sans-serif;word-wrap:break-word;white-space:pre-wrap;"><?= isset($item) ? $item -> patent_range : '' ?></span></td>
+				<td colspan="4"><span style="border-style:none;color:#666666;font:14px Helvetica Neue,Helvetica,PingFang SC,\5FAE\8F6F\96C5\9ED1,Tahoma,Arial,sans-serif;word-wrap:break-word;white-space:pre-wrap;"><?= isset($item) ? $item -> patent_range : '' ?></span></td>
 
 			</tr>
 			<tr>
 				<td>專利分析相關文件</td>
-				<td colspan="4" style="background-color:transparent;color:#666666;font:14px Helvetica Neue,Helvetica,PingFang SC,\5FAE\8F6F\96C5\9ED1,Tahoma,Arial,sans-serif;">
+				<td colspan="4" style="color:#666666;font:14px Helvetica Neue,Helvetica,PingFang SC,\5FAE\8F6F\96C5\9ED1,Tahoma,Arial,sans-serif;">
 					<?php if(!empty($item->files) && $item->files !==''): ?>
 						<?php foreach ($item->files as $each) : ?>
 							<?= isset($each -> file_name) ? $each -> file_name: '' ?>
