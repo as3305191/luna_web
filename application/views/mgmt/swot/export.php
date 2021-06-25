0d075ec842477bb835d17965a4eacff75d39d66d
江官駿 <as3305191@gmail.com>
2021-06-25 11:57:16 +0800

    up

--- application/views/mgmt/swot/export.php
+++ application/views/mgmt/swot/export.php
@@ -1,5 +1,16 @@
 
-<?php $this->load->view('layout/head'); ?>
+<meta charset="utf-8">
+<meta name="description" content="">
+<meta name="author" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<!-- <meta name=”google-site-verification” content=”4AWlkYHST6nMramtRF-oITkJO3oojQ5pZLS_YbiocQU” /> -->
+
+<meta http-equiv="pragma" content="no-cache">
+<meta http-equiv="cache-control" content="no-cache">
+<meta http-equiv="expires" content="0">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+<link rel="stylesheet" href="<?= base_url('js/layui/dist/css/layui.css') ?>">
 <style>
 .layui-table1{ 
 	/* border-style:groove !important; */
