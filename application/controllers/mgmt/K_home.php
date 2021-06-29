55a18ce61043e2d9da7ca1a1fc55a8fcef098d71
江官駿 <as3305191@gmail.com>
2021-06-29 15:15:42 +0800

    up

---
+++ application/controllers/mgmt/K_home.php
@@ -0,0 +1,192 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class K_home extends MY_Mgmt_Controller {
+
+	function __construct() {
+		parent::__construct();
+		$this -> load -> model('Users_dao', 'dao');
+		$this -> load -> model('Images_dao', 'img_dao');
+		$this -> load -> model('Department_dao','d_dao');
+	}
+
+	public function index()
+	{
+		$data = array();
+		$data = $this -> setup_user_data($data);
+		$data['role_list'] = $this -> dao -> find_all_department();
+		$data['login_user'] = $this -> dao -> find_by_id($data['login_user_id']);
+		// $this -> to_json($data);
+		$this->load->view('mgmt/k_home/list', $data);
+	}
+	public function get_data() {
+		$res = array();
+		$data = $this -> get_posts(array(
+			'length',
+			'start',
+			'columns',
+			'search',
+			'order',
+		));
+
+		$s_data = $this -> setup_user_data(array());
+		$login_user = $this -> dao -> find_by_id($s_data['login_user_id']);
+		$items = $this -> dao -> query_ajax($data);
+		if(!empty($items->in_departant)){
+			$items->in_departants = explode(",", str_replace('#', ',', $items->in_departant));
+		}
+		$res['items'] = $items;
+		$res['recordsFiltered'] = $this -> dao -> count_ajax($data);
+		$res['recordsTotal'] = $this -> dao -> count_all_ajax($data);
+		$this -> to_json($res);
+	}
+
+	public function edit($id) {
+		$data = array();
+		$data['id'] = $id;
+		if(!empty($id)) {
+			$q_data = $this -> get_posts(array(
+				'length',
+				'start',
+				'columns',
+				'search',
+				'order',
+			));
+			
+			$q_data['id'] = $id;
+			$list = $this -> dao -> query_ajax($q_data);
+			$item = $list[0];
+			if($item->role_id>0){
+				$department = $this -> d_dao -> find_by_id($item->role_id);
+				if($department->level==4){
+					$item->new_department_id = $department->parent_id;
+					$item->div_id = $department->id;
+					$data['div_list'] = $this -> dao -> find_all_div($department->parent_id);
+				} else{
+					$item->department_id = $department->id;
+				}
+			} else{
+				$item->department_id = 0;
+			}
+			if(!empty($item->in_department)){
+				$item->in_departments = explode(",", str_replace('#', ',', trim($item->in_department, "#")));
+
+			} else{
+				$item->department_id = 0;
+			}
+			$data['item'] = $item;
+		}
+
+		$s_data = $this -> setup_user_data(array());
+		$login_user = $this -> dao -> find_by_id($s_data['login_user_id']);
+		$data['login_user'] = $login_user;
+		$data['department_list'] = $this -> dao -> find_all_department();
+		// $this -> to_json($data);
+
+		$this->load->view('mgmt/users/edit', $data);
+	}
+
+	public function find_div_by_department() {
+		$department = $this -> get_post('department');
+		if($department>0){
+			$list = $this -> d_dao -> find_all_by('parent_id', $department);
+		}
+		$res = array();
+		$res['div_list'] = $list;
+		$this -> to_json($res);
+	}
+
+	public function insert() {
+		$res = array();
+		$id = $this -> get_post('id');
+		$data = $this -> get_posts(array(
+			'account',
+			'password',
+			'user_name',
+			
+		));
+		$role_id = $this -> get_post('role_id');
+		$div_id = $this -> get_post('div_id');
+		$department = $this -> get_post('in_department');
+		if(!empty($department)){
+			$new_department = str_replace(',', '#',implode(',',$department));
+			if($new_department=='##'){
+				$new_department=='0';
+				$data['in_department'] = $new_department;
+			} else{
+				if(substr($new_department,-1)=="#" && substr($new_department,0,1)=="#"){
+					$data['in_department'] = $new_department;
+				} else{
+					$data['in_department'] = "#".$new_department."#";
+				}
+			}
+		}
+		if(empty($id)) {
+			// insert
+			if(!empty($div_id)&&$div_id>0){
+				$data['role_id'] = $div_id;
+			} else{
+				$data['role_id'] = $role_id;
+			}
+			$this -> dao -> insert($data);
+		} else {
+			if(!empty($div_id)&&$div_id>0){
+				$data['role_id'] = $div_id;
+			} else{
+				$data['role_id'] = $role_id;
+			}
+			$this -> dao -> update($data, $id);
+		}
+		$res['success'] = TRUE;
+ 		$this -> to_json($res);
+	}
+
+	public function delete($id) {
+		$res['success'] = TRUE;
+		$this -> dao -> delete($id);
+		$this -> to_json($res);
+	}
+
+
+	public function check_account($id) {
+		$account = $this -> get_post('account');
+		$item = $this -> dao -> find_by("account", $account);
+		$res = array();
+		if(!empty($id)) {
+			if (!empty($item)) {
+				if($item -> id == $id) {
+					$res['valid'] = TRUE;
+				} else {
+					$res['valid'] = FALSE;
+				}
+				$res['item'] = $item;
+			} else {
+				$res['valid'] = TRUE;
+			}
+		} else { // create
+			if (!empty($item)) {
+				$res['valid'] = FALSE;
+			} else {
+				$res['valid'] = TRUE;
+			}
+		}
+
+		$this -> to_json($res);
+	}
+
+	public function check_code() {
+		$code = $this -> get_post('intro_code');
+		$list = $this -> dao -> find_all_by('code', $code);
+		$res = array();
+		$res['valid'] = (count($list) > 0);
+		$this -> to_json($res);
+	}
+
+	public function chg_user() {
+		$user_id = $this -> get_post('user_id');
+		$this -> session -> set_userdata('user_id', $user_id);
+		$res = array();
+
+		$this -> to_json($res);
+	}
+}
