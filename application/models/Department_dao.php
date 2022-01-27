93b4e34680275a8fdb5d773b660daac615655b11
pony <as3305191@gmail.com>
2022-01-27 10:21:57 +0800

    up

--- application/models/Department_dao.php
+++ application/models/Department_dao.php
@@ -194,6 +194,15 @@ class Department_dao extends MY_Model {
 
 		return $list;
 	}
+	function find_bfl_d_or_c(){
+		$this -> db -> from($this -> table_name);
+		$this -> db -> where('level =','5');
+		$this -> db -> where('34< id < 52');
+		$this -> db -> order_by('pos','asc');
+		$list = $this -> db -> get() -> result();
+
+		return $list;
+	}
 	
 }
 ?>
