e3519266a77cae351ede0b9475df2780b36ee897
pony <as3305191@gmail.com>
2022-02-09 16:40:18 +0800

    up

--- application/models/Swot_dao.php
+++ application/models/Swot_dao.php
@@ -91,8 +91,8 @@ class Swot_dao extends MY_Model {
 			$title = $data['title'];
 			$this -> db -> where("_m.title",$title);
 		} 
-		if(!empty($data['style'])){
-			$list_style = $data['style'];
+		if(!empty($data['list_style'])){
+			$list_style = $data['list_style'];
 			$this -> db -> where("_m.swot_style_id",$list_style);
 		}
 		if(!empty($data['id'])){
