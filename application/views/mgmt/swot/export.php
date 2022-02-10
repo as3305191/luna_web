cc6ebe4059a0cb5d7e3c9cd42ca4b141f2a6cbd4
pony <as3305191@gmail.com>
2022-02-10 12:05:24 +0800

    up

--- application/views/mgmt/swot/export.php
+++ application/views/mgmt/swot/export.php
@@ -373,5 +373,5 @@ Ref: https://developer.apple.com/library/ios/documentation/AppleApplications/Ref
 	<?php endif?>
 </div>
 <script>
-	document.getElementsByTagName("html")[0].style.backgroundColor="#fff";
+	document.getElementsByTagName("div")[0].style.backgroundColor="#fff";
 </script>
