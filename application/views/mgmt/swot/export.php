c735c42a04ad52f097b13c35a680e1ae97b8a6a5
pony <as3305191@gmail.com>
2022-02-17 08:26:59 +0800

    up

--- application/views/mgmt/swot/export.php
+++ application/views/mgmt/swot/export.php
@@ -128,7 +128,7 @@ Ref: https://developer.apple.com/library/ios/documentation/AppleApplications/Ref
 					<td colspan="1" class="noBreak td_table col-xs-4">
 						<table class="swot_table" width="100%" height="100%"  align="center">
 							<tr >
-								<td style="border:none"><img src="<?= base_url("api/images/get/947")?>" style="min-width:50%;min-height: 50%;-webkit-background-size:cover;-moz-background-size: cover;-o-background-size: cover;background-size: cover;" ></td>
+								<td style="border:none"><img src="<?= base_url("api/images/get/947")?>" style="min-width:50%;height: 50%;-webkit-background-size:cover;-moz-background-size: cover;-o-background-size: cover;background-size: cover;" ></td>
 							</tr>
 						</table>
 					</td>
