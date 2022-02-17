588f2e1424f62f1555e3a5fa14ce7516018be960
pony <as3305191@gmail.com>
2022-02-17 08:33:21 +0800

    up

--- application/views/mgmt/swot/export.php
+++ application/views/mgmt/swot/export.php
@@ -78,11 +78,10 @@ Ref: https://developer.apple.com/library/ios/documentation/AppleApplications/Ref
 	*{
 		-webkit-print-color-adjust: exact !important;
 		/* background-color: #fff !important; */
-		position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
+		top: 0;
+		right: 0;
+		bottom: 0;
+		left: 0;
 	}
 
 	.layui-table1{ 
