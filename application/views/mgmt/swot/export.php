349d7f8085739c318420e82cb6003611633927aa
pony <as3305191@gmail.com>
2022-02-18 16:07:39 +0800

    up

--- application/views/mgmt/swot/export.php
+++ application/views/mgmt/swot/export.php
@@ -101,7 +101,7 @@
 }
 </style>
 <!-- Widget ID (each widget will need unique ID)-->
-<div class="ex_page" style="padding:40px 40px 0px 40px;overflow: auto;background-color:#fff;" height="100%" width="70%">
+<div class="ex_page" style="padding:40px 40px 0px 30px;overflow: auto;background-color:#fff;" height="90%" width="70%">
 
 <?php if(!empty( $item -> unify) && $item -> unify>0 && $item -> class_id==3): ?>
 
