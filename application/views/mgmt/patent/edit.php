653fa0611adff40ab79c73e52d899186fe479ee2
pony <as3305191@gmail.com>
2022-04-11 17:06:11 +0800

    up

--- application/views/mgmt/patent/edit.php
+++ application/views/mgmt/patent/edit.php
@@ -198,8 +198,8 @@
 					<div class="col-md-1">
 						<button type="button" class="btn btn-sm btn-primary" id="add_key"><i class="fa fa-plus-circle fa-lg"></i></button>
 					</div>
-					<div class="col-md-2">
-						<button type="button" class="btn btn-sm btn-primary" id="add_key" style="color:red"><i class="fa fa-minus-circle fa-lg"></i></button>
+					<div class="col-md-1" style="color:red">
+						<button type="button" class="btn btn-sm btn-primary" id="minus_key" ><i class="fa fa-minus-circle fa-lg"></i></button>
 					</div>
 				</div>
 			</fieldset>
