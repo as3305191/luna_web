642ff40e1393fde3dc362b2e20c13043bd767a5f
pony <as3305191@gmail.com>
2022-06-14 14:29:22 +0800

    up

--- application/views/mgmt/patent/edit.php
+++ application/views/mgmt/patent/edit.php
@@ -1221,10 +1221,10 @@ function do_save() {
 					// console.log(d);
 					$patent_key = $('#patent_key_array').empty();
 					option ='';
-					if(patent_key_array[0]!==''){
+					if(patent_key_array[0].length>0){
 						$new_key_array = patent_key_array[0].split(",");
 					} else{
-						$new_key_array =[''];
+						$new_key_array =[];
 					}
 					$.each(d.key, function(){
 						if($new_key_array.indexOf(this.id)>=0){
