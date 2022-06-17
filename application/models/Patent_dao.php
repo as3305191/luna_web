7aa059850f646322f91dcb0b9514a7bcf7afb357
pony <as3305191@gmail.com>
2022-06-17 14:48:39 +0800

    up

--- application/models/Patent_dao.php
+++ application/models/Patent_dao.php
@@ -71,7 +71,7 @@ class Patent_dao extends MY_Model {
 
 		// query results
 		$query = $this -> db -> get();
-		return $query -> row();
+		return $query -> result();
 	}
 
 	function query_ajax_export($data) {
