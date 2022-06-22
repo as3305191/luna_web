5c8853ef4ebfbf281752d73bd1d9c4292984a686
pony <as3305191@gmail.com>
2022-06-22 15:55:32 +0800

    up

--- application/models/Users_dao.php
+++ application/models/Users_dao.php
@@ -423,17 +423,10 @@ class Users_dao extends MY_Model {
 		$this -> load -> model('Nav_dao', 'nav_dao');
 		$lv1_list = $this -> nav_dao -> find_all_by_parent_id(0);
 		$sub_list = $this -> nav_dao -> find_all_not_lv1();
-		$new_role_id = explode(",", $role_id);
-		if (count($new_role_id)>1){
-			$sql = "select  * from role_power where role_id = ($new_role_id[0])";
-			for($i=1;$i<count($new_role_id);$i++){
-				$sql .= "or role_id = {$new_role_id[$i]}";
-			}
-			$rp_list = $this -> db -> query($sql) -> result();
-		} else{
-			$sql = "select * from role_power where role_id = {$role_id}";
-			$rp_list = $this -> db -> query($sql) -> result();
-		}
+
+		$sql = "select * from role_power where role_id = {$role_id}";
+		$rp_list = $this -> db -> query($sql) -> result();
+
 
 		
 
