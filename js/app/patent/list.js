f87a43953b5a8879cd11fef5ce918d2e800e20f8
pony <as3305191@gmail.com>
2022-06-22 16:44:19 +0800

    up

--- js/app/patent/list.js
+++ js/app/patent/list.js
@@ -81,7 +81,7 @@ var patentAppClass = (function(app) {
 			app.tableReload();
 		});
 
-		$('#key_search_array').on("select2:selecting", function(){
+		$('#key_search_array').select2().on('change', function(){
 			app.tableReload();
 		});
 	    // $(window).trigger("hashchange");
