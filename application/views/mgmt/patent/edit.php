d7940cc64c7c24cdc5e6b1019b1052a0713f6a57
pony <as3305191@gmail.com>
2022-11-04 08:43:47 +0800

    up

--- application/views/mgmt/patent/edit.php
+++ application/views/mgmt/patent/edit.php
@@ -118,6 +118,7 @@
 				<div class="form-group">
 					<label class="col-md-3 control-label"></label>
 					<div class="col-md-6">
+					<input type="text"  class="form-control" name="same_family_id"  id="same_family_id" value="<?= isset($same_family_id) ? $same_family_id: 0 ?>"/>
 						<?php if(!empty($same_family)): ?>
 							<?php foreach($same_family as $each): ?>
 								<?php if(!empty($each -> patnet_num)&& $each -> patnet_num !==""): ?>
@@ -510,10 +511,8 @@ if($('#item_id').val()>0){
 	img=[];
 	now_patent_status = [];
 	same_family_id = '<?php $same_family_id?>';
-	// $.each(<?php $same_family_id?>, function(){
-	// 	same_family_id.push(this.id);	
-	// });
-	console.log(same_family_id);
+
+	// console.log(same_family_id);
 	if($('#img_id').val().length>0){
 		img.push($('#img_id').val());
 		// pdf_array.splice($.inArray(0,pdf_array),1);
