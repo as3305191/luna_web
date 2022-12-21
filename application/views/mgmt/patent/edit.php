953d7f0b4911982aa0e1a163cb1a68ad77d9d2e8
pony <as3305191@gmail.com>
2022-12-21 13:31:48 +0800

    up

--- application/views/mgmt/patent/edit.php
+++ application/views/mgmt/patent/edit.php
@@ -399,7 +399,7 @@
 				<div class="form-group">
 					<label class="col-md-3 control-label">優先權</label>
 					<div class="col-md-6">
-						<div type="text" contenteditable="true" class="form-control" name="p_id_for_priority"  id="p_id_for_priority" onclick="do_search_patent_num(1)" <?= $login_user->role_id==9 || $login_user->role_id==11 || $login_user->role_id==28? '': 'readonly' ?>>
+						<div contenteditable="true" class="form-control" name="p_id_for_priority"  id="p_id_for_priority" onclick="do_search_patent_num(1)" <?= $login_user->role_id==9 || $login_user->role_id==11 || $login_user->role_id==28? '': 'readonly' ?>>
 						</div>
 					</div>
 				</div>
@@ -408,7 +408,7 @@
 				<div class="form-group">
 					<label class="col-md-3 control-label">連續案</label>
 					<div class="col-md-6">
-						<div type="text" contenteditable="true" class="form-control" name="p_id_for_continuous_cases"  id="p_id_for_continuous_cases" onclick="do_search_patent_num(2)" <?= $login_user->role_id==9 || $login_user->role_id==11 || $login_user->role_id==28? '': 'readonly' ?>>
+						<div contenteditable="true" class="form-control" name="p_id_for_continuous_cases"  id="p_id_for_continuous_cases" onclick="do_search_patent_num(2)" <?= $login_user->role_id==9 || $login_user->role_id==11 || $login_user->role_id==28? '': 'readonly' ?>>
 						</div>
 					</div>
 				</div>
@@ -417,7 +417,7 @@
 				<div class="form-group">
 					<label class="col-md-3 control-label">部分連續案</label>
 					<div class="col-md-6">
-						<div type="text" contenteditable="true" class="form-control" name="p_id_for_part_continuous_cases"  id="p_id_for_part_continuous_cases" onclick="do_search_patent_num(3)" <?= $login_user->role_id==9 || $login_user->role_id==11 || $login_user->role_id==28? '': 'readonly' ?>>
+						<div contenteditable="true" class="form-control" name="p_id_for_part_continuous_cases"  id="p_id_for_part_continuous_cases" onclick="do_search_patent_num(3)" <?= $login_user->role_id==9 || $login_user->role_id==11 || $login_user->role_id==28? '': 'readonly' ?>>
 						</div>
 					</div>
 				</div>
@@ -426,7 +426,7 @@
 				<div class="form-group">
 					<label class="col-md-3 control-label">分割案</label>
 					<div class="col-md-6">
-						<div type="text" contenteditable="true" class="form-control" name="p_id_for_split_case"  id="p_id_for_split_case" onclick="do_search_patent_num(4)" <?= $login_user->role_id==9 || $login_user->role_id==11 || $login_user->role_id==28? '': 'readonly' ?>>
+						<div contenteditable="true" class="form-control" name="p_id_for_split_case"  id="p_id_for_split_case" onclick="do_search_patent_num(4)" <?= $login_user->role_id==9 || $login_user->role_id==11 || $login_user->role_id==28? '': 'readonly' ?>>
 						</div>
 					</div>
 				</div>
@@ -1288,7 +1288,7 @@ function do_save() {
 											break;
 										}else{
 											$('#p_id_for_priority').text(me.patnet_num);
-											break;
+											break;w
 										}										
 									}else
 									if(me.public_num){
