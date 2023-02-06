df928c84a54b9ec9febf23836707632a70968cf0
pony <as3305191@gmail.com>
2023-02-06 13:58:50 +0800

    up

--- application/views/mgmt/patent_key/list.php
+++ application/views/mgmt/patent_key/list.php
@@ -61,7 +61,7 @@ thead tr th {
 									<thead>
 										<tr>
 											
-											<th class="min10"></th>
+											<th class="min30"></th>
 											<th class="min100">關鍵字</th>
 										
 										</tr>
