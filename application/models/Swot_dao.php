72a5fc403af496ded21002f481809417acb9302c
pony <as3305191@gmail.com>
2023-03-08 08:12:54 +0800

    up

--- application/models/Swot_dao.php
+++ application/models/Swot_dao.php
@@ -127,12 +127,13 @@ class Swot_dao extends MY_Model {
 			$this -> db -> where("_m.role_id",$d_or_c);
 			$this -> db -> where("_m.swot_style_id",$list_style);
 			$this -> db -> where("_m.title",$list_title);
-			$this -> db -> or_group_start();
+			$this -> db -> group_end();
+			$this -> db -> group_start();
 			$this -> db -> where("_m.class_id",$d_or_c);
 			$this -> db -> where("_m.swot_style_id",$list_style);
 			$this -> db -> where("_m.title",$list_title);
 			$this -> db -> group_end();
-			$this -> db -> group_end();
+			
 		}
 		if(!empty($data['dep'])){
 			$dep = $data['dep'];
