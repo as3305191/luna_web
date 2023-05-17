a9cb97adf3cf33fe2a8a4e6c337d1321f2b6ef3a
pony <as3305191@gmail.com>
2023-05-17 11:46:18 +0800

    up

--- js/app.js
+++ js/app.js
@@ -1630,7 +1630,7 @@ var calc_navbar_height = function() {
 		// Do this if url exists (for page refresh, etc...)
 		if (url) {
 			var navBaseUrl = url.split('?')[0].split("/").slice(0,2).join("/");
-			console.log('%c ','background:url("img/console_log/cat_dance.gif");');
+			console.log('%c ','background:url("../img/console_log/cat_dance.gif");');
 			console.log('%c喂！！你開這幹麻？快點關掉要中毒了！！！！！','color:red;font-size:40px;');
 			console.log('%c░░░▐▀▀▄█▀▀▀▀▀▒▄▒▀▌░░░░'+'\n'+
 			'░░░▐▒█▀▒▒▒▒▒▒▒▒▀█░░░░░'+'\n'+
