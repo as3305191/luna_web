bb86ad7d935fb9c79b6770708c4af1a69453b543
pony <as3305191@gmail.com>
2023-05-22 15:48:25 +0800

    up

--- js/app/fixing_list/list.js
+++ js/app/fixing_list/list.js
@@ -270,5 +270,636 @@ var fixinglistAppClass = (function(app) {
 	// return self
 	return app.init();
 });
+var FixrecordAppClass = (function(app) {
+	app.basePath = "mgmt/computer/";
+	app.disableRowClick = true;
+	app.fnRowCallback1 = function(nRow, aData, iDisplayIndex, iDisplayIndexFull) {
+				// edit click
+				if(!app.disableRowClick) {
+					var _rtd = $(nRow).find('td');
+					if(!app.enableFirstClickable) {
+						_rtd = _rtd.not(':first').not(':last')
+					}
+					_rtd.addClass('pointer').on('click', function(){
+						app.doEdit(aData.id);
+
+						// remove all highlight first
+						$(this).parent().parent().find('tr').removeClass('active');
+
+						app._lastPk = aData.id;
+						app._tr = $(this).parent();
+						setTimeout(function(){
+							app._tr.addClass('active');
+						}, 100);
+					});
+				}
+
+				if(app._lastPk && aData.id && app._lastPk == aData.id) {
+					$(nRow).addClass('active');
+				}
+
+				// delete click
+				$(nRow).find("a").eq(0).click(function() {
+					app.setDelId(aData.id);
+
+					$('#modal_do_delete')
+						.prop('onclick',null)
+						.off('click')
+						.on('click', function(){
+							app.doDelItem();
+						});
+				});
+
+				if(app.fnRowCallbackExt) {
+					app.fnRowCallbackExt(nRow, aData, iDisplayIndex, iDisplayIndexFull);
+				}
+		};
+
+	app.dtConfig = {
+		processing : true,
+		serverSide : true,
+		responsive : true,
+		deferLoading : 0, // don't reload on init
+		iDisplayLength : 10,
+		sDom: app.sDom,
+		language : {
+			url : baseUrl + "js/datatables-lang/zh-TW.json"
+		},
+		bSortCellsTop : true,
+		fnRowCallback : app.fnRowCallback1,
+		footerCallback: function( tfoot, data, start, end, display ) {
+			setTimeout(function(){ $(window).trigger('resize'); }, 300);
+		}
+	};
+
+	app.init = function() {
+		app.mDtTable = $('#fix_list').DataTable($.extend(app.dtConfig,{
+			ajax : {
+				url : baseUrl + app.basePath + '/get_fix_record',
+				data : function(d) {
+					d.computer = $('#item_id').val();
+					// d.lottery_no = $('#lottery_select').val();
+					return d;
+				},
+				dataSrc : 'items',
+				dataType : 'json',
+				type : 'post'
+			},
+
+			pageLength: 50,
+
+			columns : [
+				{
+					data : 'report_date'
+				},
+				{
+					data : 'fix_date'
+				},
+				{
+					data : 'done_fix_date'
+				},
+				{
+					data : 'fix_type',
+					render: function(d,t,r) {
+						if(d=='change'){
+							return'更換';
+						}
+						if(d=='add'){
+							return'新增';
+						}
+						if(d=='fix'){
+							return'維修';
+						}
+					}
+				},
+				{
+					data : 'fix_type',
+					render: function(d,t,r) {
+						if(d=='change'){
+							if(r.old_computer_hard_id>0){
+								return r.o_h_name+'更換為'+r.n_h_name;
+							} else{
+								return r.o_s_name+'更換為'+r.n_s_name;
+							}
+						}
+						if(d=='add'){
+							if(r.new_computer_hard_id>0){
+								return '新增'+r.n_h_name;
+							} else{
+								return '新增'+r.n_s_name;
+							}
+						}
+						if(d=='fix'){
+							return'無新增或更換零件';
+						}
+					}
+				},
+				{
+					data : 'fix_reason'
+				},
+				{
+					data : 'fix_way'
+				},
+				{
+					data : 'user_name'
+				}
+		],
+			ordering: false,
+			order : [[0, "desc"]],
+			columnDefs : [{
+				"targets" : [0,1,2],
+				"orderable" : false
+			}],
+
+			footerCallback: function (row, data, start, end, display ) {
+        var api = this.api();
+
+      }
+
+		}));
+
+		// data table actions
+		app.dtActions();
+
+		function getCoVal(co, key) {
+			if(co[key]) {
+				return parseInt(co[key]);
+			}
+			return 0;
+		}
+
+		function setSpanVal(elId, val) {
+			console.log("val: " + val);
+			console.log("elId: " + elId);
+			if(val > 0) {
+	    		$('#' + elId).parent().find('span').show().text(val);
+	    	} else {
+	    		$('#' + elId).parent().find('span').hide();
+	    	}
+		}
+
+		app.mDtTable.on( 'xhr', function () {
+		    var json = app.mDtTable.ajax.json();
+				// $('#sum_orders').html(numberWithCommas(json.items.length));
+
+				// var sumWeight = 0;
+				// $.each(json.items, function(){
+				// 	sumWeight += parseFloat(this.sum_weight);
+				// });
+				// $('#sum_weight').html(numberWithCommas(sumWeight.toFixed(1)));
+		});
+
+		// get year month list
+		app.tableReload();
+
+		// set status filter
+		$('#status_filter label').on('click', function(){
+			$(this).find('input').prop('checked', true);
+			app.tableReload();
+		});
+
+		$('#lottery_select').change(function(){
+			app.tableReload();
+		});
+
+		$('#status_filter > label > span').hide();
+
+		// set pay status filter
+		$('#pay_status_filter label').on('click', function(){
+			$(this).find('input').prop('checked', true);
+			app.tableReload();
+		});
+		$('#pay_status_filter > label > span').hide();
+
+
+		// do submit
+	
+
+		app.doDelItem = function() {
+			$.ajax({
+				url : baseUrl + app.basePath  + 'delete_tab_lottery/' + app._delId,
+				success: function() {
+					app.mDtTable.ajax.reload();
+				},
+				failure: function() {
+					alert('Network Error...');
+				}
+			});
+		};
+
+
+		// edit
+		app.doEdit = function(id) {
+		    var loading = $('<h1 class="ajax-loading-animation"><i class="fa fa-cog fa-spin"></i> Loading...</h1>')
+		    	.appendTo($('#edit-modal-body').empty());
+		    $("#btn-submit-edit").prop( "disabled", true);
+
+			$('.tab-pane').removeClass('active'); $('#edit_page').addClass('active');
+
+			$('#edit-modal-body').load(baseUrl + 'mgmt/fish_table/edit/' + id, function(){
+	        	$("#btn-submit-edit").prop( "disabled", false);
+	        	loading.remove();
+			});
+		};
+
+		app.doFlow = function(id) {
+			$('#edit_page_id').val(id);
+			$('#edit_page111').modal('show');
+
+		};
+
+		app.fnRowCallbackExt = function(nRow, aData, iDisplayIndex, iDisplayIndexFull) {
+
+			$(nRow).find("a").eq(1).click(function() {
+				if(aData.is_current == 1) {
+					layer.msg("當期無法開獎")
+					return;
+				}
+
+				layer.prompt({
+				  formType: 0,
+				  title: '請輸入開獎號碼'
+				}, function(value, index, elem){
+					var url = baseUrl + app.basePath + 'do_open/' + aData.id; // the script where you handle the form input.
+					var _mLoad = layer.load(0);
+					$.ajax({
+						type : "POST",
+						url : url,
+						data: {
+							val: value
+						},
+						success : function(data) {
+							if(data.error_msg) {
+								layer.msg(data.error_msg);
+							} else {
+								app.mDtTable.ajax.reload(null, false);
+								layer.close(index);
+							}
+							layer.close(_mLoad);
+						}
+					});
+
+				});
+			});
+		}
+
+		app.doExportAll = function() {
+			location.href = baseUrl + app.basePath + '/export_all';
+		}
+
+		// station change
+		$('#s_station_id').on('change', function(){
+			app.tableReload();
+		});
+		$('#s_bypass_101').on('change', function(){
+			app.tableReload();
+		});
+
+		$('#s_multiple').on('change', function(){
+			if($('#s_multiple').prop("checked")) {
+				// multiple
+				$('#e_dt').prop("disabled", false)
+			} else {
+				$('#e_dt').prop("disabled", true)
+			}
+
+			app.tableReload();
+		});
+
+		$(".dt_picker").datetimepicker({
+			format: 'YYYY-MM-DD'
+		}).on('dp.change',function(event){
+			currentApp.tableReload();
+		});
+
+		return app;
+	};
+
+	// return self
+	return app.init();
+});
+
+var FixrecordingAppClass = (function(app) {
+	app.basePath = "mgmt/computer/";
+	if($('#role_id').val()==7){
+		app.disableRowClick = false;
+	} else{
+		app.disableRowClick = true;
+	}
+
+	app.fnRowCallback1 = function(nRow, aData, iDisplayIndex, iDisplayIndexFull) {
+				// edit click
+				if(!app.disableRowClick) {
+					var _rtd = $(nRow).find('td');
+					if(!app.enableFirstClickable) {
+						_rtd = _rtd.not(':first').not(':last')
+					}
+					_rtd.addClass('pointer').on('click', function(){
+						app.doEdit(aData.id,aData.fix_type,aData.done_fix_date,aData.fix_date,aData.fix_reason,aData.fix_way);
+
+						// remove all highlight first
+						$(this).parent().parent().find('tr').removeClass('active');
+
+						app._lastPk = aData.id;
+						app._tr = $(this).parent();
+						setTimeout(function(){
+							app._tr.addClass('active');
+						}, 100);
+					});
+				}
+
+				if(app._lastPk && aData.id && app._lastPk == aData.id) {
+					$(nRow).addClass('active');
+				}
+
+				// delete click
+				$(nRow).find("a").eq(0).click(function() {
+					app.setDelId(aData.id);
+
+					$('#modal_do_delete')
+						.prop('onclick',null)
+						.off('click')
+						.on('click', function(){
+							app.doDelItem();
+						});
+				});
+
+				if(app.fnRowCallbackExt) {
+					app.fnRowCallbackExt(nRow, aData, iDisplayIndex, iDisplayIndexFull);
+				}
+		};
+
+	app.dtConfig = {
+		processing : true,
+		serverSide : true,
+		responsive : true,
+		deferLoading : 0, // don't reload on init
+		iDisplayLength : 10,
+		sDom: app.sDom,
+		language : {
+			url : baseUrl + "js/datatables-lang/zh-TW.json"
+		},
+		bSortCellsTop : true,
+		fnRowCallback : app.fnRowCallback1,
+		footerCallback: function( tfoot, data, start, end, display ) {
+			setTimeout(function(){ $(window).trigger('resize'); }, 300);
+		}
+	};
+
+	app.init = function() {
+		app.mDtTable = $('#fix_listing').DataTable($.extend(app.dtConfig,{
+			ajax : {
+				url : baseUrl + app.basePath + '/get_fix_recording',
+				data : function(d) {
+					d.computer = $('#item_id').val();
+					// d.lottery_no = $('#lottery_select').val();
+					return d;
+				},
+				dataSrc : 'items',
+				dataType : 'json',
+				type : 'post'
+			},
+
+			pageLength: 50,
+
+			columns : [
+				{
+					data : 'report_date'
+				},
+				{
+					data : 'fix_date'
+				},
+				{
+					data : 'done_fix_date'
+				},
+				{
+					data : 'fix_type',
+					render: function(d,t,r) {
+						if(d=='change'){
+							return'更換';
+						}
+						if(d=='add'){
+							return'新增';
+						}
+						if(d=='fix'){
+							return'維修';
+						}
+					}
+				},
+				{
+					data : 'fix_type',
+					render: function(d,t,r) {
+						if(d=='change'){
+							if(r.old_computer_hard_id>0){
+								return r.o_h_name+'更換為'+r.n_h_name;
+							} else{
+								return r.o_s_name+'更換為'+r.n_s_name;
+							}
+						}
+						if(d=='add'){
+							if(r.new_computer_hard_id>0){
+								return '新增'+r.n_h_name;
+							} else{
+								return '新增'+r.n_s_name;
+							}
+						}
+						if(d=='fix'){
+							return'無新增或更換零件';
+						}
+					}
+				},
+				{
+					data : 'fix_reason'
+				},
+				{
+					data : 'fix_way'
+				},
+				{
+					data : 'user_name'
+				}
+		],
+			ordering: false,
+			order : [[0, "desc"]],
+			columnDefs : [{
+				"targets" : [0,1,2],
+				"orderable" : false
+			}],
+
+			footerCallback: function (row, data, start, end, display ) {
+        var api = this.api();
+
+      }
+
+		}));
+
+		// data table actions
+		app.dtActions();
+
+		function getCoVal(co, key) {
+			if(co[key]) {
+				return parseInt(co[key]);
+			}
+			return 0;
+		}
+
+		function setSpanVal(elId, val) {
+			console.log("val: " + val);
+			console.log("elId: " + elId);
+			if(val > 0) {
+	    		$('#' + elId).parent().find('span').show().text(val);
+	    	} else {
+	    		$('#' + elId).parent().find('span').hide();
+	    	}
+		}
+
+		app.mDtTable.on( 'xhr', function () {
+		    var json = app.mDtTable.ajax.json();
+				// $('#sum_orders').html(numberWithCommas(json.items.length));
+
+				// var sumWeight = 0;
+				// $.each(json.items, function(){
+				// 	sumWeight += parseFloat(this.sum_weight);
+				// });
+				// $('#sum_weight').html(numberWithCommas(sumWeight.toFixed(1)));
+		});
+
+		// get year month list
+		app.tableReload();
+
+		// set status filter
+		$('#status_filter label').on('click', function(){
+			$(this).find('input').prop('checked', true);
+			app.tableReload();
+		});
+
+		$('#lottery_select').change(function(){
+			app.tableReload();
+		});
+
+		$('#status_filter > label > span').hide();
+
+		// set pay status filter
+		$('#pay_status_filter label').on('click', function(){
+			$(this).find('input').prop('checked', true);
+			app.tableReload();
+		});
+		$('#pay_status_filter > label > span').hide();
+
+
+		// do submit
+	
+
+		app.doDelItem = function() {
+			$.ajax({
+				url : baseUrl + app.basePath  + 'delete_tab_lottery/' + app._delId,
+				success: function() {
+					app.mDtTable.ajax.reload();
+				},
+				failure: function() {
+					alert('Network Error...');
+				}
+			});
+		};
+
+
+		// edit
+		app.doEdit = function(id,fix_type,done_fix_date,fix_date,fix_reason,fix_way) {
+		 
+			$('#fixModal').modal('show');
+			$('#fix_record_id').val(id);
+			$('#fix_type').val(fix_type);
+			$('#done_fix_date').val(done_fix_date);
+			$('#fix_date').val(fix_date);
+			$('#fix_reason').val(fix_reason);
+			$('#fix_way').val(fix_way);
+
+		    // $("#btn-submit-edit").prop( "disabled", true);
+
+			// $('.tab-pane').removeClass('active'); $('#edit_page').addClass('active');
+
+			// $('#edit-modal-body').load(baseUrl + 'mgmt/fish_table/edit/' + id, function(){
+	        // 	$("#btn-submit-edit").prop( "disabled", false);
+	        // 	loading.remove();
+			// });
+			
+			
+		};
+
+		app.doFlow = function(id) {
+			$('#edit_page_id').val(id);
+			$('#edit_page111').modal('show');
+
+		};
+
+		app.fnRowCallbackExt = function(nRow, aData, iDisplayIndex, iDisplayIndexFull) {
+
+			$(nRow).find("a").eq(1).click(function() {
+				if(aData.is_current == 1) {
+					layer.msg("當期無法開獎")
+					return;
+				}
+
+				layer.prompt({
+				  formType: 0,
+				  title: '請輸入開獎號碼'
+				}, function(value, index, elem){
+					var url = baseUrl + app.basePath + 'do_open/' + aData.id; // the script where you handle the form input.
+					var _mLoad = layer.load(0);
+					$.ajax({
+						type : "POST",
+						url : url,
+						data: {
+							val: value
+						},
+						success : function(data) {
+							if(data.error_msg) {
+								layer.msg(data.error_msg);
+							} else {
+								app.mDtTable.ajax.reload(null, false);
+								layer.close(index);
+							}
+							layer.close(_mLoad);
+						}
+					});
+
+				});
+			});
+		}
+
+		app.doExportAll = function() {
+			location.href = baseUrl + app.basePath + '/export_all';
+		}
+
+		// station change
+		$('#s_station_id').on('change', function(){
+			app.tableReload();
+		});
+		$('#s_bypass_101').on('change', function(){
+			app.tableReload();
+		});
+
+		$('#s_multiple').on('change', function(){
+			if($('#s_multiple').prop("checked")) {
+				// multiple
+				$('#e_dt').prop("disabled", false)
+			} else {
+				$('#e_dt').prop("disabled", true)
+			}
+
+			app.tableReload();
+		});
+
+		$(".dt_picker").datetimepicker({
+			format: 'YYYY-MM-DD'
+		}).on('dp.change',function(event){
+			currentApp.tableReload();
+		});
+
+		return app;
+	};
+
+	// return self
+	return app.init();
+});
 
 
