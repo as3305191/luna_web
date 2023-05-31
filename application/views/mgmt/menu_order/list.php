f00100ecc849d65383b0637af41f2c2aa9abf65c
pony <as3305191@gmail.com>
2023-05-31 14:41:12 +0800

    up

--- application/views/mgmt/menu_order/list.php
+++ application/views/mgmt/menu_order/list.php
@@ -15,24 +15,20 @@
 			<div class="row">
 				<!-- NEW WIDGET START -->
 			<article class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-				<div class="col-md-12 col-xs-12 col-sm-12" style="padding:10px 20px 10px 13px;">
-					<div class="col-md-12 col-xs-12 col-sm-12 " style="padding:0px 0px 6px 0px;">
-						<span style="font-size: 16pt;color:#0d0d56">開放的菜單</span>
-					</div>
-					<button class="btn-success text-light btn_active menu_1" style="border-radius: 5px; padding: 10px; width: 160px; height: 48px;" onclick="backStep1()">菜單1</button>
-					<!-- <button class="btn-warning text-light" style="border-radius: 5px; padding: 10; width: 160px; height: 48px; background-color: #FFD835; color: #f56b10;">場次設定</button> -->
-					<button class="btn-light text-light btn_unsuccess menu_2" style="border-radius: 5px; padding: 10px; width: 160px; height: 48px;">菜單2</button>
-					<button class="btn-light text-light btn_unsuccess menu_3" style="border-radius: 5px; padding: 10px; width: 160px; height: 48px;">菜單3</button>
-				
-				
-					<hr/>
-				</div>
+			
 				<div>
 					<!-- widget edit box -->
 					<div class="jarviswidget-editbox">
 						<!-- This area used as dropdown edit box -->
-
+							<div class="col-md-12 col-xs-12 col-sm-12 " style="padding:0px 0px 6px 0px;">
+								<span style="font-size: 16pt;color:#0d0d56">開放的菜單</span>
+							</div>
+							<button class="btn-success text-light btn_active menu_1" style="border-radius: 5px; padding: 10px; width: 160px; height: 48px;" onclick="backStep1()">菜單1</button>
+							<!-- <button class="btn-warning text-light" style="border-radius: 5px; padding: 10; width: 160px; height: 48px; background-color: #FFD835; color: #f56b10;">場次設定</button> -->
+							<button class="btn-light text-light btn_unsuccess menu_2" style="border-radius: 5px; padding: 10px; width: 160px; height: 48px;">菜單2</button>
+							<button class="btn-light text-light btn_unsuccess menu_3" style="border-radius: 5px; padding: 10px; width: 160px; height: 48px;">菜單3</button>
 					</div>
+					<hr/>
 					<div class="widget-body no-padding">
 						<table id="dt_list" class="table table-striped table-bordered table-hover" >
 							<thead>
