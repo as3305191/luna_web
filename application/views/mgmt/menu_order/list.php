64ec9b32d67b4c7de79566c74ed3c3fb1388415d
pony <as3305191@gmail.com>
2023-05-31 14:42:51 +0800

    up

--- application/views/mgmt/menu_order/list.php
+++ application/views/mgmt/menu_order/list.php
@@ -17,9 +17,6 @@
 			<article class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 			
 				<div>
-					<!-- widget edit box -->
-					<div class="jarviswidget-editbox">
-						<!-- This area used as dropdown edit box -->
 							<div class="col-md-12 col-xs-12 col-sm-12 " style="padding:0px 0px 6px 0px;">
 								<span style="font-size: 16pt;color:#0d0d56">開放的菜單</span>
 							</div>
@@ -27,6 +24,10 @@
 							<!-- <button class="btn-warning text-light" style="border-radius: 5px; padding: 10; width: 160px; height: 48px; background-color: #FFD835; color: #f56b10;">場次設定</button> -->
 							<button class="btn-light text-light btn_unsuccess menu_2" style="border-radius: 5px; padding: 10px; width: 160px; height: 48px;">菜單2</button>
 							<button class="btn-light text-light btn_unsuccess menu_3" style="border-radius: 5px; padding: 10px; width: 160px; height: 48px;">菜單3</button>
+					<!-- widget edit box -->
+					<div class="jarviswidget-editbox">
+						<!-- This area used as dropdown edit box -->
+						
 					</div>
 					<hr/>
 					<div class="widget-body no-padding">
