174f547f9b5d2ea2e95b3f090dc426c0dcc557bc
pony <as3305191@gmail.com>
2023-05-31 15:06:23 +0800

    up

--- application/views/mgmt/menu_order/list.php
+++ application/views/mgmt/menu_order/list.php
@@ -18,9 +18,9 @@
 			
 
 
-				<div class="row" style="padding:0px 0px 6px 0px;">
+				<div class="row" style="padding:0px 0px 6px 12px;">
 
-					<div class="col-md-12 col-xs-12 col-sm-12 " style="padding:0px 0px 6px 12px;">
+					<div class="col-md-12 col-xs-12 col-sm-12 " style="padding:0px 0px 6px 0px;">
 						<span style="font-size: 16pt;color:#0d0d56">開放的菜單</span>
 					</div>
 					<button class="btn-success text-light btn_active menu_1" style="border-radius: 5px; padding: 10px; width: 160px; height: 48px;" onclick="backStep1()">菜單1</button>
