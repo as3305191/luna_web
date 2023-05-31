a5ef755d33763e1b480bcfe836ea1d0b05827723
pony <as3305191@gmail.com>
2023-05-31 09:26:51 +0800

    up

--- application/views/mgmt/menu/list.php
+++ application/views/mgmt/menu/list.php
@@ -109,7 +109,7 @@ thead tr th {
 		} 
 	}];
 
-	var mOrderIdx = 0;
+	// var mOrderIdx = 0;
 	
 	var defaultContent = '<a href="#deleteModal" role="button" data-toggle="modal" style="margin-right: 5px;"><i class="fa fa-trash fa-lg"></i></a>';
 
