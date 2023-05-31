00bdabe440f5e07e5b5f2f06cedaf2550700329d
pony <as3305191@gmail.com>
2023-05-31 14:37:18 +0800

    up

--- application/views/mgmt/menu_order/list.php
+++ application/views/mgmt/menu_order/list.php
@@ -6,73 +6,103 @@
 <div>
     <header>
 </header>
-    <div class="col-md-12 col-xs-12 col-sm-12" style="padding:10px 20px 10px 13px;">
-        <div class="col-md-12 col-xs-12 col-sm-12 " style="padding:0px 0px 6px 0px;">
-            <span style="font-size: 16pt;color:#0d0d56">開放的菜單</span>
-        </div>
-        <button class="btn-success text-light btn_active menu_1" style="border-radius: 5px; padding: 10px; width: 160px; height: 48px;" onclick="backStep1()">菜單1</button>
-        <!-- <button class="btn-warning text-light" style="border-radius: 5px; padding: 10; width: 160px; height: 48px; background-color: #FFD835; color: #f56b10;">場次設定</button> -->
-        <button class="btn-light text-light btn_unsuccess menu_2" style="border-radius: 5px; padding: 10px; width: 160px; height: 48px;">菜單2</button>
-        <button class="btn-light text-light btn_unsuccess menu_3" style="border-radius: 5px; padding: 10px; width: 160px; height: 48px;">菜單3</button>
-       
-       
-        <hr/>
-    </div>
-	<div>
-		<!-- widget edit box -->
-		<div class="jarviswidget-editbox">
-			<!-- This area used as dropdown edit box -->
+<div class="tab-content">
+	<div class="tab-pane active" id="list_page">
 
-		</div>
-		<div class="widget-body no-padding">
-			<table id="dt_list" class="table table-striped table-bordered table-hover" width="100%">
-				<thead>
-					<tr>
-						<th></th>
-						<th>選擇店家</th>
-						<th>品項</th>
-						<th>金額</th>
-						<th>備注</th>
-						<th></th>
-					</tr>
-					<tr>
-						<td class="min50" style="border-right:none;"></td>
-						<td class="min120" style="border-right:none;">
-							<div class="input-group col-md-12">
-								<select id="menu_name" class="form-control">
+		<!-- widget grid -->
+		<section id="widget-grid" class="">
+			<!-- row -->
+			<div class="row">
+				<!-- NEW WIDGET START -->
+				<article class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<div class="col-md-12 col-xs-12 col-sm-12" style="padding:10px 20px 10px 13px;">
+					<div class="col-md-12 col-xs-12 col-sm-12 " style="padding:0px 0px 6px 0px;">
+						<span style="font-size: 16pt;color:#0d0d56">開放的菜單</span>
+					</div>
+					<button class="btn-success text-light btn_active menu_1" style="border-radius: 5px; padding: 10px; width: 160px; height: 48px;" onclick="backStep1()">菜單1</button>
+					<!-- <button class="btn-warning text-light" style="border-radius: 5px; padding: 10; width: 160px; height: 48px; background-color: #FFD835; color: #f56b10;">場次設定</button> -->
+					<button class="btn-light text-light btn_unsuccess menu_2" style="border-radius: 5px; padding: 10px; width: 160px; height: 48px;">菜單2</button>
+					<button class="btn-light text-light btn_unsuccess menu_3" style="border-radius: 5px; padding: 10px; width: 160px; height: 48px;">菜單3</button>
+				
+				
+					<hr/>
+				</div>
+				<div>
+					<!-- widget edit box -->
+					<div class="jarviswidget-editbox">
+						<!-- This area used as dropdown edit box -->
+
+					</div>
+					<div class="widget-body no-padding">
+						<table id="dt_list" class="table table-striped table-bordered table-hover" width="100%">
+							<thead>
+								<tr>
+									<th></th>
+									<th>選擇店家</th>
+									<th>品項</th>
+									<th>金額</th>
+									<th>備注</th>
+									<th></th>
+								</tr>
+								<tr>
+									<td class="min50" style="border-right:none;"></td>
+									<td class="min120" style="border-right:none;">
+										<div class="input-group col-md-12">
+											<select id="menu_name" class="form-control">
+												
+											</select> 
+										</div>
+									</td>
+									<td style="border-right:none;">
+										<div class="input-group col-md-12">
+											<input type="text" class="form-control" id="order_name" placeholder="品項">
+										</div>
+									</td>
+									<td style="border-right:none;">
+										<div class="input-group col-md-12">
+											<input type="text" class="form-control" id="amount" placeholder="金錢總額">
+										</div>
+									</td>
+									<td style="border-right:none;">
+										<div class="input-group col-md-12">
+											<input type="text" class="form-control" id="note" placeholder="備注">
+										</div>
+									</td>
 									
-								</select> 
-							</div>
-						</td>
-						<td style="border-right:none;">
-							<div class="input-group col-md-12">
-								<input type="text" class="form-control" id="order_name" placeholder="品項">
-							</div>
-						</td>
-						<td style="border-right:none;">
-							<div class="input-group col-md-12">
-								<input type="text" class="form-control" id="amount" placeholder="金錢總額">
-							</div>
-						</td>
-						<td style="border-right:none;">
-							<div class="input-group col-md-12">
-								<input type="text" class="form-control" id="note" placeholder="備注">
-							</div>
-						</td>
-						
-						<td style="border-right:none;">
-							<button type="button" class="btn btn-sm btn-primary" onclick="add_order()"><i class="fa fa-plus-circle fa-lg"></i></button>
-						</td>
-					</tr>
-				</thead>
-				<tbody>
-					
-				</tbody>
-			</table>
+									<td style="border-right:none;">
+										<button type="button" class="btn btn-sm btn-primary" onclick="add_order()"><i class="fa fa-plus-circle fa-lg"></i></button>
+									</td>
+								</tr>
+							</thead>
+							<tbody>
+								
+							</tbody>
+						</table>
 
-		</div>
+					</div>
+				</div>
+				</article>
+				<!-- WIDGET END -->
+
+			</div>
+
+			<!-- end row -->
+
+		</section>
+		<!-- end widget grid -->
 	</div>
 
+	<div class="tab-pane animated fadeIn" id="edit_page">
+		<section class="">
+			<!-- row -->
+			<div class="row">
+				<article class="col-xs-12 col-sm-12 col-md-12 col-lg-12" id="edit-modal-body">
+									
+				</article>
+			</div>
+		</section>
+	</div>
+</div>
 <?php $this -> load -> view('general/delete_modal'); ?>
 <script type="text/javascript">
 
