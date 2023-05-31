cd581de820017190f4c6120d7142cf567fd6d32a
pony <as3305191@gmail.com>
2023-05-31 17:36:16 +0800

    up

--- application/views/mgmt/menu_order/list.php
+++ application/views/mgmt/menu_order/list.php
@@ -222,7 +222,7 @@ var mColDefs = [{
 		$('.menu_'+id).removeClass('btn_unsuccess');
 		$('.menu_'+id).addClass('btn_active btn-success ');
 		$('#menu_id').val(id);
-		load_menu(id);
+		load_menu();
 	}
 	function add_order(){//按下+按鈕時新增畫面以及寫入資料庫
 		var menu_id = $('#menu_name').val();
