277c5a8d10c246d5029a7ceb5e8ccc2446c2fed2
pony <as3305191@gmail.com>
2023-05-31 09:56:36 +0800

    up

--- application/views/mgmt/menu1/list.php
+++ application/views/mgmt/menu1/list.php
@@ -14,8 +14,8 @@
 					<div class="jarviswidget">
 						<header>
 							<div class="widget-toolbar pull-left">
-								<label class="col-md-3 control-label">圖片類別</label>
-								<div class="col-md-6">
+								<label class="col-md-5 control-label">店家類別</label>
+								<div class="col-md-3">
 									<select id="img_style" class="form-control">
 										<!-- option from javascript -->
 									</select>
