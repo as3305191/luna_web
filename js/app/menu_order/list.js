55724c878f7d9c0b3844036b0f629957aa05a049
pony <as3305191@gmail.com>
2023-06-07 11:12:46 +0800

    up

--- js/app/menu_order/list.js
+++ js/app/menu_order/list.js
@@ -1,47 +1,116 @@
 var menuorderAppClass = (function(app) {
 	app.basePath = "mgmt/menu_order/";
 	app.disableRowClick = true;
+	app.fnRowCallback1 = function(nRow, aData, iDisplayIndex, iDisplayIndexFull) {
+				// edit click
+				if(!app.disableRowClick) {
+					var _rtd = $(nRow).find('td');
+					if(!app.enableFirstClickable) {
+						_rtd = _rtd.not(':first').not(':last')
+					}
+					_rtd.addClass('pointer').on('click', function(){
+						app.doEdit(aData.id);
+
+						// remove all highlight first
+						$(this).parent().parent().find('tr').removeClass('active');
+
+						app._lastPk = aData.id;
+						app._tr = $(this).parent();
+						setTimeout(function(){
+							app._tr.addClass('active');
+						}, 100);
+					});
+				}
+
+				if(app._lastPk && aData.id && app._lastPk == aData.id) {
+					$(nRow).addClass('active');
+				}
+
+			
+
+				if(app.fnRowCallbackExt) {
+					app.fnRowCallbackExt(nRow, aData, iDisplayIndex, iDisplayIndexFull);
+				}
+		};
+
+	app.dtConfig = {
+		processing : true,
+		serverSide : true,
+		responsive : true,
+		deferLoading : 0, // don't reload on init
+		iDisplayLength : 10,
+		sDom: app.sDom,
+		language : {
+			url : baseUrl + "js/datatables-lang/zh-TW.json"
+		},
+		bSortCellsTop : true,
+		fnRowCallback : app.fnRowCallback1,
+		footerCallback: function( tfoot, data, start, end, display ) {
+			setTimeout(function(){ $(window).trigger('resize'); }, 300);
+		}
+	};
+
 	app.init = function() {
 		app.mDtTable = $('#dt_list').DataTable($.extend(app.dtConfig,{
 			ajax : {
 				url : baseUrl + app.basePath + '/get_data',
 				data : function(d) {
-					// d.s_menu_name = $('#s_menu_name').val();
-					
+					// d.computer = $('#item_id').val();
+					// d.lottery_no = $('#lottery_select').val();
+					return d;
 				},
 				dataSrc : 'items',
 				dataType : 'json',
-				type : 'post',
-				complete:function(data){
-				}
+				type : 'post'
 			},
-			iDisplayLength : 10,
-			columns : mCols,
-			order : false,
-			columnDefs : mColDefs
+
+			pageLength: 10,
+
+			columns : [null,{
+				data : 'menu_name'
+			},{
+				data : 'order_name'
+			},{
+				data : 'amount'
+			},{
+				data : 'note'
+			}],
+			ordering: false,
+			order : [[0, "desc"]],
+			columnDefs : [{
+				"targets" : [0,1,2,3,4],
+				"orderable" : false
+			}],
+
+			footerCallback: function (row, data, start, end, display ) {
+				var api = this.api();
+			}
+
 		}));
 
 		// data table actions
 		app.dtActions();
 
+		app.mDtTable.on( 'xhr', function () {
+		    var json = app.mDtTable.ajax.json();
+	
+		});
+
 		// get year month list
 		app.tableReload();
 
-		app.doExportAll = function(id) {
-			window.open(baseUrl + app.basePath + 'export_all/' + id);
-		}
-		
+	
 
-		$('#s_menu_name').on('change', function(){
-			app.tableReload();
-		});
-	    // $(window).trigger("hashchange");
 
+		// do submit
+	
 		return app;
 	};
 
+	// return self
 	return app.init();
 });
+
 var menu_otherAppClass = (function(app) {
 	app.basePath = "mgmt/menu_order/";
 	app.disableRowClick = true;
