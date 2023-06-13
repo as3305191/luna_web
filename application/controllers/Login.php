943e8c9ce07809e4b51e665f4877b0da780d2c9c
pony <as3305191@gmail.com>
2023-06-13 08:54:11 +0800

    up

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -15,7 +15,7 @@ class Login extends MY_Base_Controller {
 	public function index() {
 		$data = array();
 		// check login
-		$data['num'] = $this -> session -> userdata("menu_order");
+		$data['menu_order'] = $this -> session -> userdata("menu_order");
 
 		if(!empty($this -> session -> userdata('user_id'))) {
 			redirect("/app/#mgmt/message");
