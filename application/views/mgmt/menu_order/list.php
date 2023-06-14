1d358ee3b78df8a27046e82b57e843402b666150
pony <as3305191@gmail.com>
2023-06-14 14:11:34 +0800

    up

--- application/views/mgmt/menu_order/list.php
+++ application/views/mgmt/menu_order/list.php
@@ -215,7 +215,7 @@
 					$('#album').append(img_html);					
        	 		});
 				$("#menu_name").val($('#menu_id').val());
-				currentApp_other.tableReload();
+				// currentApp_other.tableReload();
 			}
 		});
 	}
