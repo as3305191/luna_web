f0bfad06651eeb72df99c83255fb4a43219832ec
pony <as3305191@gmail.com>
2023-10-19 15:56:32 +0800

    up

--- js/app/menu_order/list.js
+++ js/app/menu_order/list.js
@@ -247,9 +247,14 @@ var menu_otherAppClass = (function(app) {
 
 		// get year month list
 		app.tableReload();
+
 		// $('#menu_name').on('change', function(){
 		// 	app.tableReload();
 		// });
+
+
+		// do submit
+		// load_menu() ;
 		return app;
 	};
 	
