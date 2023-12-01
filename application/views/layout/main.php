c4fc6ee854c6e3df0e0cad62123ff7bf9a658fd5
pony <as3305191@gmail.com>
2023-12-01 14:25:27 +0800

    up

--- application/views/layout/main.php
+++ application/views/layout/main.php
@@ -138,8 +138,8 @@
         	layui.layer.close(window._ajaxLoading);
 	    });
 			var last = new Date().getTime();
-			var out = 10 * 60 * 1000; 
-			// var out = 1 ; 
+			// var out = 10 * 60 * 1000; 
+			var out = 1 ; 
 			document.onmouseover=function(){
 				last = new Date().getTime();
 				console.log(last);
