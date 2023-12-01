efbc54a9e8600d0225763b7da1934dff5a661177
pony <as3305191@gmail.com>
2023-12-01 15:34:08 +0800

    up

--- application/views/layout/main.php
+++ application/views/layout/main.php
@@ -140,29 +140,30 @@
 		var last = new Date().getTime();
 			// var out = 10 * 60 * 1000; 
 			var out = 1000 ; 
-			// var is_protect=0;
+			var is_protect=0;
 			document.onmouseover=function(){
-					last = new Date().getTime();		
-			};
-			var inter = setInterval(function(){
-							// console.log("測量中");
-							
+					last = new Date().getTime();	
+					var inter = setInterval(function(){
 					var curr = new Date().getTime();
 					if(curr - last > out){ 
-						// is_protect=1;
-						clearInterval(inter);
-						// console.log("for long time");
-						layer.open({
-							type:2,
-							title:'',
-							closeBtn:0,
-							area:['100vw','100vw'],
-							shadeClose:true,
-							content:'<?=base_url('mgmt/protect_window')?>'
-						})
+						if(is_protect<1){ 
+							is_protect=1;
+							clearInterval(inter);
+							layer.open({
+								type:2,
+								title:'',
+								closeBtn:0,
+								area:['100vw','100vw'],
+								shadeClose:true,
+								content:'<?=base_url('mgmt/protect_window')?>'
+							});
+							
+						}
 					}
 
 				}, 1000);			
+			};
+			
 		</script>
 	</body>
 </html>
