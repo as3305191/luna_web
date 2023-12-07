258506d390310dafdef9b856514f2504c18a79f1
pony <as3305191@gmail.com>
2023-12-07 13:23:46 +0800

    up

---
+++ application/views/mgmt/question_title/list.php
@@ -0,0 +1,85 @@
+<div class="tab-content">
+	<div class="tab-pane active" id="list_page">
+
+		<!-- widget grid -->
+		<section id="widget-grid" class="">
+
+			<!-- row -->
+			<div class="row">
+
+				<!-- NEW WIDGET START -->
+				<article class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+
+					<!-- Widget ID (each widget will need unique ID)-->
+					<div class="jarviswidget">
+						<header>
+							<!-- <div class="widget-toolbar pull-left">
+								<div class="btn-group">
+									<button onclick="currentApp.doEdit(0)" class="btn dropdown-toggle btn-xs btn-success" data-toggle="dropdown">
+										<i class="fa fa-plus"></i>新增
+									</button>
+								</div>
+							</div> -->
+						</header>
+
+						<!-- widget div-->
+						<div>
+							<!-- widget edit box -->
+							<div class="jarviswidget-editbox">
+								<!-- This area used as dropdown edit box -->
+							</div>
+							<!-- end widget edit box -->
+							<!-- widget content -->
+							<div class="widget-body no-padding">
+
+								<table id="dt_list" class="table table-striped table-bordered table-hover" width="100%">
+									<thead>
+										<tr>
+											<th class="min100"></th>
+											<th class="min150">問卷標題</th>
+											<th class="min150">匯出</th>
+											<th class="min150">建立時間</th>
+										</tr>
+										<!-- <tr class="search_box">
+											<th></th>
+											<th></th>
+											<th></th>
+										</tr> -->
+									</thead>
+									<tbody>
+									</tbody>
+								</table>
+							</div>
+							<!-- end widget content -->
+						</div>
+						<!-- end widget div -->
+					</div>
+					<!-- end widget -->
+				</article>
+				<!-- WIDGET END -->
+			</div>
+			<!-- end row -->
+		</section>
+		<!-- end widget grid -->
+	</div>
+
+	<div class="tab-pane animated fadeIn" id="edit_page">
+		<section class="">
+			<!-- row -->
+			<div class="row">
+				<article class="col-xs-12 col-sm-12 col-md-12 col-lg-12" id="edit-modal-body">
+
+				</article>
+			</div>
+		</section>
+	</div>
+</div>
+<?php $this -> load -> view('general/delete_modal'); ?>
+
+<script type="text/javascript">
+	loadScript(baseUrl + "js/class/BaseAppClass.js", function(){
+		loadScript(baseUrl + "js/app/question_title/list.js", function(){
+			currentApp = new QuestionTitleAppClass(new BaseAppClass({}));
+		});
+	});
+</script>
