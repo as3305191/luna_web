d91a3dbf57feeb1fb10320abe86ed32286faf9cc
pony <as3305191@gmail.com>
2024-01-04 14:49:32 +0800

    up

--- application/controllers/mgmt/Menu_orderby_user.php
+++ application/controllers/mgmt/Menu_orderby_user.php
@@ -50,10 +50,7 @@ class Menu_orderby_user extends MY_Mgmt_Controller {
 				} else{
 					$each->timestamp= '';
 				}
-				
-			}
-			foreach($items as $each){
-				$total_amount += $each->amount;
+				$total_amount += intval($each->amount);
 			}
 		$res['total_amount'] = $total_amount;
 		$res['items'] = $items;
