e1f5af0531833b673e2568c50573f510c40ff5de
pony <as3305191@gmail.com>
2024-01-04 15:17:55 +0800

    up

--- js/app/menu_orderby_user/list.js
+++ js/app/menu_orderby_user/list.js
@@ -14,7 +14,7 @@ var menuorderuserAppClass = (function(app) {
 				type : 'post',
 				complete:function(data){
 					if(data){
-						console.log(data.d);
+						console.log(data);
 						// if(data.total!=='0'||data.total!==null||data.total>0){
 						// 	$('#total').text(data.total);
 						// } else{
