e32dda4e09071921733c79b0e3b2d02692e25670
pony <as3305191@gmail.com>
2024-01-05 10:24:24 +0800

    up

--- application/controllers/mgmt/Menu_order.php
+++ application/controllers/mgmt/Menu_order.php
@@ -20,7 +20,7 @@ class Menu_order extends MY_Mgmt_Controller {
 		$login_user = $this -> users_dao -> find_by_id($data['login_user_id']);		
 		$login_user_dep=explode('#',ltrim(rtrim($login_user->in_department,'#'),'#'));
 		$list = $this -> menu_dao -> find_all_open();
-	
+		$map_list= array();
 		$weekarray=array("日","一","二","三","四","五","六");
 		
 		foreach($list as $each){
