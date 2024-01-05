3213c89a29eb784d382cf5fa5290436825e1b019
pony <as3305191@gmail.com>
2024-01-05 11:48:05 +0800

    up

--- application/views/mgmt/menu_orderby_user/list.php
+++ application/views/mgmt/menu_orderby_user/list.php
@@ -210,7 +210,7 @@ $('#add_img_style').click(function() {
 					if(d.list.length>0){
 						$.each(d.list, function(){
 							if(this.open_dep!=='0'){
-								$('<option style="" value='+this.id+'>&#xF023'+this.timestamp+' '+this.menu_name+'</option>'
+								$('<option style="" value='+this.id+'>&#xf023'+this.timestamp+' '+this.menu_name+'</option>'
 									).appendTo($menu_name);
 							} else{
 								$('<option/>', {
