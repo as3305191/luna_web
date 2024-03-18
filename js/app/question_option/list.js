7f8e0dfc31102eb532f15fb372e774c8ce067188
pony <as3305191@gmail.com>
2024-03-18 13:58:36 +0800

    up

--- js/app/question_option/list.js
+++ js/app/question_option/list.js
@@ -8,6 +8,8 @@ var QuestionoptionAppClass = (function(app) {
 					var _rtd = $(nRow).find('td');
 					if(!app.enableFirstClickable) {
 						_rtd = _rtd.not(':first')
+						_rtd = _rtd.not(':last')
+					}
 					}
 					_rtd.addClass('pointer').on('click', function(){
 						app.doEdit(aData.id);
