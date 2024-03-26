ed1995df2097498d3bb5c4285f838488e6d75e99
pony <as3305191@gmail.com>
2024-03-26 09:35:49 +0800

    up

--- application/views/layout/show_que1.php
+++ application/views/layout/show_que1.php
@@ -106,76 +106,162 @@
   $( document ).ready(function() {
     $('#name').focus();
   })
-var q1_6_check_value;
-var q1_7_check_value;
+  
+  var  q1 = null;
+  var  q2 = null;
+  var  q3 = null;
+  var  q4 = null;
+  var  q5 = null;
+  var  q6 = null;
+  var  q7 = null;
+  var  q8 = null;
+  var  q9 = null;
+  var  q10 = null;
+  var  q11 = null;
+  var  q12 = null;
+  
+  var  q1o = '無';
+  var  q2o = '無';
+  var  q3o = '無';
+  var is_ok=false;
+  $('input:radio[name="q1_1"]').on('change', function(){
+    q1 = $('input:radio[name="q1_1"]:checked').val();
+   
+  });
+  $('input:radio[name="q1_2"]').on('change', function(){
+    q2 = $('input:radio[name="q1_2"]:checked').val();
+   
+  });
+  $('input:radio[name="q1_3"]').on('change', function(){
+    q3 = $('input:radio[name="q1_3"]:checked').val();
+   
+  });
+  $('input:radio[name="q1_4"]').on('change', function(){
+    q4 = $('input:radio[name="q1_4"]:checked').val();
+   
+  });
+  $('input:radio[name="q1_5"]').on('change', function(){
+    q5 = $('input:radio[name="q1_5"]:checked').val();
+   
+  });
+  
 $("input[name='q1_6']").on('change', function(){
     obj = document.getElementsByName("q1_6");
-    q1_6_check_value = [];
+    q6 = [];
     for (i in obj) {
         if (obj[i].checked)
-        q1_6_check_value.push(obj[i].value);
+        q6.push(obj[i].value);
     }
 
-    console.log(q1_6_check_value);
+    // console.log(q6);
   });
   $("input[name='q1_7']").on('change', function(){
     obj = document.getElementsByName("q1_7");
-    q1_7_check_value = [];
+    q7 = [];
 
     for (i in obj) {
       if (obj[i].checked){
-        q1_7_check_value.push(obj[i].value);
+        q7.push(obj[i].value);
       }
     }
-    console.log(q1_7_check_value);
+    // console.log(q7);
 
   });
+  $('input:radio[name="q1_8"]').on('change', function(){
+    q8 = $('input:radio[name="q1_8"]:checked').val();
+   
+  });
+  $('input:radio[name="q1_9"]').on('change', function(){
+    q9 = $('input:radio[name="q1_9"]:checked').val();
+   
+  });
+  $('input:radio[name="q1_10"]').on('change', function(){
+    q10 = $('input:radio[name="q1_10"]:checked').val();
+   
+  });
+  $('input:radio[name="q1_11"]').on('change', function(){
+    q11 = $('input:radio[name="q1_11"]:checked').val();
+   
+  });
+  $('input:radio[name="q1_12"]').on('change', function(){
+    q12 = $('input:radio[name="q1_12"]:checked').val();
+   
+  });
+ 
+
+
+  $('[name="q1_1o"]').on('change', function(){
+    q1o = $('[name="q1_1o"]').val();
+    // console.log(q1o);
+   
+  });
+  $('[name="q1_5o"]').on('change', function(){
+    q2o = $('[name="q1_5o"]').val();
+    // console.log(q2o);
+
+  });
+  $('[name="q1_6o"]').on('change', function(){
+    q3o = $('[name="q1_6o"]').val();
+    // console.log(q3o);
+
+  });
+
+
+
+
+
+
   $('.cancel').click(function() {
     var index = parent.layer.getFrameIndex(window.name); //先得到当前iframe层的索引
     parent.layer.close(index);
   })
   $('.dosubmit').click(function() {
-   
-   $.ajax({
-     url: '<?= base_url() ?>' + 'mgmt/question_for_user/save_q1',
-     type: 'POST',
-     data: {
-       qid:$('#qid').val(),
-       q1 : document.querySelector("input[name='q1_1']:checked").value,
-       q2 : document.querySelector("input[name='q1_2']:checked").value,
-       q3 : document.querySelector("input[name='q1_3']:checked").value,
-       q4 : document.querySelector("input[name='q1_4']:checked").value,
-       q5 : document.querySelector("input[name='q1_5']:checked").value,
-       q6 : q1_6_check_value.toString(),
-       q7 : q1_7_check_value.toString(),
-       q8 : document.querySelector("input[name='q1_8']:checked").value,
-       q9 : document.querySelector("input[name='q1_9']:checked").value,
-       q10 : document.querySelector("input[name='q1_10']:checked").value,
-       q11 : document.querySelector("input[name='q1_11']:checked").value,
-       q12 : document.querySelector("input[name='q1_12']:checked").value,
-       q1o : document.querySelector("input[name='q1_1o']").value,
-       q2o: document.querySelector("input[name='q1_5o']").value,
-       q3o : document.querySelector("input[name='q1_6o']").value,
+    if (q1!==null&&q2!==null&&q3!==null&&q4!==null&&q5!==null&&q6!==null&&
+         q7!==null&&q8!==null&&q9!==null&&q10!==null&&q11!==null&&q12!==null){
+          $.ajax({
+            url: '<?= base_url() ?>' + 'mgmt/question_for_user/save_q1',
+            type: 'POST',
+            data: {
+              qid:$('#qid').val(),
+              q1 : q1,
+              q2 : q2,
+              q3 : q3,
+              q4 : q4,
+              q5 : q5,
+              q6 : q6,
+              q7 : q7,
+              q8 : q8,
+              q9 : q9,
+              q10 : q10,
+              q11 : q11,
+              q12 : q12,
+              q1o : q1o,
+              q2o : q2o,
+              q3o : q3o,
+            },
+            dataType: 'json',
+            success: function(d) {
+              if(d) {
+                console.log(d);
+              }
+              if(d.success){
+                var index = parent.layer.getFrameIndex(window.name); //先得到当前iframe层的索引
+                parent.layer.close(index);
+                parent.location.reload();
+              }
+              if(d.error){
+                layer.msg(d.error);
+              }
+            },
+            failure:function(){
+              layer.msg('faialure');
+            }
+          });    
+    } else{
+      alert('請填寫完全部題目！');
 
-     },
-     dataType: 'json',
-     success: function(d) {
-       if(d) {
-         console.log(d);
-       }
-       if(d.success){
-         var index = parent.layer.getFrameIndex(window.name); //先得到当前iframe层的索引
-         parent.layer.close(index);
-         parent.location.reload();
-       }
-       if(d.error){
-         layer.msg(d.error);
-       }
-     },
-     failure:function(){
-       layer.msg('faialure');
-     }
-   });
+    }
+   
 })
   $('.trash_btn').click(function() {
     $(this).closest('.itemp').remove();
