e4e2cf2bb65454ae649e5f368719879c59eff69c
pony <as3305191@gmail.com>
2024-03-26 15:30:45 +0800

    up

--- js/app/question_option/list.js
+++ js/app/question_option/list.js
@@ -267,17 +267,48 @@ var QuestionoptionAppClass = (function(app) {
 	// return self
 	return app.init();
 });
+
+
+
 var QuestionnotfinishAppClass = (function(app) {
 	app.basePath = "mgmt/question_option/";
 	app.disableRowClick = true;
-
 	app.fnRowCallback1 = function(nRow, aData, iDisplayIndex, iDisplayIndexFull) {
 				// edit click
-				
+				if(!app.disableRowClick) {
+					var _rtd = $(nRow).find('td');
+					if(!app.enableFirstClickable) {
+						_rtd = _rtd.not(':first').not(':last')
+					}
+					_rtd.addClass('pointer').on('click', function(){
+						app.doEdit(aData.id);
+
+						// remove all highlight first
+						$(this).parent().parent().find('tr').removeClass('active');
+
+						app._lastPk = aData.id;
+						app._tr = $(this).parent();
+						setTimeout(function(){
+							app._tr.addClass('active');
+						}, 100);
+					});
+				}
+
+				$(nRow).find("a").eq(0).click(function() {
+					app.setDelId(aData.id);
+					$('#modal_do_delete')
+					.prop('onclick',null)
+					.off('click')
+					.on('click', function(){
+						app.doDelItem();
+					});
+				});
 
 				if(app._lastPk && aData.id && app._lastPk == aData.id) {
 					$(nRow).addClass('active');
 				}
+
+			
 
 				if(app.fnRowCallbackExt) {
 					app.fnRowCallbackExt(nRow, aData, iDisplayIndex, iDisplayIndexFull);
@@ -304,47 +335,37 @@ var QuestionnotfinishAppClass = (function(app) {
 	app.init = function() {
 		app.mDtTable = $('#dt_list_not_finish').DataTable($.extend(app.dtConfig,{
 			ajax : {
-				url : baseUrl + app.basePath + 'get_data_not_finish',
+				url : baseUrl + app.basePath + '/get_data_not_finish',
 				data : function(d) {
-					d.item_id = $('#export_item_id').val();
-					// d.s_news_style = $('#s_news_style').val();
+					d.item_id = $('#export_item_id').val()
+					// d.lottery_no = $('#lottery_select').val();
 					return d;
 				},
 				dataSrc : 'items',
 				dataType : 'json',
 				type : 'post'
 			},
+
 			pageLength: 10,
-			columns : [ {
-				data : 'user_name'
-			},],
+			
+			columns : [{
+					data : 'user_name'
+				},],
 			ordering: false,
 			order : [[0, "desc"]],
-			columnDefs :[{
-				targets : 0,
-				data : null,
-				defaultContent : '',
-				searchable : false,
-				orderable : false,
-				width : "5%",
-				className : ''
-			}, {
+			columnDefs : [{
 				"targets" : [0],
 				"orderable" : false
-			},
-		],
+			}],
 
 			footerCallback: function (row, data, start, end, display ) {
 				var api = this.api();
 			}
-		}));	
 
+		}));
 
 		// data table actions
 		app.dtActions();
-
-		// get year month list
-	
 
 		app.mDtTable.on( 'xhr', function () {
 		    var json = app.mDtTable.ajax.json();
@@ -352,12 +373,14 @@ var QuestionnotfinishAppClass = (function(app) {
 		});
 
 		app.tableReload();
+
 		return app;
 	};
-
+	
 	// return self
 	return app.init();
 });
+
 var QuestioneachdetailAppClass = (function(app) {
 	app.basePath = "mgmt/question_option/";
 	app.disableRowClick = true;
