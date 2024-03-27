04a308b469ea65e5ee6b0835ef5f456bb36f5a50
pony <as3305191@gmail.com>
2024-03-27 11:18:46 +0800

    up

--- application/controllers/mgmt/Message.php
+++ application/controllers/mgmt/Message.php
@@ -133,11 +133,14 @@ class Message extends MY_Mgmt_Controller {
 					$not_read = $this -> dao -> find_not_read($data['login_user_id'],$each_offline_user->id);
 					$each_offline_user->no_read =$not_read;
 				}
-				if($not_read>0){
-					$res['offline_users'][] = $all_users;
-				} else{
-					$res['offline_users_no_noread'][] = $all_users;
+				foreach($all_users as $each_offline_user){
+					if($each_offline_user->no_read>0){
+						$res['offline_users'][] = $each_offline_user;
+					} else{
+						$res['offline_users_no_noread'][] = $each_offline_user;
+					}
 				}
+				
 			}
 		}
 			if(!empty($online_id_array)){
