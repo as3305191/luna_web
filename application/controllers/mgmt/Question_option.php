a55320248665edcd7cadc2c0a87f3003bfa666f4
pony <as3305191@gmail.com>
2024-03-27 14:32:24 +0800

    up

--- application/controllers/mgmt/Question_option.php
+++ application/controllers/mgmt/Question_option.php
@@ -209,7 +209,10 @@ class Question_option extends MY_Mgmt_Controller {
 		$this -> to_json($res);
 	}
 	public function export_excel(){
-		$fileName = '點餐表'.date('Y-m-d H:i:s').'.xls';
+		$id = $this -> get_get('id');
+		$question_ans_list = $this -> question_ans_dao -> find_by_all_p($id);
+	
+		$fileName = $question_ans_list->question_style_name."(".$question_ans_list->user_name.")-".date('Y-m-d H:i:s').'.xls';
 
 		$objPHPExcel = new PHPExcel();
 		// $objPHPExcel->setActiveSheetIndex(0);
@@ -219,75 +222,72 @@ class Question_option extends MY_Mgmt_Controller {
 		// $objPHPExcel->getActiveSheet()->SetCellValue('D1', '其他設定');
 	
 	
-		$sheet = $objPHPExcel->getActiveSheet();
+		// $sheet = $objPHPExcel->getActiveSheet();
 
-		$items = $this -> menu_dao -> find_all_open_without_stop();
-		//Start adding next sheets
-		
-		for ($i=0;$i<count($items);$i++) {
+		switch ($question_ans_list->qs_id) {
+			case 1:
+				// for ($i=0;$i<count($question_ans_list);$i++) {
 	
-		  // Add new sheet
-		  $objWorkSheet = $objPHPExcel->createSheet($i); //Setting index when creating
-	
-		  //Write cells
-		  if($items[$i]->menu_style_id==4){
-			$objWorkSheet->setCellValue('A1', '名字')
-						->setCellValue('B1', '品項')
-						->setCellValue('C1', '糖')
-						->setCellValue('D1', '冰')
-						->setCellValue('E1', '備註')
-						->setCellValue('F1', '金額');
+					// Add new sheet
+					$objWorkSheet = $objPHPExcel->createSheet(0); //Setting index when creating
+			  
+					//Write cells
+					  $objWorkSheet->setCellValue('A1', '工作型態')
+								  ->setCellValue('B1', '性別')
+								  ->setCellValue('C1', '年資')
+								  ->setCellValue('D1', '平均每週工作時數')
+								  ->setCellValue('E1', '班別')
+								  ->setCellValue('F1', '曾經遭遇的暴力攻擊情境')
+								  ->setCellValue('G1', '公司提供有關預防暴力攻擊之衛生教育訓練')
+								  ->setCellValue('H1', '我清楚了解如何辨識職場發生的暴力危害')
+								  ->setCellValue('I1', '我清楚了解如何進行暴力危害的風險評估')
+								  ->setCellValue('J1', '我清楚了解如何避免或遠離暴力危害事件')
+								  ->setCellValue('K1', '我清楚了解暴力危害事件發生時如何尋求支援管道')
+								  ->setCellValue('L1', '我具備因應暴力危害事件的事務處理與執行能力');
 
-						$items_order = $this -> menu_order_dao -> find_order_by_menu($items[$i]->id);
-						$total = 0;
-						for ($j=0;$j<count($items_order);$j++) {
-							$k = $j+2;
-							$total+=intval($items_order[$j]->amount);
-							$objWorkSheet->setCellValue('A'.$k , $items_order[$j]->user_name)
-										->setCellValue('B'.$k , $items_order[$j]->order_name)
-										->setCellValue('C'.$k , $items_order[$j]->sugar)
-										->setCellValue('D'.$k , $items_order[$j]->ice)
-										->setCellValue('E'.$k , $items_order[$j]->note)
-										->setCellValue('F'.$k , $items_order[$j]->amount);
-						}
-						$last = count($items_order)+2;
-						$objWorkSheet->setCellValue('A'.$last , '')
-										->setCellValue('B'.$last , '')
-										->setCellValue('C'.$last, '')
-										->setCellValue('D'.$last, '')
-										->setCellValue('E'.$last, '')
-										->setCellValue('F'.$last , '總金額')
-										->setCellValue('G'.$last ,  $total);
+								//   $items_order = $this -> menu_order_dao -> find_order_by_menu($items[$i]->id);
+								  $total = 0;
+								  for ($j=0;$j<count($question_ans_list);$j++) {
+									  $k = $j+2;
+									  $objWorkSheet->setCellValue('A'.$k , $question_ans_list[$j]->user_name)
+												  ->setCellValue('B'.$k , $question_ans_list[$j]->order_name)
+												  ->setCellValue('C'.$k , $question_ans_list[$j]->sugar)
+												  ->setCellValue('D'.$k , $question_ans_list[$j]->ice)
+												  ->setCellValue('E'.$k , $question_ans_list[$j]->note)
+												  ->setCellValue('F'.$k , $question_ans_list[$j]->amount)
+												  ->setCellValue('G'.$k , $question_ans_list[$j]->amount)
+												  ->setCellValue('H'.$k , $question_ans_list[$j]->amount)
+												  ->setCellValue('I'.$k , $question_ans_list[$j]->amount)
+												  ->setCellValue('J'.$k , $question_ans_list[$j]->amount)
+												  ->setCellValue('K'.$k , $question_ans_list[$j]->amount)
+												  ->setCellValue('L'.$k , $question_ans_list[$j]->amount);
 
-
-
-		  } else{
-			$objWorkSheet->setCellValue('A1', '名字')
-					   ->setCellValue('B1', '品項')
-					   ->setCellValue('C1', '備註')
-					   ->setCellValue('D1', '金額');
-			$items_order = $this -> menu_order_dao -> find_order_by_menu($items[$i]->id);
-			$total = 0;
-			for ($j=0;$j<count($items_order);$j++) {
-				$k = $j+2;
-				$total+=intval($items_order[$j]->amount);
-				$objWorkSheet->setCellValue('A'.$k , $items_order[$j]->user_name)
-							->setCellValue('B'.$k , $items_order[$j]->order_name)
-							->setCellValue('C'.$k , $items_order[$j]->note)
-							->setCellValue('D'.$k , $items_order[$j]->amount);
-			}
-			$last = count($items_order)+2;
-			$objWorkSheet->setCellValue('A'.$last , '')
-							->setCellValue('B'.$last , '')
-							->setCellValue('C'.$last, '')
-							->setCellValue('D'.$last , '總金額')
-							->setCellValue('E'.$last ,  $total);
-		  }
+								  }
+								//   $last = count($items_order)+2;
+								//   $objWorkSheet->setCellValue('A'.$last , '')
+								// 				  ->setCellValue('B'.$last , '')
+								// 				  ->setCellValue('C'.$last, '')
+								// 				  ->setCellValue('D'.$last, '')
+								// 				  ->setCellValue('E'.$last, '')
+								// 				  ->setCellValue('F'.$last , '總金額')
+								// 				  ->setCellValue('G'.$last ,  $total);
 		  
-		  // Rename sheet
-		  $objWorkSheet->setTitle($items[$i]->menu_name);
-		}
-	
+		  
+						$objWorkSheet->setTitle($question_ans_list->question_style_name);
+				// }
+			  
+			  break;
+			case 2:
+			  //code block;
+			  break;
+			case 3:
+			  //code block
+			  break;
+			default:
+			  //code block
+		  }
+
+		
 		$objWriter = new PHPExcel_Writer_Excel5($objPHPExcel);
 
 		// download file
