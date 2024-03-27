3f253e482ead55ddccb5e6a3b843253134d1f04a
pony <as3305191@gmail.com>
2024-03-27 10:45:02 +0800

    up

--- server.php
+++ server.php
@@ -307,7 +307,7 @@ class Sock{
                 }
             }
 
-            $sql="SELECT id FROM `users` WHERE status='0'";
+            $sql="SELECT id FROM `users` WHERE status='0'  order by status desc";
             $select=mysqli_query($link,$sql);
             foreach($select as $each){
                 $map_all_user[]=$each['id'];
@@ -522,7 +522,7 @@ class Sock{
         $now_online=array_diff($this->online_user,$map_all_user);
         $this->online_user=$now_online;
         $ar['now_online']=$this->online_user;
-        $sql1="SELECT id FROM `users` WHERE status='0' order by status asc";
+        $sql1="SELECT id FROM `users` WHERE status='0'";
         $select1=mysqli_query($link,$sql1);
         foreach($select1 as $each){
             $map_all_user1[]=$each['id'];
