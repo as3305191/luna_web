10f3972db2b4caf7c568af4bd30828c7e82a0a7a
pony <as3305191@gmail.com>
2024-03-27 10:02:28 +0800

    up

--- application/models/Users_dao.php
+++ application/models/Users_dao.php
@@ -740,12 +740,14 @@ class Users_dao extends MY_Model {
 		$this -> db -> select('_m.role_id');
 
 
+		$this -> db -> group_start();
 		$this -> db -> where('_m.role_id>',3);
-		$this -> db -> or_where('_m.role_id<=',27);
-		
-		$this -> db -> or_where('_m.role_id>=',69);
-		$this -> db -> or_where('_m.role_id<=',74);
-
+		$this -> db -> where('_m.role_id<=',37);
+		$this -> db -> or_group_start();
+		$this -> db -> where('_m.role_id>=',69);
+		$this -> db -> where('_m.role_id<=',74);
+		$this -> db -> group_end();
+		$this -> db -> group_end();
 	
 		$query = $this -> db -> get();
 		$list = $query -> result();
