2b4d2bc7fb2b3c79cf0aa918e97441eb0489d9ca
pony <as3305191@gmail.com>
2024-03-27 09:03:32 +0800

    up

--- application/models/Question_ans_dao.php
+++ application/models/Question_ans_dao.php
@@ -159,10 +159,9 @@ class Question_ans_dao extends MY_Model {
 		}
 	}
 
-	function find_all_not_finish($data, $is_count = FALSE) {
+	function find_all_not_finish($data) {
 
-		$start = $data['start'];
-		$limit = $data['length'];
+		
 		$item_id = $data['item_id'];
 		// select
 		$this -> db -> from("$this->table_name as _m");
@@ -172,22 +171,10 @@ class Question_ans_dao extends MY_Model {
 		
 		// $this -> db -> join("users u", "u.id = _m.user_id", "left");
 		
-		
-		if(!$is_count) {
-			$this -> db -> limit($limit, $start);
-		}
-		
 		$this -> db -> where('_m.question_option_id',$item_id);
 		
-		// $this -> db -> order_by('_m.id','desc');
-
-		// query results
-		if(!$is_count) {
-			$query = $this -> db -> get();
-			return $query -> result();
-		} else {
-			return $this -> db -> count_all_results();
-		}
+		$query = $this -> db -> get();
+		return $query -> result();
 
 	}
 }
