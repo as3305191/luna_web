82198a7c6ae5b520e0ed8f840e34cb5c303eecaa
pony <as3305191@gmail.com>
2024-03-28 11:03:39 +0800

    up

--- application/controllers/mgmt/Question_option.php
+++ application/controllers/mgmt/Question_option.php
@@ -356,18 +356,23 @@ class Question_option extends MY_Mgmt_Controller {
 					$objWorkSheet = $objPHPExcel->createSheet($i); //Setting index when creating
 			  
 					//Write cells
-					  $objWorkSheet->setCellValue('A1', '工作型態')
-								  ->setCellValue('B1', '性別')
-								  ->setCellValue('C1', '年資')
-								  ->setCellValue('D1', '平均每週工作時數')
-								  ->setCellValue('E1', '班別')
-								  ->setCellValue('F1', '曾經遭遇的暴力攻擊情境')
-								  ->setCellValue('G1', '公司提供有關預防暴力攻擊之衛生教育訓練')
-								  ->setCellValue('H1', '我清楚了解如何辨識職場發生的暴力危害')
-								  ->setCellValue('I1', '我清楚了解如何進行暴力危害的風險評估')
-								  ->setCellValue('J1', '我清楚了解如何避免或遠離暴力危害事件')
-								  ->setCellValue('K1', '我清楚了解暴力危害事件發生時如何尋求支援管道')
-								  ->setCellValue('L1', '我具備因應暴力危害事件的事務處理與執行能力');
+					  $objWorkSheet->setCellValue('A1', '你常覺得疲勞嗎？')
+								  ->setCellValue('B1', '你常覺得身體上體力透支嗎？')
+								  ->setCellValue('C1', '你常覺得情緒上心力交瘁嗎？')
+								  ->setCellValue('D1', '你常會覺得，「我快要撐不下去了」嗎?')
+								  ->setCellValue('E1', '你常覺得精疲力竭嗎？')
+								  ->setCellValue('F1', '你常常覺得虛弱，好像快要生病了嗎？')
+								  ->setCellValue('G1', '個人相關疲勞分數')
+								  ->setCellValue('H1', '個人相關過勞分級')
+								  ->setCellValue('I1', '你的工作會令人情緒上心力交瘁嗎？')
+								  ->setCellValue('J1', '你的工作會讓你覺得快要累垮了嗎？')
+								  ->setCellValue('K1', '你的工作會讓你覺得挫折嗎？')
+								  ->setCellValue('L1', '工作一整天之後，你覺得精疲力竭嗎？')
+								  ->setCellValue('M1', '上班之前只要想到又要工作一整天，你就覺得沒力嗎？')
+								  ->setCellValue('N1', '上班時你會覺得每一刻都很難熬嗎？')
+								  ->setCellValue('O1', '不工作的時候，你有足夠的精力陪朋友或家人嗎？')
+								  ->setCellValue('p1', '工作相關疲勞分數')
+								  ->setCellValue('Q1', '工作相關過勞分級');
 
 								//   $items_order = $this -> menu_order_dao -> find_order_by_menu($items[$i]->id);
 								  for ($j=0;$j<count($question_ans_list);$j++) {
@@ -378,12 +383,17 @@ class Question_option extends MY_Mgmt_Controller {
 												  ->setCellValue('D'.$k , $question_ans_list[$j]->q4)
 												  ->setCellValue('E'.$k , $question_ans_list[$j]->q5)
 												  ->setCellValue('F'.$k , $question_ans_list[$j]->q6)
-												  ->setCellValue('G'.$k , $question_ans_list[$j]->q7)
-												  ->setCellValue('H'.$k , $question_ans_list[$j]->q8)
-												  ->setCellValue('I'.$k , $question_ans_list[$j]->q9)
-												  ->setCellValue('J'.$k , $question_ans_list[$j]->q10)
-												  ->setCellValue('K'.$k , $question_ans_list[$j]->q11)
-												  ->setCellValue('L'.$k , $question_ans_list[$j]->q12);
+												  ->setCellValue('G'.$k , $question_ans_list[$j]->q1o)
+												  ->setCellValue('H'.$k , $question_ans_list[$j]->q2o)
+												  ->setCellValue('I'.$k , $question_ans_list[$j]->q7)
+												  ->setCellValue('J'.$k , $question_ans_list[$j]->q8)
+												  ->setCellValue('K'.$k , $question_ans_list[$j]->q9)
+												  ->setCellValue('L'.$k , $question_ans_list[$j]->q10)
+												  ->setCellValue('M'.$k , $question_ans_list[$j]->q11)
+												  ->setCellValue('N'.$k , $question_ans_list[$j]->q12)
+												  ->setCellValue('O'.$k , $question_ans_list[$j]->q13)
+												  ->setCellValue('P'.$k , $question_ans_list[$j]->q3o)
+												  ->setCellValue('Q'.$k , $question_ans_list[$j]->q4o);
 
 								  }
 								//   $last = count($items_order)+2;
