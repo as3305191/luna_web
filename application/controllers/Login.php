3a892e353229d9f9475f4c6cab9e375f3ac379f8
pony <as3305191@gmail.com>
2024-04-02 10:03:04 +0800

    up

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -87,8 +87,8 @@ class Login extends MY_Base_Controller {
 					$uniqueId = uniqid(rand(), TRUE);
 					$s_uid = md5($uniqueId);
 					$this -> session -> set_userdata("s_uid", $s_uid);
-					$this -> users_dao -> update(array('token' => $s_uid), $user -> id);
 					$this -> session -> set_userdata("lang", $user->lang);
+					$this -> users_dao -> update(array('token' => $s_uid), $user -> id);
 					$user_role_list = $this -> d_dao -> find_by('id', $user->role_id);
 
 					// if($user->role_id>2){
