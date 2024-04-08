65e929fc3e1afefa2a6d111b3c513f73382f86a8
pony <as3305191@gmail.com>
2024-04-08 08:43:25 +0800

    up

--- application/views/layout/show_que1.php
+++ application/views/layout/show_que1.php
@@ -13,7 +13,7 @@
 </style>
 <div class="col-xs-12" style="padding:20px">
     <div class="col-xs-12">
-        <span style="font-size:12pt"><?= $lang->_lang['q1_vh'] ?></span>
+        <span style="font-size:12pt"><?= $this->_lang['q1_vh'] ?></span>
     </div>
     <hr/>
     <input type="hidden" id="qid" value="<?= isset($qid) ? $qid : '' ?>" />
