a44ab3493c0f87021c1c64aa0d120b60f34e2f44
pony <as3305191@gmail.com>
2024-04-10 09:53:21 +0800

    up

--- application/models/Question_option_dao.php
+++ application/models/Question_option_dao.php
@@ -161,5 +161,29 @@ class Question_option_dao extends MY_Model {
 		return $query -> result();
 
 	}
+	function find_by_all_item($id) {
+
+		// select
+		$this -> db -> from("$this->table_name as _m");
+
+		$this -> db -> select('_m.*');
+		$this -> db -> select('u.user_name as user_name');
+		$this -> db -> select('qa.*');
+		$this -> db -> select('qs.question_style_name as question_style_name');
+		$this -> db -> select('qs.id as qs_id');
+		$this -> db -> select('d.name as dep_name');
+
+		$this -> db -> join("users u", "u.id = _m.user_id", "left");
+		$this -> db -> join("department d", "d.id = u.role_id", "left");
+		$this -> db -> join("question_ans qa", "qa.question_option_id = _m.id", "left");
+		$this -> db -> join("question_style qs", "qs.id = qo.question_style_id", "left");
+
+		$this -> db -> where('_m.question_option_id',$id);
+		
+		$query = $this -> db -> get();
+		return $query -> result();
+
+	}
+
 }
 ?>
