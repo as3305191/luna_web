85153f2902aaa0c9f8ff02c31bdd2d1325a2d1d1
pony <as3305191@gmail.com>
2024-04-11 13:55:36 +0800

    update

--- application/views/layout/show_que5.php
+++ application/views/layout/show_que5.php
@@ -136,13 +136,11 @@
   function change_yn(q_num){
     if($('input:radio[name="q'+q_num+'_o1"]:checked').val()=='否'){
       for(var $j=3;$j<=21;$j++){
-          $('#q'+q_num+'_o1_'+$j).addClass("disabledinput");
-          $('.disabledinput').disable(true);
+          $('#q'+q_num+'_o1_'+$j).addClass("disabledinput").disable(true);
       }
     }else{
       for(var $j=3;$j<=21;$j++){
-          $('.disabledinput').disable(false);
-          $('#q'+q_num+'_o1_'+$j).removeClass("disabledinput");
+          $('#q'+q_num+'_o1_'+$j).disable(false).removeClass("disabledinput");
       }
     }
   }
