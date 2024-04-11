e64c73cc6f6e5129375a1808d5adf342a716e89c
pony <as3305191@gmail.com>
2024-04-11 08:42:05 +0800

    up

--- application/views/mgmt/question_for_user/list.php
+++ application/views/mgmt/question_for_user/list.php
@@ -27,13 +27,6 @@
 					<button class="btn_active btn-success text-light  menu_btn menu_0" style="border-radius: 5px; padding: 10px; width: 300px; height: 48px;" onclick="q_click(<?= $each['question_style_id']?>,<?= $each['id'] ?>)"><?=$each['question_title']?></button>
 				<?php endforeach ?>
 			<?php endif?>
-			<!-- <button class="btn_active btn-success text-light  menu_btn menu_0" style="border-radius: 5px; padding: 10px; width: 220px; height: 48px;" onclick="q_click(2,7)">問卷2</button>
-			<button class="btn_active btn-success text-light  menu_btn menu_0" style="border-radius: 5px; padding: 10px; width: 220px; height: 48px;" onclick="q_click(3,8)">問卷3</button>
-			<button class="btn_active btn-success text-light  menu_btn menu_0" style="border-radius: 5px; padding: 10px; width: 220px; height: 48px;" onclick="q_click(4,9)">問卷4</button>
-			<button class="btn_active btn-success text-light  menu_btn menu_0" style="border-radius: 5px; padding: 10px; width: 220px; height: 48px;" onclick="q_click(5,10)">問卷5</button> -->
-
-			<!-- <button class="btn-success text-light btn_active menu_1" style="border-radius: 5px; padding: 10px; width: 160px; height: 48px;" onclick="backStep1()">菜單1</button>
-			<button class="btn-light text-light btn_unsuccess menu_3" style="border-radius: 5px; padding: 10px; width: 160px; height: 48px;">菜單3</button> -->
 		</div>
 
 				
@@ -107,7 +100,7 @@
 					type:2,
 					title:'',
 					closeBtn:0,
-					area:['1200px','700px'],
+					area:['1500px','700px'],
 					shadeClose:true,
 					content:'<?=base_url()?>'+url
 				})
