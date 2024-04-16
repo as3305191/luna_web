631637fa3cddfe1dfbd6b34ab218c8048fcb5034
pony <as3305191@gmail.com>
2024-04-16 16:54:03 +0800

    up

--- application/views/layout/show_que5.php
+++ application/views/layout/show_que5.php
@@ -268,11 +268,13 @@
 
 
               for(var $j=1;$j<=7;$j++){
+                var nq_num = q_num+$j;
                 if($('input:radio[name="q'+q_num+'_o1"]:checked').val()=='否'){
-                  $('.q'+q_num+$j).addClass("disabledinput").attr('disabled',true).attr('checked',false);
-                  $('.q'+q_num+$j+'_o').val('');
+                  $('.q'+nq_num).addClass("disabledinput").attr('disabled',true).attr('checked',false);
+                  $('.q'+nq_num+'_o').val('');
+                  console.log(nq_num);
+                }
               }
-            }
 
 
             // break;
