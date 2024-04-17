2b1f1348d76395ea5ce967475fc4afc9ed4216cf
pony <as3305191@gmail.com>
2024-04-17 15:37:41 +0800

    up

--- application/views/layout/show_que5.php
+++ application/views/layout/show_que5.php
@@ -178,7 +178,7 @@
       
       if(j==1){
         each_dom_script='$(\'input:radio[name="q'+$i_1+'_o'+$j_1+'"]\').on(\'change\', function(){'+
-          'obj = document.getElementsByName("q'+$i_1+'_o'+$j_1+'");'+
+          'obj = document.getElementsByName("q'+$i_1+'");'+
           'q'+$num+' = [];'+
             'for (i in obj) {'+
                 'if (obj[i].checked){'+
