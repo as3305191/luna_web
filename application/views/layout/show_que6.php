a5e119405e709de31a1710b3ead4f9726c8c35f8
pony <as3305191@gmail.com>
2024-04-18 15:24:59 +0800

    up

--- application/views/layout/show_que6.php
+++ application/views/layout/show_que6.php
@@ -18,21 +18,6 @@
     <hr/>
     <input type="hidden" id="qid" value="<?= isset($qid) ? $qid : '' ?>" />
     <table class="col-xs-12">
-		<thead >
-			<tr>
-				<td class="min50">序號</td>
-				<td>1</td>
-				<td>2</td>
-				<td>3</td>
-				<td>4</td>
-				<td>5</td>
-				<td>6</td>
-				<td>7</td>
-				<td>8</td>
-				<td>9</td>
-				<td>10</td>
-			</tr>
-		</thead>
 		<tbody id="example_tbody">
 			<tr>
 				<td class="min50">項目</td>
@@ -69,6 +54,8 @@
 			</tr>
 		</tbody>
 	</table>
+  其他意見有問有答：<textarea class="form-control" name="q1o"></textarea>
+
     <div class="col-xs-12 no-padding" style="margin-top:20px">
         <div class="col-xs-6 no-padding">
             <button type="button" class="btn btn-secondary cancel" style="width:120px;height:40px;float:left">取消</button>
@@ -97,96 +84,11 @@
   var  q8 = null;
   var  q9 = null;
   var  q10 = null;
-  var  q11 = null;
-  var  q12 = null;
   
   var  q1o = '無';
   var  q2o = '無';
   var  q3o = '無';
   var is_ok=false;
-  $('input:radio[name="q1_1"]').on('change', function(){
-    q1 = $('input:radio[name="q1_1"]:checked').val();
-   
-  });
-  $('input:radio[name="q1_2"]').on('change', function(){
-    q2 = $('input:radio[name="q1_2"]:checked').val();
-   
-  });
-  $('input:radio[name="q1_3"]').on('change', function(){
-    q3 = $('input:radio[name="q1_3"]:checked').val();
-   
-  });
-  $('input:radio[name="q1_4"]').on('change', function(){
-    q4 = $('input:radio[name="q1_4"]:checked').val();
-   
-  });
-  $('input:radio[name="q1_5"]').on('change', function(){
-    q5 = $('input:radio[name="q1_5"]:checked').val();
-   
-  });
-  
-$("input[name='q1_6']").on('change', function(){
-    obj = document.getElementsByName("q1_6");
-    q6 = [];
-    for (i in obj) {
-        if (obj[i].checked){
-          q6.push(obj[i].value);
-        }
-    }
-
-    // console.log(q6);
-  });
-  $("input[name='q1_7']").on('change', function(){
-    obj = document.getElementsByName("q1_7");
-    q7 = [];
-
-    for (i in obj) {
-      if (obj[i].checked){
-        q7.push(obj[i].value);
-      }
-    }
-    // console.log(q7);
-
-  });
-  $('input:radio[name="q1_8"]').on('change', function(){
-    q8 = $('input:radio[name="q1_8"]:checked').val();
-   
-  });
-  $('input:radio[name="q1_9"]').on('change', function(){
-    q9 = $('input:radio[name="q1_9"]:checked').val();
-   
-  });
-  $('input:radio[name="q1_10"]').on('change', function(){
-    q10 = $('input:radio[name="q1_10"]:checked').val();
-   
-  });
-  $('input:radio[name="q1_11"]').on('change', function(){
-    q11 = $('input:radio[name="q1_11"]:checked').val();
-   
-  });
-  $('input:radio[name="q1_12"]').on('change', function(){
-    q12 = $('input:radio[name="q1_12"]:checked').val();
-   
-  });
- 
-
-
-  $('[name="q1_1o"]').on('change', function(){
-    q1o = $('[name="q1_1o"]').val();
-    // console.log(q1o);
-   
-  });
-  $('[name="q1_5o"]').on('change', function(){
-    q2o = $('[name="q1_5o"]').val();
-    // console.log(q2o);
-
-  });
-  $('[name="q1_6o"]').on('change', function(){
-    q3o = $('[name="q1_6o"]').val();
-    // console.log(q3o);
-
-  });
-
 
   function sum(id_name) {
 		onclick_option_val = $('select[name="'+id_name+'"]').val();
@@ -222,21 +124,19 @@ $("input[name='q1_6']").on('change', function(){
             type: 'POST',
             data: {
               qid:$('#qid').val(),
-              q1 : q1,
-              q2 : q2,
-              q3 : q3,
-              q4 : q4,
-              q5 : q5,
-              q6 : q6.toString(),
-              q7 : q7.toString(),
-              q8 : q8,
-              q9 : q9,
-              q10 : q10,
-              q11 : q11,
-              q12 : q12,
-              q1o : q1o,
-              q2o : q2o,
-              q3o : q3o,
+              q1 : $('select[name="s1"]').val(),
+              q2 : $('select[name="s2"]').val(),
+              q3 : $('select[name="s3"]').val(),
+              q4 : $('select[name="s4"]').val(),
+              q5 : $('select[name="s5"]').val(),
+              q6 : $('select[name="s6"]').val(),
+              q7 : $('select[name="s7"]').val(),
+              q8 : $('select[name="s8"]').val(),
+              q9 : $('select[name="s9"]').val(),
+              q10 : $('select[name="s10"]').val(),
+
+              q1o : $('[name="q1o"]').text(),
+            
             },
             dataType: 'json',
             success: function(d) {
