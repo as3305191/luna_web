d433b325bf5ffefbf1ccd3f265302496e5c40d31
pony <as3305191@gmail.com>
2024-04-19 08:34:13 +0800

    up

--- application/controllers/mgmt/Question_for_user.php
+++ application/controllers/mgmt/Question_for_user.php
@@ -134,36 +134,21 @@ class Question_for_user extends MY_Mgmt_Controller {
 					"question_title" => $title_dep,
 				);
 			} else{
-				if(!empty($under_role_list)){
-					$question_option_open_list_dep = $this -> question_ans_dao -> find_all_not_write_dep($each_by_dep->id,$each->id);
-					if(!empty($question_option_open_list_dep)){
-						if($each->note==''){
-							$title_dep=$each->qs_name.'-'.$each_by_dep->name;
-						} else{
-							$title_dep=$each->qs_name.'('.$each_by_dep->name.')-'.$each->note;
-						}
-						$data['question_option_id_list_by_dep'][] = array (
-							"id" => $each->id,
-							"question_ans_id" => $question_option_open_list_dep->id,
-							"role_id" => $login_user->role_id,
-							"question_style_id" => 6,
-							"question_title" => $title_dep,
-						);
+				$question_option_open_list_dep = $this -> question_ans_dao -> find_all_not_write_dep($each_by_dep->id,$each->id);
+				if(!empty($question_option_open_list_dep)){
+					if($each->note==''){
+						$title_dep=$each->qs_name.'-'.$each_by_dep->name;
 					} else{
-						if($each->note==''){
-							$title_dep=$each->qs_name.'-'.$each_by_dep->name;
-						} else{
-							$title_dep=$each->qs_name.'('.$each_by_dep->name.')-'.$each->note;
-						}
-						$data['question_option_id_list_by_dep'][] = array (
-							"id" => $each->id,
-							"question_ans_id" => 0,
-							"role_id" => $login_user->role_id,
-							"question_style_id" => 6,
-							"question_title" => $title_dep,
-						);
+						$title_dep=$each->qs_name.'('.$each_by_dep->name.')-'.$each->note;
 					}
-				}
+					$data['question_option_id_list_by_dep'][] = array (
+						"id" => $each->id,
+						"question_ans_id" => $question_option_open_list_dep->id,
+						"role_id" => $login_user->role_id,
+						"question_style_id" => 6,
+						"question_title" => $title_dep,
+					);
+				} 
 			}
 		}
 		
