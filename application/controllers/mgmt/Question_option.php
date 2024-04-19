cb280be92ba298b29e3f7308bb6e17de60b19913
pony <as3305191@gmail.com>
2024-04-19 13:00:31 +0800

    up

--- application/controllers/mgmt/Question_option.php
+++ application/controllers/mgmt/Question_option.php
@@ -881,8 +881,8 @@ class Question_option extends MY_Mgmt_Controller {
 		$white_table->addCell(10000,null,8)->addText($list->dep_name,array('bold' => true, 'size'=>14),array('align'=>'right'));
 
 		$table->addRow(4000);
-		$table->addCell(10000,null,8)->addText('員工滿意度(A(10分); B(9分); C(8分); D(7分); E(6分)):\r\n'.
-		'系統:'.$items[0].','.'制度:'.$items[1].','.'成本:'.$items[2].','.'協助:'.$items[3].','.'彈性:'.$items[4].'\r\n'.
+		$table->addCell(10000,null,8)->addText('員工滿意度(A(10分); B(9分); C(8分); D(7分); E(6分)):\n'.
+		'系統:'.$items[0].','.'制度:'.$items[1].','.'成本:'.$items[2].','.'協助:'.$items[3].','.'彈性:'.$items[4].'\n'.
 		'系統:'.$items[5].','.'訓練:'.$items[6].','.'福利:'.$items[7].','.'分享:'.$items[8].','.'公平:'.$items[9],null,array('align'=>'left'));
 		
 
