85c875cc79a74d662cf6fda49b0632869138e222
pony <as3305191@gmail.com>
2024-04-22 10:53:43 +0800

    up

--- application/controllers/mgmt/Question_option.php
+++ application/controllers/mgmt/Question_option.php
@@ -57,11 +57,9 @@ class Question_option extends MY_Mgmt_Controller {
 				foreach($items_list as $each_item){
 					$f_user_id_list[] = $each_item->user_id;
 					foreach($all_user_list as $each_user){
-						if(in_array($each_user->id,$f_user_id_list)){
-						} else{
+						if(in_array($each_user->id,$f_user_id_list)==0){
 							$res['items'][] = $each_user;
-						}
-	
+						} 
 					}
 				}
 				
