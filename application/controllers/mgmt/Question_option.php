75c139fa57c17a560021634e382d5e17327aae71
pony <as3305191@gmail.com>
2024-04-22 10:54:46 +0800

    up

--- application/controllers/mgmt/Question_option.php
+++ application/controllers/mgmt/Question_option.php
@@ -67,7 +67,7 @@ class Question_option extends MY_Mgmt_Controller {
 				$res['items'] = $all_user_list;
 			}
 
-			$res['f_user_id_list'] = in_array(4,$f_user_id_list);
+			$res['f_user_id_list'] = in_array(1,$f_user_id_list);
 		} else{
 			if($q_style_list->for_dep==1){
 				$items_list_for_dep= array();
