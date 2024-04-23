123dbefb628d899e3644ea754f691226e09313c4
pony <as3305191@gmail.com>
2024-04-23 09:54:18 +0800

    up

--- application/controllers/mgmt/Question_option.php
+++ application/controllers/mgmt/Question_option.php
@@ -741,9 +741,11 @@ class Question_option extends MY_Mgmt_Controller {
 					$objWorkSheet->setCellValue($all_cell_name[0].$new_num, $cell_title[$i]); 
 			    }
 				for ($j=0;$j<count($question_ans_list);$j++){//每7 option為1題
-					$new_num = $j+1;
-					$new_num_2 = $j+2;
-					$objWorkSheet->setCellValue($all_cell_name[$new_num].$new_num_2, $question_ans_list[$j]->dep_name);
+					for ($k=0;$k<count($question_ans_list[$j]);$k++){//每7 option為1題
+						$new_num = $j+1;
+						$objWorkSheet->setCellValue($all_cell_name[$new_num].$new_num, $question_ans_list[$j]->dep_name);	
+					}
+					
 
 				}
 				$objWorkSheet->setTitle($question_style->question_style_name.$note);
