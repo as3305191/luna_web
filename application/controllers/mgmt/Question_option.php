3cd2a925589ece48d01053b53e5abd453929a878
pony <as3305191@gmail.com>
2024-04-25 15:15:19 +0800

    up

--- application/controllers/mgmt/Question_option.php
+++ application/controllers/mgmt/Question_option.php
@@ -772,19 +772,19 @@ class Question_option extends MY_Mgmt_Controller {
 					if($question_ans_list[$j]->$ans_1_title==''){
 						$first_num = 0;
 					} else{
-						$first_num = implode(",", $question_ans_list[$j]->$ans_1_title);;
+						$first_num = implode(",", $question_ans_list[$j]->$ans_1_title);
 					}
 					if($question_ans_list[$j]->$ans_2_title==''){
 						$last_num = 0;
 					} else{
 						// $last_num = $question_ans_list[$j]->$ans_2_title;
-						$last_num = implode(",", $question_ans_list[$j]->$ans_2_title);;
+						$last_num = implode(",", $question_ans_list[$j]->$ans_2_title);
 
 					}
 					$ans_1 = $first_num * $last_num;
 					$objWorkSheet->setCellValue($all_cell_name[$new_num].'1', $question_ans_list[$j]->dep_name);
-					$objWorkSheet->setCellValue($all_cell_name[$new_num].'2', $first_num);
-					$objWorkSheet->setCellValue($all_cell_name[$new_num].'3', $last_num);
+					$objWorkSheet->setCellValue($all_cell_name[$new_num].'2', $question_ans_list[$j]->$ans_1_title);
+					// $objWorkSheet->setCellValue($all_cell_name[$new_num].'3', $last_num);
 
 				}
 				$objWorkSheet->setTitle($question_style->question_style_name.$note);
