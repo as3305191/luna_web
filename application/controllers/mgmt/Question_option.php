b48f6bba82a8f8591f7a3fd4b9cd57746994f029
pony <as3305191@gmail.com>
2024-04-25 12:05:29 +0800

    up

--- application/controllers/mgmt/Question_option.php
+++ application/controllers/mgmt/Question_option.php
@@ -750,6 +750,7 @@ class Question_option extends MY_Mgmt_Controller {
 						$objWorkSheet->mergeCells("A".$mergeCells.":A".$mergeCells_3)->setCellValue($all_cell_name[0].$mergeCells, $cell_title[$cell_title_i]); 
 					}
 					if($i<5){
+						$mergeCells_c = $i-1;
 						$objWorkSheet->setCellValue($all_cell_name[1].$new_num, $t1_style[$i]); 
 					} else{
 						$newt1_style_i = ($i-1)%4;
