a3005fd6cd9d7a1e2d868de01fa62245b4a3e7c9
pony <as3305191@gmail.com>
2024-04-26 14:11:07 +0800

    up

--- application/controllers/mgmt/Question_option.php
+++ application/controllers/mgmt/Question_option.php
@@ -823,7 +823,7 @@ class Question_option extends MY_Mgmt_Controller {
 										$horizontal_sum_by_num+=intval($each);
 									}
 								} else{
-									$horizontal_sum_by_num=0;
+									$horizontal_sum[$nnnew_x]=[];
 								}
 								
 								$objWorkSheet->setCellValue($all_cell_name[($new_num+1)].$nnnew_x,implode(",",$horizontal_sum[$nnnew_x]));
