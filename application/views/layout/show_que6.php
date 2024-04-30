a030f48cea015ea84adbf9aaa52b9606bdfea49c
pony <as3305191@gmail.com>
2024-04-30 09:15:23 +0800

    up

--- application/views/layout/show_que6.php
+++ application/views/layout/show_que6.php
@@ -144,7 +144,7 @@
 
   $('[name="q1o"]').on('change', function(){
     var q1o_str = $('[name="q1o"]').val();
-    var q1o_str_row_num = q1o_str.spilt("\n").length;
+    var q1o_str_row_num = q1o_str.spilt("/n").length;
     if(q1o_str.length<=630||q1o_str==''){
       str_ok=true;
       str_row_num_ok=true;
