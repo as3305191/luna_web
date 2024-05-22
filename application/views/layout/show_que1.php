ae94da9ab65951328b2c8a9a86f7a02b59375558
pony <as3305191@gmail.com>
2024-05-22 10:02:03 +0800

    up

--- application/views/layout/show_que1.php
+++ application/views/layout/show_que1.php
@@ -48,14 +48,14 @@
     <input type="radio" name="q1_5" value="需值班" id="q1_54"><label for="q1_54"><?= $this->_lang['q1_wf_od'] ?></label>
     <?= $this->_lang['q1_wf_noote'] ?><br>
     <?= $this->_lang['q1_we_v'] ?><br>
-    <input type="checkbox" name="q1_6" value="無" id="q1_66" style="color:red;"><label for="q1_66"><?= $this->_lang['q1_we_n'] ?></label><br>
+    <input type="checkbox" name="q1_6" value="無" id="q1_66" ><label for="q1_66" style="color:red;"><?= $this->_lang['q1_we_n'] ?></label><br>
     <input type="checkbox" name="q1_6" value="肢體暴力，如毆打、踢、推、捏、拉扯等" id="q1_61"><label for="q1_61"><?= $this->_lang['q1_we_pbv'] ?></label> <?= $this->_lang['q1_wf_noote'] ?><input name="q1_7o"><br>
     <input type="checkbox" name="q1_6" value="言語暴力，如辱罵、言語騷擾、冷嘲熱諷等" id="q1_62"><label for="q1_62"><?= $this->_lang['q1_we_vv'] ?></label> <?= $this->_lang['q1_wf_noote'] ?><input name="q1_8o"><br>
     <input type="checkbox" name="q1_6" value="心理暴力，如威脅、恐嚇、歧視、排擠、騷擾等" id="q1_63"><label for="q1_63"><?= $this->_lang['q1_we_pv'] ?></label> <?= $this->_lang['q1_wf_noote'] ?><input name="q1_9o"><br>
     <input type="checkbox" name="q1_6" value="性騷擾，如不當的性暗示與行為" id="q1_64"><label for="q1_64"><?= $this->_lang['q1_we_sh'] ?></label> <?= $this->_lang['q1_wf_noote'] ?><input name="q1_10o"><br>
     <input type="checkbox" name="q1_6" value="其他" id="q1_65"><label for="q1_65"><?= $this->_lang['q1_we_o'] ?></label> <input name="q1_6o"><br>
     <?= $this->_lang['q1_dfv'] ?><input name="q1_5o"><br>
-    <input type="checkbox" name="q1_7" value="沒有提供任何工作安全衛生教育訓練" id="q1_71" style="color:red;"><label for="q1_71"><?= $this->_lang['q1_nwst'] ?></label><br>
+    <input type="checkbox" name="q1_7" value="沒有提供任何工作安全衛生教育訓練" id="q1_71"><label for="q1_71" style="color:red;"><?= $this->_lang['q1_nwst'] ?></label><br>
     <input type="checkbox" name="q1_7" value="人身安全之防範" id="q1_72"><label for="q1_72"><?= $this->_lang['q1_pap'] ?></label><br>
     <input type="checkbox" name="q1_7" value="防護用具之使用" id="q1_73"><label for="q1_73"><?= $this->_lang['q1_upe'] ?></label><br>
     <input type="checkbox" name="q1_7" value="危害通識教育訓練" id="q1_74"><label for="q1_74"><?= $this->_lang['q1_dgt'] ?></label><br>
