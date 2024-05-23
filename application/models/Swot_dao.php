a723016664a7620c92471feabe7e608f9514bbbe
pony <as3305191@gmail.com>
2024-05-23 09:54:12 +0800

    up

--- application/models/Swot_dao.php
+++ application/models/Swot_dao.php
@@ -128,6 +128,8 @@ class Swot_dao extends MY_Model {
 					$this -> db -> or_group_start();
 					$this -> db -> where("_m.class_id",$d_or_c);
 					$this -> db -> group_end();
+					$this -> db -> group_end();
+
 					$this -> db -> where("_m.swot_style_id",$list_style);
 					$this -> db -> where("_m.title",$list_title);
 				}
