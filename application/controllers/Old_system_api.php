56730882fd984d88333671b62ade9da713e9cef7
pony <as3305191@gmail.com>
2024-10-15 13:56:34 +0800

    up

---
+++ application/controllers/Old_system_api.php
@@ -0,0 +1,206 @@
+<?php
+// use Lcobucci\JWT\Parser;
+defined('BASEPATH') OR exit('No direct script access allowed');
+use Lcobucci\JWT\Parser;
+class Old_system_api extends MY_Mgmt_Controller {
+	function __construct() {
+		parent::__construct();
+		// $this -> load -> model('User_msg_dao', 'dao');
+		// $this -> load -> model('Images_dao', 'img_dao');
+		// $this -> load -> model('Users_dao', 'users_dao');
+		// $this -> load -> model('User_online_dao', 'user_online_dao');
+
+	}
+
+	public function index()
+	{
+		header("Content-Type:text/html; charset=utf-8");
+		$serverName="KTX-2008D1\sqlexpress";
+		$connectionInfo=array("Database"=>"informationexc","TrustServerCertificate"=>"yes","UID"=>"exchange","PWD"=>"97238228","CharacterSet" => "UTF-8");
+		$conn=sqlsrv_connect($serverName,$connectionInfo);
+		
+		$sql = "SELECT * FROM [informationexc].[dbo].[order_store]";    
+		
+		/* Execute the query. */    
+		
+		$stmt = sqlsrv_query( $conn, $sql);    
+		
+		if ( $stmt ){    
+			 //   $id = $stmt['id']; 
+			  echo "<td>".json_encode($stmt)."</td>"; 
+			 //  echo"<td>".$stmt['name2']."</td>";
+		 } 
+		else{ 
+			 echo "Error in statement execution.\n";    
+			 die( print_r( sqlsrv_errors(), true));    
+		}    
+		
+		sqlsrv_free_stmt( $stmt);    
+		sqlsrv_close( $conn);
+		// $this -> to_json($data);
+		$this->load->view('mgmt/old_system_api/list', $data);
+	}
+
+	public function get_data() {
+		$res = array();
+		$data = $this -> get_posts(array(
+			'length',
+			'start',
+			'columns',
+			'search',
+			'order',
+		));
+		$s_data = $this -> setup_user_data(array());
+		$login_user = $this -> dao -> find_by_id($s_data['login_user_id']);
+		$items = $this -> dao -> query_fixing_ajax($data);
+		$res['items'] = $items;
+		$res['recordsFiltered'] = $this -> dao -> count_ajax($data);
+		$res['recordsTotal'] = $this -> dao -> count_all_ajax($data);
+		$this -> to_json($res);
+	}
+
+	public function insert() {
+		$res = array();
+		$me_id = $this -> get_post('me');
+		$to_message_id = $this -> get_post('f_chat_id');
+		$msg = $this -> get_post('message');
+		$status = $this -> get_post('status');
+
+		$data['from_user_id'] = $me_id;
+		$data['to_user_id'] = $to_message_id;
+		$data['content'] = $msg;
+		$data['status'] = $status;
+
+		if(!empty($me_id) && !empty($to_message_id) && !empty($msg)) {
+			// insert
+			$last_id = $this -> dao -> insert($data);
+		} 
+		$res['last_id'] = $last_id;
+		$res['success'] = TRUE;
+ 		$this -> to_json($res);
+	}
+
+	public function find_last() {
+		$res = array();
+		$data = array();
+		$me_id = $this -> get_post('me');
+		$to_message_id = $this -> get_post('f_chat_id');
+		$message = $this -> get_post('message');
+		$data['me_id'] = $me_id;
+		$data['to_message_id'] = $to_message_id;
+		$data['message'] = $message;
+		$last_id = $this -> dao -> find_last_message($data);
+		$res['last_id'] = $last_id;
+		$res['success'] = TRUE;
+ 		$this -> to_json($res);
+	}
+
+	public function reload_message_record() {
+		$res = array();
+		$me_id = $this -> get_post('me_id');
+		$to_message_id = $this -> get_post('to_message_id');
+		$type = $this -> get_post('type');
+		$f_chat_id = $this -> get_post('f_chat_id');
+
+		$data['user_id'] = $me_id;
+		$data['to_user_id'] = $to_message_id;
+		if(!empty($me_id) && !empty($to_message_id)) {
+			// insert
+			$msg_list = $this -> dao -> find_record($data);
+			foreach($msg_list as $each){
+				$user_name_find = $this -> users_dao -> find_by_id($each->from_user_id);
+				$to_user_name_find = $this -> users_dao -> find_by_id($each->to_user_id);
+				$each->user_name = $user_name_find->user_name;
+				$each->to_user_name = $to_user_name_find->user_name;
+				if($f_chat_id>0 ){
+					$find_me_no_read = $this -> dao -> find_by_me_no_read($me_id,$to_message_id);
+					foreach($find_me_no_read as $each_no_read_list){
+						$u_data['status'] = '1';
+						$this -> dao -> update($u_data, $each_no_read_list->id);
+					}
+				}
+			}
+			$to_user_name_list = $this -> users_dao -> find_by_id($to_message_id);
+			$res['msg_list'] = $msg_list;
+			$res['to_user_name_list'] = $to_user_name_list;
+		} 
+		$res['success'] = TRUE;
+ 		$this -> to_json($res);
+	}
+
+	public function find_all_user() {
+		$all_users = $this -> users_dao -> find_all_user_by_message();
+		$res['all_users'] = $all_users;
+		$res['success'] = TRUE;
+		$this -> to_json($res);
+	}
+
+	public function find_offline_users() {
+		$data = array();
+		$data = $this -> setup_user_data($data);
+		$offline_arr = $this -> get_post('id_array');
+		$online_id_array = $this -> get_post('online_id_array');
+		if(!empty($offline_arr)){
+			foreach($offline_arr[0] as $each_offline){
+				$all_users = $this -> users_dao -> find_all_offline_users($each_offline);
+				foreach($all_users as $each_offline_user){
+					$not_read = $this -> dao -> find_not_read($data['login_user_id'],$each_offline_user->id);
+					$each_offline_user->no_read =$not_read;
+				}
+				// $res['offline_users'][] = $all_users;
+				foreach($all_users as $each_offline_user){
+					if($each_offline_user->no_read>0){
+						$res['offline_users'][] = $each_offline_user;
+					} else{
+						$res['offline_users_no_noread'][] = $each_offline_user;
+					}
+				}
+			}
+		}
+			if(!empty($online_id_array)){
+				foreach($online_id_array as $each_online){
+					$online_users = $this -> users_dao -> find_all_offline_users($each_online);
+					foreach($online_users as $each_online_user){
+						$not_read = $this -> dao -> find_not_read($data['login_user_id'],$each_online_user->id);
+						$each_online_user->no_read =$not_read;
+					}
+					$res['online_users'][] = $online_users;
+				}
+			} else{
+				$res['online_users']=false;
+			}
+	
+		$res['success'] = TRUE;
+		$this -> to_json($res);
+	}
+
+	public function find_offline_user() {
+		$f_chat_id = $this -> get_post('f_chat_id');
+		// json_decode($offline_arr,true);
+		$user_list = $this -> users_dao -> find_by_id($f_chat_id);
+		$res['user_list'] = $user_list;
+		$res['success'] = TRUE;
+		$this -> to_json($res);
+	}
+
+	public function find_me_is_online() {
+		$data = array();
+		$data = $this -> setup_user_data($data);
+		$login_user = $this -> users_dao -> find_by_id($data['login_user_id']);
+		$is_online_list= $this -> user_online_dao -> find_me_is_online_or_not();
+		if(!empty($is_online_list->now_online)){
+			$is_online_array[] = $is_online_list[0]->now_online;
+			if(in_array($login_user ->id, $is_online_array)){
+				$res['is_online'] = 0;//在線上
+			} else{
+				$res['is_online'] = 2;//不在線上
+			}
+		} else{
+			$res['is_online'] = 2;//不在線上
+		}
+
+		
+		$res['success'] = TRUE;
+		$this -> to_json($res);
+	}
+}
---
+++ application/views/mgmt/Old_system_api/edit.php
@@ -0,0 +1,356 @@
+<style>
+
+.btn_1 {
+	background-color: #FFD22F !important;
+	color: #F57316 !important;
+}
+.is_ok {
+	background-color:lightgreen;
+}
+.not_ok {
+	background-color:red;
+}
+.fileinput-upload{
+	display:none !important;
+}
+.fileinput-remove{
+	display:none !important;
+}
+.fail_fieldset{
+	display:none !important;
+}
+.family_span:hover{
+    color:#FFD22F;
+}
+.remove_pa:hover{
+    color:#9AFF02;
+}
+
+</style>
+<!-- Widget ID (each widget will need unique ID)-->
+<div class="jarviswidget" id="wid-id-7" data-widget-colorbutton="false"	data-widget-editbutton="false" data-widget-deletebutton="false" data-widget-sortable="false">
+	<header>
+		<div class="widget-toolbar pull-left">
+			<a href="javascript:void(0);" id="back_parent" onclick="currentApp.backTo()" class="btn btn-default">
+				<i class="fa fa-arrow-circle-left"></i>返回
+			</a>
+		</div>
+		
+		<div class="widget-toolbar pull-left">
+			<a href="javascript:void(0);"  onclick="do_save();" class="btn btn-default btn-danger">
+				<i class="fa fa-save"></i>存檔
+			</a>
+		</div>
+		
+	</header>
+	<!-- widget div-->
+	<div>
+		<!-- widget edit box -->
+		<div class="jarviswidget-editbox">
+			<!-- This area used as dropdown edit box -->
+			<input class="form-control" type="text">
+		</div>
+		<!-- end widget edit box -->
+		<!-- widget content -->
+		<div class="widget-body">
+			<form id="app-edit-form" method="post" class="form-horizontal">
+				<input type="hidden" name="id" id="item_id" value="<?= isset($item) ? $item -> id : '' ?>" />
+			
+				<div class="form-group" style="padding:0px 26px">
+        <div class="clearfix"></div>
+    </div>
+    <hr/>
+		<div>
+			
+			<fieldset>
+				<div class="form-group">
+					<label class="col-md-3 control-label">店名</label>
+					<div class="col-md-6" id="patnet_status" >
+						<input type="text" required class="form-control" name="menu_name"  id="menu_name" value="<?= isset($item) ? $item -> menu_name : '' ?>"  />
+						<div style="color:#a90329;" id="same_name">
+						</div>
+					</div>
+				</div>
+			</fieldset>
+			<fieldset>
+				<div class="form-group">
+					<label class="col-md-3 control-label">分類</label>
+					<div class="col-md-6">
+						<input type="hidden" required class="form-control" name="s_menu_style_id"  id="s_menu_style_id" value="<?= isset($item) ? $item -> menu_style_id : 0 ?>"  />
+						<select id="menu_style_id" class="form-control">
+											<!-- option from javascript -->
+						</select>
+					</div>
+				</div>
+			</fieldset>
+
+			<fieldset>
+				<div class="form-group">
+					<label class="col-md-3 control-label">開放日期</label>
+					<div class="col-md-6">
+						<input class="form-control input-xs min100 dt_picker"  id="open_date" type="text"  value="<?= isset($item) ? $item -> open_date : 0 ?>" />
+					</div>
+				</div>
+			</fieldset>
+			<?php if($login_user->id=='205' ||$login_user->id=='114'||$login_user->id=='87'||$login_user->id=='108'||$login_user->id=='137'): ?>
+				<fieldset >
+					<div class="form-group" >
+						<label class="col-md-3 control-label">開放部門</label>
+						<input type="hidden" id="dep_array" value="<?= isset($item) ? $item->open_dep : '' ?>" />
+						<div class="col-md-6">
+
+							<select id="user_dep_array" name="user_dep[]" class="form-control" multiple>
+							
+							</select>
+							
+						</div>
+
+					</div>
+				</fieldset>
+			<?php endif?>
+			
+			<fieldset>
+				<div class="form-group">
+					<label class="col-md-3 control-label">備註</label>
+					<div class="col-md-6">
+						<input type="text" required class="form-control" name="note"  id="note" value="<?= isset($item) ? $item -> note : '' ?>"  />
+					</div>
+				</div>
+			</fieldset>
+			
+
+			<fieldset>
+				<div class="form-group">
+					<label class="col-md-3 control-label">圖片</label>
+					<div class="col-md-6">
+						<input id="img-input" name="file[]" type="file" accept="image/*" multiple class="file-loading form-control" >
+						<input id="img_id" type="hidden"  value="<?= isset($item) ? $item -> img_id : '' ?>" >
+					</div>
+				</div>
+			</fieldset>
+
+
+		
+		</div>
+		
+		</form>
+
+		</div>
+		<!-- end widget content -->
+
+	</div>
+	<!-- end widget div -->
+
+</div>
+<!-- end widget -->
+<!-- <script src="http://www.appelsiini.net/download/jquery.jeditable.mini.js"></script> -->
+<script>
+	var img=false
+	if($('#item_id').val()>0){
+		img=[];
+		if($('#img_id').val().length>0){
+			img.push($('#img_id').val());
+		}
+	} else{
+		img=[];
+	}
+
+	$(".dt_picker").datetimepicker({
+		format : 'YYYY.MM.DD'
+	}).on('dp.change',function(event){
+
+	});
+	var dep_array =[];
+
+	if(typeof $('#dep_array').val() !=='undefined'){
+		dep_array.push($('#dep_array').val());
+
+	}
+
+
+	function find_dep() {	
+		
+		$.ajax({
+			url: '<?= base_url() ?>' + 'mgmt/menu/find_dep',
+			type: 'POST',
+			data: {},
+			dataType: 'json',
+			success: function(d) {
+				if(d) {
+					// console.log(d.dep);
+					$user_dep_array = $('#user_dep_array').empty();
+					option ='';
+					if(dep_array.length>0){
+						$new_user_dep_array = dep_array[0].split(",");
+
+					} else{
+						$new_user_dep_array = [];
+
+					}
+
+					$.each(d.dep, function(){
+						if($new_user_dep_array.indexOf(this.id)>=0){
+							option +='<option value="'+this.id+'"  selected="selected">'+this.name+'</option>';
+						} else{
+							option +='<option value="'+this.id+'">'+this.name+'</option>';
+						}
+
+					});
+					$user_dep_array.append(option).select2();
+					// select2()
+				}	
+			},
+			failure:function(){
+				alert('faialure');
+			}
+		});
+
+	}
+	find_dep();
+	$('#user_dep_array').on('change', function(){
+		$('#dep_array').val('');
+		$('#dep_array').val($('#user_dep_array').val());
+	});
+
+
+$("#img-input").fileinput({
+					language: "zh-TW",
+			
+		<?php if(!empty($item -> image) && count($item -> image) > 0): ?>
+
+        	initialPreview: [
+        		<?php foreach($item -> image as $img): ?>
+        			'<?=  base_url('mgmt/images/get/' . $img->id) ?>',
+        		<?php endforeach ?>
+			],
+        	initialPreviewConfig: [
+			<?php foreach($item -> image as $img): ?>
+				{
+						'caption' : '<?= $img -> image_name ?>',
+						'size' : <?= $img -> image_size ?>,
+						'width' : '120px',
+						'url' : '<?= base_url('mgmt/images/delete/' . $img->id )  ?>',
+						'downloadUrl': '<?=base_url('mgmt/images/get/' . $img->id)?>',
+						'key' : <?= $img->id?>
+				},
+				
+    		<?php endforeach ?>
+        	],
+        <?php else: ?>
+        	initialPreview: [],
+        	initialPreviewConfig: [],
+        <?php endif ?>
+        initialPreviewAsData: true,
+		overwriteInitial: false,
+		purifyHtml:true ,
+		maxFileCount: 1,
+		
+        uploadUrl: 'mgmt/images/upload/img',
+        uploadExtraData: {
+        }
+    }).on('fileuploaded', function(event, data, previewId, index) {
+		// upload image
+		var id = data.response.id;
+		img.push(id);	
+		console.log(img);
+	}).on('fileselect', function(event, numFiles, label) {
+    	$("#img-input").fileinput('upload');
+	}).on('filedeleted', function(event,data,key) {
+		img.splice($.inArray(data,img),1);
+	}).on('fileuploaderror', function(event, data, previewId, index) {
+		alert('upload error');
+	}).on('filedeleted', function(event,data,key) {
+	});
+
+
+	$('#menu_name').on('keyup', function(){
+		var url = baseUrl + 'mgmt/menu/menu_name_check'; // the script where you handle the form input.
+		$.ajax({
+			type : "POST",
+			url : url,
+			data : {
+				menu_name: $('#menu_name').val(),
+			},
+			success : function(data) {
+				if(data.count_same_menu_name>0){
+					$('#same_name').text('已有相似店家： '+data.same_name_list);
+				} else{
+					$('#same_name').text('');
+				}
+				
+			}
+		});
+	});
+	function do_save() {
+	var url = baseUrl + 'mgmt/menu/insert'; // the script where you handle the form input.
+	$.ajax({
+		type : "POST",
+		url : url,
+		data : {
+			id: $('#item_id').val(),
+			menu_style_id:$('#menu_style_id').val(),
+			menu_name: $('#menu_name').val(),
+			img: img.join(","),
+			open_date: $('#open_date').val(),
+			note: $('#note').val(),
+			dep_array: $('#dep_array').val()
+		},
+		success : function(data) {
+			if(data.error_msg) {
+				layer.msg(data.error_msg);
+			} else {
+				currentApp.mDtTable.ajax.reload(null, false);
+				currentApp.backTo();
+			}
+		}
+	});
+};
+
+function load_style() {
+		$.ajax({
+			url: '<?= base_url() ?>' + 'mgmt/menu/find_menu_style',
+		
+			type: 'POST',
+			dataType: 'json',
+			success: function(d) {
+				if(d) {
+					var menu_style_id=$('#s_menu_style_id').val();
+					// console.log(d);
+					$img_style = $('#menu_style_id').empty();
+					var option = '<option value="0">無</option>';
+					$img_style.append(option);
+					if(menu_style_id>0 ){ 
+						$.each(d.menu_style, function(){
+							if(this.id==menu_style_id){
+								$('<option/>', {
+									'value': this.id,
+									'text': this.menu_style
+								}).attr("selected", true).appendTo($img_style);	
+							} else{
+								$('<option/>', {
+									'value': this.id,
+									'text': this.menu_style
+								}).appendTo($img_style);
+							}
+						});
+					} else{
+						$.each(d.menu_style, function(){
+							$('<option/>', {
+								'value': this.id,
+								'text': this.menu_style
+							}).appendTo($img_style);
+						});
+					}
+					
+				}
+			},
+			failure:function(){
+				alert('faialure');
+			}
+		});
+	}
+	load_style();
+	$('#menu_style_id').on('change', function(){
+		$('#s_menu_style_id').val($('#menu_style_id').val()) ;
+	});
+</script>
