8c5f09428917c12fcbd9195eef8e48535aa041e5
pony <as3305191@gmail.com>
2024-10-23 13:47:55 +0800

    up

--- application/controllers/old_system_view/old_system_view_home.php
+++ application/controllers/old_system_view/old_system_view_home.php
@@ -82,7 +82,7 @@ class Old_system_view_home extends MY_Base_Controller {
 		$connectionInfo=array("Database"=>"informationexc","TrustServerCertificate"=>"yes","UID"=>"exchange","PWD"=>"97238228","CharacterSet" => "UTF-8");
 		$conn=sqlsrv_connect($serverName,$connectionInfo);
 		
-		$sql = "SELECT id FROM account where account=$account and deldate=NULL";    
+		$sql = "SELECT id FROM account where account=$account";    
 		
 		/* Execute the query. */    
 		
