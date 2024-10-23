8e1c2da0d1de87ac8881aad35c9671901c2070c7
pony <as3305191@gmail.com>
2024-10-23 16:35:27 +0800

    up

--- application/views/old_system_view/old_system_view_home.php
+++ application/views/old_system_view/old_system_view_home.php
@@ -43,12 +43,7 @@
 
               <!-- Product Table Panel -->
               <div class="card border-0">
-                <div class="card-header d-flex align-items-center justify-content-between g-bg-gray-light-v5 border-0 g-mb-15">
-                  <h3 class="h6 mb-0">
-                      <i class="icon-directions g-pos-rel g-top-1 g-mr-5"></i> 所有學員
-                    </h3>
-
-                </div>
+               
 
                
                
@@ -56,21 +51,19 @@
                   <h3 class="h6 mb-0">
                       <i class="icon-directions g-pos-rel g-top-1 g-mr-5"></i> 餐廳
                   </h3>
+                  <h3 class="h6 mb-0">
+                      <i class="icon-directions g-pos-rel g-top-1 g-mr-5"></i> 剩餘金額
+                  </h3>
                 </div>
 
                 <div class="card-block g-pa-0" >
                   <table id="dt_list" class="table table-bordered u-table--v2">
                     <thead class="text-uppercase g-letter-spacing-1">
                       <tr>
-                        <th class="g-font-weight-300 g-color-black dt_list_th" style="min-width: 100px">照片</th>
-                        <th class="g-font-weight-300 g-color-black dt_list_th" style="min-width: 100px">學員</th>
-                        <th class="g-font-weight-300 g-color-black dt_list_th" style="min-width: 100px">年齡</th>
-                        <th class="g-font-weight-300 g-color-black dt_list_th" style="min-width: 100px">身高</th>
-                        <th class="g-font-weight-300 g-color-black dt_list_th" style="min-width: 100px">性別</th>
-                        <th class="g-font-weight-300 g-color-black dt_list_th" style="min-width: 100px">體重</th>
-                        <th class="g-font-weight-300 g-color-black dt_list_th_big" style="min-width: 100px">體重變化</th>
-                        <th class="g-font-weight-300 g-color-black dt_list_th_big" style="min-width: 100px">脂肪變化</th>
-                        <th class="g-font-weight-300 g-color-black dt_list_th_big" style="min-width: 100px">肥胖程度</th>
+                        <th class="g-font-weight-300 g-color-black dt_list_th" style="min-width: 100px">餐點名稱</th>
+                        <th class="g-font-weight-300 g-color-black dt_list_th" style="min-width: 100px">備註</th>
+                        <th class="g-font-weight-300 g-color-black dt_list_th" style="min-width: 100px">幫人點餐</th>
+                        <th class="g-font-weight-300 g-color-black dt_list_th_big" style="min-width: 100px">價錢</th>
                       </tr>
                     </thead>
                     <tbody id="dt_list_body">
