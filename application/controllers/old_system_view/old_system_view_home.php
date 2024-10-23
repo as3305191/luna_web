239f97177ef747e3fd426a7fcdd42ba306733b6e
pony <as3305191@gmail.com>
2024-10-23 14:55:05 +0800

    up

--- application/controllers/old_system_view/old_system_view_home.php
+++ application/controllers/old_system_view/old_system_view_home.php
@@ -25,16 +25,16 @@ class Old_system_view_home extends MY_Base_Controller {
 
 		$today = date("Y-m-d");
 		$data['now'] = 'old_system_view_home';//現在哪頁
-		$old_user_id = $this -> get_old_user_id($login_user->account),true;
-		$menu_open_list = $this -> get_data_menu($today),true;
+		$old_user_id = $this -> get_old_user_id($login_user->account);
+		$menu_open_list = $this -> get_data_menu($today);
 		$old_user_ewallet = $this -> get_user_ewallet($old_user_id);
-		// $income = 0;
-		// $outcome = 0;
-		// foreach($old_user_ewallet as $each){
-		// 	$income.=$each->$income;
-		// 	$outcome.=$each->$outcome;
-		// }
-		// $total_old_user_ewallet = $income - $outcome;
+		$income = 0;
+		$outcome = 0;
+		foreach($old_user_ewallet as $each){
+			$income.=$each->$income;
+			$outcome.=$each->$outcome;
+		}
+		$total_old_user_ewallet = $income - $outcome;
 		$data['old_user_id'] = $old_user_id;
 		$data['total_old_user_ewalle'] = $old_user_ewallet;
 		$data['menu_open_list'] = $menu_open_list;
