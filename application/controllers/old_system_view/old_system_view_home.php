6185968e59842d1ccb8bf6060e5e88d3c10b50b4
pony <as3305191@gmail.com>
2024-10-23 15:04:57 +0800

    up

--- application/controllers/old_system_view/old_system_view_home.php
+++ application/controllers/old_system_view/old_system_view_home.php
@@ -31,10 +31,10 @@ class Old_system_view_home extends MY_Base_Controller {
 		$income = 0;
 		$outcome = 0;
 		foreach($old_user_ewallet as $each){
-			if($each->$income >=0){
+			if($each->$income !==null){
 				$income.=$each->$income;
 			}
-			if($each->$outcome >=0){
+			if($each->$outcome !==null){
 				$outcome.=$each->$outcome;
 			}
 			
