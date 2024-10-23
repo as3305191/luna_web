ddb0c1e7b383ee2e14c546825d9eaf7f4330b616
pony <as3305191@gmail.com>
2024-10-23 14:26:55 +0800

    up

--- application/controllers/old_system_view/old_system_view_home.php
+++ application/controllers/old_system_view/old_system_view_home.php
@@ -40,7 +40,7 @@ class Old_system_view_home extends MY_Base_Controller {
 		$conn=sqlsrv_connect($serverName,$connectionInfo);
 		// $sql = "SELECT * FROM order_menu";    
 
-		$sql = "SELECT * FROM order_menu where odate='$today'";    
+		$sql = "SELECT * FROM order_menu where odate='$today' and ISNULL(enddate,'')";    
 		
 		/* Execute the query. */    
 		
@@ -48,7 +48,7 @@ class Old_system_view_home extends MY_Base_Controller {
 		
 		$result_array = array();
 		while($row=sqlsrv_fetch_array($stmt)){
-            $result_array[] = array_shift($row);
+            $result_array[] = $row;
         }
 	    $this -> to_json($result_array);
 		// $this->load->view('mgmt/old_system_api/list', $result_array);
