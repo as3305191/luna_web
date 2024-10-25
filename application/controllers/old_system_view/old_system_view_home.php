642e3ec267bd4cb607c08da416b41644ee41664c
pony <as3305191@gmail.com>
2024-10-25 17:08:23 +0800

    up

--- application/controllers/old_system_view/old_system_view_home.php
+++ application/controllers/old_system_view/old_system_view_home.php
@@ -21,7 +21,7 @@ class Old_system_view_home extends MY_Base_Controller {
 		$count_today_= array();
 
 		$s_data = $this -> setup_user_data(array());
-		$login_user = $this -> users_dao -> find_by_id($s_data['login_user_id']);
+		$login_user = $this -> users_dao -> find_by_id($s_data['old_system_view_user_id']);
 
 		$today = date("Y-m-d");
 		$data['now'] = 'old_system_view_home';//現在哪頁
