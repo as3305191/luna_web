68622e069bd46058e1bbcd5cb3197edb69d1b93a
pony <as3305191@gmail.com>
2024-10-25 16:29:58 +0800

    up

--- application/views/old_system_view/old_system_view_home.php
+++ application/views/old_system_view/old_system_view_home.php
@@ -147,6 +147,12 @@
 <?php $this -> load -> view("old_system_view/old_system_view_script")  ?>
 <script src="<?= base_url() ?>assets/vendor/fancybox1/jquery.fancybox.min.js"></script>
 <script>
+  $(document).ready(function() {
+    // if(<?php count($store_list) >0 ?>){
+    //   	img_album(<?= $store_list[$i]->new_img_address ?>);
+    // }
+  })
+
   var baseUrl = '<?=base_url('')?>';
 	function menu_click(id,new_img_address) {
 			//   document.getElementById(id).show();
