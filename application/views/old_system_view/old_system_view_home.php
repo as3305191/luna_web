184b20e2998b5394c220dae3016b446cd27973f2
pony <as3305191@gmail.com>
2024-10-29 15:39:35 +0800

    up

--- application/views/old_system_view/old_system_view_home.php
+++ application/views/old_system_view/old_system_view_home.php
@@ -11,7 +11,7 @@
   .element {
     /* border: 5px solid; */
     position: absolute;
-    /* top: 50%; */
+    top: 65%;
     left: 50%;
     transform: translate(-50%, -50%);
     padding: 10px;
@@ -19,7 +19,7 @@
   .element_table {
     /* border: 5px solid; */
     position: absolute;
-    top: 50%;
+    top: 105%;
     /* left: 50%;
     transform: translate(-50%, -50%);
     padding: 10px; */
