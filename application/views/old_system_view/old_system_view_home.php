868d0ba8edbb1d51513eaf725a78769ba7797acc
pony <as3305191@gmail.com>
2024-10-29 15:31:32 +0800

    up

--- application/views/old_system_view/old_system_view_home.php
+++ application/views/old_system_view/old_system_view_home.php
@@ -113,7 +113,7 @@
                     </div>
                   </div>
 
-                  <table id="dt_list" class="table table-bordered u-table--v2" style="padding:0px 0px 0px 0px;">
+                  <table id="dt_list" class="table table-bordered u-table--v2" style="top: 20%;">
                     <thead class="text-uppercase g-letter-spacing-1">
                       <tr>
                         <th class="g-font-weight-300 g-color-black dt_list_th" style="min-width: 100px"></th>
