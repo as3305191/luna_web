e6c9cdc8666ddc93d3d295b55eedc886473637e4
pony <as3305191@gmail.com>
2024-10-29 15:42:17 +0800

    up

--- application/views/old_system_view/old_system_view_home.php
+++ application/views/old_system_view/old_system_view_home.php
@@ -19,7 +19,7 @@
   .element_table {
     /* border: 5px solid; */
     position: absolute;
-    top: 115%;
+    top: 110%;
     /* left: 50%;
     transform: translate(-50%, -50%);
     padding: 10px; */
