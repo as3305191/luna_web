7fc2fe536a37a4fe6350b4ea37a393fb33c9ec03
pony <as3305191@gmail.com>
2024-10-29 13:22:11 +0800

    up

--- application/views/old_system_view/old_system_view_home.php
+++ application/views/old_system_view/old_system_view_home.php
@@ -77,7 +77,7 @@
                       <button class="btn_active btn-success text-light menu_btn menu_<?= $store_list[0]->id ?>" style="border-radius: 5px; padding: 10px; width: 220px; height: 48px;" onclick="menu_click('<?= $store_list[0]->id ?>','<?= $store_list[0]->new_img_address ?>')">&nbsp;<?= $store_list[0]->store ?></button>
                       <input type="hidden" class="form-control" id="store_id" value="<?= $store_list[0]->id ?>">
                       <?php for ($i=1;$i<count($store_list);$i++) : ?>                      
-                        <button class="btn_unsuccess menu_btn menu_<?= $store_list[$i]->id ?>" style="border-radius: 5px; padding: 10px; width: 220px; height: 48px;" onclick="menu_click('<?= $store_list[$i]->id ?>','<?= $store_list[$i]->new_img_address ?>')">&nbsp;<?= $store_list[$i]->store ?></button>
+                        <button class="text-light btn_unsuccess menu_btn menu_<?= $store_list[$i]->id ?>" style="border-radius: 5px; padding: 10px; width: 220px; height: 48px;" onclick="menu_click('<?= $store_list[$i]->id ?>','<?= $store_list[$i]->new_img_address ?>')">&nbsp;<?= $store_list[$i]->store ?></button>
                         <?php endfor ?>
                     <?php endif?>
                     <div id="img_album" class="g-pos-rel" style="padding:10px 0px 6px 12px;">
@@ -164,10 +164,10 @@
         $('.menu_img').addClass('menu_img_unsuccess');
         $('.menu_img_'+id).removeClass('menu_img_unsuccess');
         // $('.menu_btn').removeClass('btn_active');
-        $('.menu_btn').removeClass('btn_active btn-success');
-        $('.menu_btn').addClass('btn_unsuccess text-light');
-        $('.menu_'+id).removeClass('btn_unsuccess text-light');
-        $('.menu_'+id).addClass('btn_active btn-success');
+        $('.menu_btn').removeClass('btn_active btn-success ');
+        $('.menu_btn').addClass('btn_unsuccess');
+        $('.menu_'+id).removeClass('btn_unsuccess');
+        $('.menu_'+id).addClass('btn_active btn-success ');
         $('#store_id').val(id);
         img_album(new_img_address);
     }
