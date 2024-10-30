0c8d2821c65bedb4010b79d1150d3cfb4ef1f830
pony <as3305191@gmail.com>
2024-10-30 11:46:00 +0800

    up

--- application/views/old_system_view/old_system_view_home.php
+++ application/views/old_system_view/old_system_view_home.php
@@ -73,7 +73,7 @@
               <div class="card border-0">
                 <div class="card-header d-flex align-items-center justify-content-between g-bg-gray-light-v5 border-0 g-mb-15">
                   <h3 class="h6 mb-0">
-                    <p id="user_money" style="font-size:15px;"><i class="icon-directions g-pos-rel g-top-1 g-mr-5"></i>剩餘金額 :<?=$total_old_user_ewallet ?> (餘額不足請找企劃課儲值，最低儲值200元)</p>
+                    <p id="user_money" style="font-size:25px;"><i class="icon-directions g-pos-rel g-top-1 g-mr-5"></i>剩餘金額 :<?=$total_old_user_ewallet ?> (餘額不足請找企劃課儲值，最低儲值200元)</p>
                   </h3>
                 </div>
                 <div class="card-block g-pa-0" >
