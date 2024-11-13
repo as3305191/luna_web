8b8267d94c88e1eb8ec8f5640c8eaca9800340a1
pony <as3305191@gmail.com>
2024-11-13 14:29:22 +0800

    up

--- application/views/sing/sing_index.php
+++ application/views/sing/sing_index.php
@@ -35,6 +35,6 @@ function getDeviceUUID() {
 }
 const deviceUUID = getDeviceUUID();
 console.log("设备唯⼀标识为: ", deviceUUID);
-document.getElementById('deviceUUID').val(deviceUUID);
+document.getElementById('deviceUUID').textContent(deviceUUID);
 </script>
 
