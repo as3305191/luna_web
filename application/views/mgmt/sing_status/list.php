bd9de3a3876942b1462f390ae894c92c7100def0
pony <as3305191@gmail.com>
2024-11-14 13:09:16 +0800

    up

--- application/views/mgmt/sing_status/list.php
+++ application/views/mgmt/sing_status/list.php
@@ -96,7 +96,11 @@ thead tr th {
 <?php $this -> load -> view('general/delete_modal'); ?>
 <script type="text/javascript">
 	var baseUrl = '<?=base_url('')?>';
+	$(".dt_picker").datetimepicker({
+		format : 'YYYY.MM.DD'
+	}).on('dp.change',function(event){
 
+	});
 	var mCols = [{
 				targets : 0,
 				data : 'status',
