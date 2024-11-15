03d94dca5632ae1c7d95e70bcaf92b1b16720733
pony <as3305191@gmail.com>
2024-11-15 17:28:35 +0800

    up

--- application/views/sing/sing_ranking.php
+++ application/views/sing/sing_ranking.php
@@ -64,35 +64,38 @@
                 </span>
               <h2 class="h4 g-color-black">排行榜</h2>
             </div>
-          
-            
-            <?php $i=1; ?>
-            <?php foreach ($ticket_array as $each_key => $each_val): ?>
-              <?php if ($each_key==$winner): ?>
-                <div class="mb-4">
-                  <h4 class="h6">第1名<span class="float-right g-ml-10"><?=$each_val?></span></h4>
-                  <div class="js-hr-progress-bar progress g-height-4 rounded-0">
-                    <div class="js-hr-progress-bar-indicator progress-bar" role="progressbar" style="width: <?= $each_val!==0 ? floor($each_val/$allticket)*100 : 0 ?>%;" aria-valuenow="<?= $each_val!==0 ? floor($each_val/$allticket)*100 : 0 ?>" aria-valuemin="0" aria-valuemax="100"></div>
-                  </div>
-                </div>
-                <?php $i++; ?>
-              <?php else : ?>
-                <div class="mb-4">
-                  <h4 class="h6">第<?= $i; ?>名<span class="float-right g-ml-10"><?=$each_val?></span></h4>
-                  <div class="js-hr-progress-bar progress g-height-4 rounded-0">
-                    <div class="js-hr-progress-bar-indicator progress-bar" role="progressbar" style="width: <?= $each_val!==0 ? floor($each_val/$allticket)*100 : 0 ?>%;" aria-valuenow="<?= $each_val!==0 ? floor($each_val/$allticket)*100 : 0 ?>" aria-valuemin="0" aria-valuemax="100"></div>
-                  </div>
-                </div>
-                <?php $i++; ?>
-              <?php endif; ?>
-            <?php endforeach; ?>
 
 
-
-            <hr class="g-brd-gray-light-v4">
+    
+            <!-- Progress Bars -->
+            <div class="mb-4">
+              <h4 class="h6">Web Design <span class="float-right g-ml-10">84%</span></h4>
+              <div class="js-hr-progress-bar progress g-height-4 rounded-0">
+                <div class="js-hr-progress-bar-indicator progress-bar" role="progressbar" style="width: 84%;" aria-valuenow="84" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
             </div>
 
-          
+            <div class="mb-4">
+              <h4 class="h6">PHP/WordPress <span class="float-right g-ml-10">57%</span></h4>
+              <div class="js-hr-progress-bar progress g-height-4 rounded-0">
+                <div class="js-hr-progress-bar-indicator progress-bar" role="progressbar" style="width: 57%;" aria-valuenow="57" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <hr class="g-brd-gray-light-v4">
+
+            <!-- Tags -->
+            <ul class="u-list-inline text-center mb-0">
+              <li class="list-inline-item">
+                <a class="u-tags-v1 g-brd-around g-brd-gray-light-v3 g-brd-primary--hover g-color-gray-dark-v4 g-color-primary--hover g-font-size-12 g-rounded-50 g-py-4 g-px-15" href="#">Design</a>
+              </li>
+              <li class="list-inline-item">
+                <a class="u-tags-v1 g-brd-around g-brd-gray-light-v3 g-brd-primary--hover g-color-gray-dark-v4 g-color-primary--hover g-font-size-12 g-rounded-50 g-py-4 g-px-15" href="#">Art</a>
+              </li>
+              <li class="list-inline-item">
+                <a class="u-tags-v1 g-brd-around g-brd-gray-light-v3 g-brd-primary--hover g-color-gray-dark-v4 g-color-primary--hover g-font-size-12 g-rounded-50 g-py-4 g-px-15" href="#">Graphic</a>
+              </li>
+            </ul>
             <!-- End Tags -->
           </div>
         </div>
