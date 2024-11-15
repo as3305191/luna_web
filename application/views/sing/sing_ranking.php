7cfec781d463fd2bf8e0c1335494099d036405f6
pony <as3305191@gmail.com>
2024-11-15 17:36:12 +0800

    up

--- application/views/sing/sing_ranking.php
+++ application/views/sing/sing_ranking.php
@@ -63,6 +63,8 @@
                   <i class="icon-communication-116 u-line-icon-pro"></i>
                 </span>
               <h2 class="h4 g-color-black">排行榜</h2>
+              <hr class="g-brd-gray-light-v4">
+
               <?php $i=1; ?>
               <?php foreach ($ticket_array as $each_key => $each_val): ?>
                 <?php if ($each_key==$winner): ?>
@@ -87,21 +89,7 @@
 
 
            
-            <!-- Progress Bars -->
-            <div class="mb-4">
-              <h4 class="h6">Web Design <span class="float-right g-ml-10">84%</span></h4>
-              <div class="js-hr-progress-bar progress g-height-4 rounded-0">
-                <div class="js-hr-progress-bar-indicator progress-bar" role="progressbar" style="width: 84%;" aria-valuenow="84" aria-valuemin="0" aria-valuemax="100"></div>
-              </div>
-            </div>
-
-            <div class="mb-4">
-              <h4 class="h6">PHP/WordPress <span class="float-right g-ml-10">57%</span></h4>
-              <div class="js-hr-progress-bar progress g-height-4 rounded-0">
-                <div class="js-hr-progress-bar-indicator progress-bar" role="progressbar" style="width: 57%;" aria-valuenow="57" aria-valuemin="0" aria-valuemax="100"></div>
-              </div>
-            </div>
-
+            
           
           </div>
         </div>
