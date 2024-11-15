b2ab5ca14458c6dc75fb39d0578a4fef03f3484b
pony <as3305191@gmail.com>
2024-11-15 09:58:40 +0800

    up

--- application/views/sing/sing_index.php
+++ application/views/sing/sing_index.php
@@ -78,7 +78,7 @@
                     <li class="g-brd-bottom g-brd-gray-light-v3 g-py-10"> <input type="radio" name="m" value="m_1" id="m_1"><label for="m_1">1. <?= isset($item) ? $item -> m_1 : '' ?></label></li>
                     <li class="g-brd-bottom g-brd-gray-light-v3 g-py-10"> <input type="radio" name="m" value="m_2" id="m_2"><label for="m_2">2. <?= isset($item) ? $item -> m_2 : '' ?></label></li>
                     <li class="g-brd-bottom g-brd-gray-light-v3 g-py-10"> <input type="radio" name="m" value="m_3" id="m_3"><label for="m_3">3. <?= isset($item) ? $item -> m_3 : '' ?></label></li>
-                    <li class="g-brd-bottom g-brd-gray-light-v3 g-py-10">   <input type="radio" name="m" value="m_4" id="m_4"><label for="m_4">4. <?= isset($item) ? $item -> m_4 : '' ?></label></li>
+                    <li class="g-brd-bottom g-brd-gray-light-v3 g-py-10"> <input type="radio" name="m" value="m_4" id="m_4"><label for="m_4">4. <?= isset($item) ? $item -> m_4 : '' ?></label></li>
                     <li class="g-brd-bottom g-brd-gray-light-v3 g-py-10"> <input type="radio" name="m" value="m_5" id="m_5"><label for="m_5">5. <?= isset($item) ? $item -> m_5 : '' ?></label></li>
                     <li class="g-py-8"> <button type="button" class="btn btn-sm btn-primary" onclick="do_save()">投票</button></li>
                   </ul>
