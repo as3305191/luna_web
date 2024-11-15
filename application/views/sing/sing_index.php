56a5ab32da94f3004ccadd245e9c867bf35a9d5f
pony <as3305191@gmail.com>
2024-11-15 10:40:29 +0800

    up

--- application/views/sing/sing_index.php
+++ application/views/sing/sing_index.php
@@ -84,11 +84,11 @@
                   <h3 class="h5 g-color-black g-mb-10">Web Design &amp; Development</h3>
                   <p class="g-color-gray-dark-v4">We strive to embrace and drive change in our industry which allows us to keep our clients relevant and ready to adapt.</p>
                     <ul class="list-unstyled g-px-30 g-mb-0 ">
-                      <li class="g-brd-bottom g-brd-gray-light-v3 g-py-10 div_center"><div class="col-md-4 g-mb-30 t_center"><input type="radio" name="m" value="m_1" id="m_1"><label for="m_1">1. <?= isset($item) ? $item -> m_1 : '' ?></label></div></li>
-                      <li class="g-brd-bottom g-brd-gray-light-v3 g-py-10 div_center"><div class="col-md-4 g-mb-30 t_center"><input type="radio" name="m" value="m_2" id="m_2"><label for="m_2">2. <?= isset($item) ? $item -> m_2 : '' ?></label></div></li>
-                      <li class="g-brd-bottom g-brd-gray-light-v3 g-py-10 div_center"><div class="col-md-4 g-mb-30 t_center"><input type="radio" name="m" value="m_3" id="m_3"><label for="m_3">3. <?= isset($item) ? $item -> m_3 : '' ?></label></div></li>
-                      <li class="g-brd-bottom g-brd-gray-light-v3 g-py-10 div_center"><div class="col-md-4 g-mb-30 t_center"><input type="radio" name="m" value="m_4" id="m_4"><label for="m_4">4. <?= isset($item) ? $item -> m_4 : '' ?></label></div></li>
-                      <li class="g-brd-bottom g-brd-gray-light-v3 g-py-10 div_center"><div class="col-md-4 g-mb-30 t_center"><input type="radio" name="m" value="m_5" id="m_5"><label for="m_5">5. <?= isset($item) ? $item -> m_5 : '' ?></label></div></li>
+                      <li class="g-brd-bottom g-brd-gray-light-v3 g-py-10 div_center"><div class="col-md-2 g-mb-30 t_center"><input type="radio" name="m" value="m_1" id="m_1"><label for="m_1">1. <?= isset($item) ? $item -> m_1 : '' ?></label></div></li>
+                      <li class="g-brd-bottom g-brd-gray-light-v3 g-py-10 div_center"><div class="col-md-2 g-mb-30 t_center"><input type="radio" name="m" value="m_2" id="m_2"><label for="m_2">2. <?= isset($item) ? $item -> m_2 : '' ?></label></div></li>
+                      <li class="g-brd-bottom g-brd-gray-light-v3 g-py-10 div_center"><div class="col-md-2 g-mb-30 t_center"><input type="radio" name="m" value="m_3" id="m_3"><label for="m_3">3. <?= isset($item) ? $item -> m_3 : '' ?></label></div></li>
+                      <li class="g-brd-bottom g-brd-gray-light-v3 g-py-10 div_center"><div class="col-md-2 g-mb-30 t_center"><input type="radio" name="m" value="m_4" id="m_4"><label for="m_4">4. <?= isset($item) ? $item -> m_4 : '' ?></label></div></li>
+                      <li class="g-brd-bottom g-brd-gray-light-v3 g-py-10 div_center"><div class="col-md-2 g-mb-30 t_center"><input type="radio" name="m" value="m_5" id="m_5"><label for="m_5">5. <?= isset($item) ? $item -> m_5 : '' ?></label></div></li>
                       <li class="g-py-8"> <button type="button" class="btn btn-sm btn-primary" onclick="do_save()">投票</button></li>
                     </ul>
                   </div>
