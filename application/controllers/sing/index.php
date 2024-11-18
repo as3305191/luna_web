5cb440c3b2144e350644b25e1a34c776e4e2e483
pony <as3305191@gmail.com>
2024-11-18 08:25:17 +0800

    up

--- application/controllers/sing/index.php
+++ application/controllers/sing/index.php
@@ -29,12 +29,12 @@ class Index extends MY_Base_Controller {
 		$winner_array = array();
 		$sing_status_id= $this -> sing_status_dao -> find_can_ranking();
 
-		// $all_ticket_item_m1 = $this -> sing_dao -> find_all_ticket_by_num('m_1',$sing_status_id);
-		// $all_ticket_item_m2 = $this -> sing_dao -> find_all_ticket_by_num('m_2',$sing_status_id);
-		// $all_ticket_item_m3 = $this -> sing_dao -> find_all_ticket_by_num('m_3',$sing_status_id);
-		// $all_ticket_item_m4 = $this -> sing_dao -> find_all_ticket_by_num('m_4',$sing_status_id);
-		// $all_ticket_item_m5 = $this -> sing_dao -> find_all_ticket_by_num('m_5',$sing_status_id);
-		// $all_ticket = $all_ticket_item_m1+$all_ticket_item_m2+$all_ticket_item_m3+$all_ticket_item_m4+$all_ticket_item_m5;
+		$all_ticket_item_m1 = $this -> sing_dao -> find_all_ticket_by_num('m_1',$sing_status_id);
+		$all_ticket_item_m2 = $this -> sing_dao -> find_all_ticket_by_num('m_2',$sing_status_id);
+		$all_ticket_item_m3 = $this -> sing_dao -> find_all_ticket_by_num('m_3',$sing_status_id);
+		$all_ticket_item_m4 = $this -> sing_dao -> find_all_ticket_by_num('m_4',$sing_status_id);
+		$all_ticket_item_m5 = $this -> sing_dao -> find_all_ticket_by_num('m_5',$sing_status_id);
+		$all_ticket = $all_ticket_item_m1+$all_ticket_item_m2+$all_ticket_item_m3+$all_ticket_item_m4+$all_ticket_item_m5;
 		// $ticket_array =  array('m1' => $all_ticket_item_m1,
 		// 					'm2' => $all_ticket_item_m2,
 		// 					'm3' => $all_ticket_item_m3,
@@ -53,7 +53,7 @@ class Index extends MY_Base_Controller {
 		$data['sing_status_id'] = $sing_status_id;
 
 		// $data['winner_name'] = $winner_array;
-		// $data['all_ticket'] = $all_ticket;
+		$data['all_ticket'] = $all_ticket;
 		// $data['ticket_array'] = $ticket_array;
 		// $data['winner']  = $winner;
 		// $data['$ticket_array[$winner]']  = $ticket_array[$winner];
