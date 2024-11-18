7a58da0b2d9750b48d6ff2d80dec44615b8127ca
pony <as3305191@gmail.com>
2024-11-18 08:18:14 +0800

    up

--- application/controllers/sing/index.php
+++ application/controllers/sing/index.php
@@ -44,17 +44,19 @@ class Index extends MY_Base_Controller {
 
 		arsort($ticket_array);
 		$winner =array_key_first($ticket_array) ;
-		foreach($ticket_array as $key=>$value){
-			if($value ==$ticket_array[$winner] ){
-				$user_name = $this -> sing_status_dao -> find_can_ranking_winner($value,$sing_status_id);
-				$winner_array[]=$user_name;
-			}
-		}
+		// foreach($ticket_array as $key=>$value){
+		// 	if($value ==$ticket_array[$winner] ){
+		// 		$user_name = $this -> sing_status_dao -> find_can_ranking_winner($value,$sing_status_id);
+		// 		$winner_array[]=$user_name;
+		// 	}
+		// }
 
 		$data['winner_name'] = $winner_array;
 		$data['all_ticket'] = $all_ticket;
 		$data['ticket_array'] = $ticket_array;
 		$data['winner']  = $winner;
+		$data['$ticket_array[$winner]']  = $ticket_array[$winner];
+
 		$this -> to_json($data);
 
 		$this -> load -> view('sing/sing_ranking', $data);
