a919b53ebbecf504e2550f73a38c6a3b4fa5246c
pony <as3305191@gmail.com>
2024-11-20 08:41:11 +0800

    up

--- application/views/sing/sing_index.php
+++ application/views/sing/sing_index.php
@@ -84,7 +84,8 @@
               <div class="col-md-12 g-mb-30">
                 <!-- Icon Blocks -->
 
-               
+                <div id="test"></div>
+
                   <div class="g-brd-around--md g-brd-gray-light-v4 text-center g-pa-10 g-px-30--lg g-py-40--lg">
                   <span class="d-block g-color-gray-dark-v4 g-font-size-40 g-mb-15">
                     <i class="icon-education-087 u-line-icon-pro"></i>
@@ -206,8 +207,7 @@
   }
 
   const deviceUUID = getDeviceUUID();
-
-
+  $('#test').append(deviceUUID);
   function do_save() {
 		var url = baseUrl + 'sing/index/give_ticket';
 
