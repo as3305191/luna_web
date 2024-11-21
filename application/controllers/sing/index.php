36187d9efab81a4032627784ae2d983e800fc85d
pony <as3305191@gmail.com>
2024-11-21 15:20:48 +0800

    up

--- application/controllers/sing/index.php
+++ application/controllers/sing/index.php
@@ -78,18 +78,18 @@ class Index extends MY_Base_Controller {
 					$data['uuid'] = $deviceUUID;
 					$last_id = $this -> sing_dao -> insert($data);
 					$res['last_id'] = $last_id;
-					$res['succ_gave'] = '投票完成。 \u000a Pemungutan suara selesai \u000a Đã hoàn thành việc bỏ phiếu';
+					$res['succ_gave'] = '投票完成。\\n Pemungutan suara selesai\\n Đã hoàn thành việc bỏ phiếu';
 
 				} else{
 					$this -> sing_dao -> update($data, $find_gave[0]->id);
-					$res['re_msg'] = '已更換投票人員。 \u000a Personil pemungutan suara telah diubah \u000a Nhân sự bỏ phiếu đã được thay đổi';
+					$res['re_msg'] = '已更換投票人員。\\n Personil pemungutan suara telah diubah\\n Nhân sự bỏ phiếu đã được thay đổi';
 				}
 			} else{
-				$res['msg'] = '投票已截止，請觀看排行榜。 \u000a Voting telah ditutup, harap perhatikan peringkatnya \u000a Bình chọn đã kết thúc, mời các bạn theo dõi bảng xếp hạng';
+				$res['msg'] = '投票已截止，請觀看排行榜。\\n Voting telah ditutup, harap perhatikan peringkatnya\\n Bình chọn đã kết thúc, mời các bạn theo dõi bảng xếp hạng';
 			}
 			
 		} else{
-			$res['msg'] = '活動已關閉無法投票。 \u000a Acara ditutup dan Anda tidak dapat memilih \u000a Sự kiện đã kết thúc và bạn không thể bỏ phiếu';
+			$res['msg'] = '活動已關閉無法投票。\\n Acara ditutup dan Anda tidak dapat memilih\\n Sự kiện đã kết thúc và bạn không thể bỏ phiếu';
 		}
 		
 
@@ -104,7 +104,7 @@ class Index extends MY_Base_Controller {
 		if(!empty($sing_status_id)){
 			$res['success'] = TRUE;
 		} else{
-			$res['msg'] = '比賽目前進行中，請稍候。 \u000a Kompetisi sedang berlangsung, harap tunggu. \u000a Cuộc thi hiện đang diễn ra, vui lòng đợi.';
+			$res['msg'] = '比賽目前進行中，請稍候。\\n Kompetisi sedang berlangsung, harap tunggu.\\n Cuộc thi hiện đang diễn ra, vui lòng đợi.';
 		}
 		$this -> to_json($res);
 		
