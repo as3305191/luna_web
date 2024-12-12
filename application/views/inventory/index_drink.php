297f0edee6f8c4986a7d4b043fa93650ef43da20
pony <as3305191@gmail.com>
2024-12-12 16:06:07 +0800

    up

--- application/views/inventory/index_drink.php
+++ application/views/inventory/index_drink.php
@@ -73,182 +73,7 @@
 	<body id="form_div" width: 100%;>
 	<table border="" class="flowtable">
 						
-							<caption>寬仕盤點飲料
-
-							</caption>
-							螺絲I1~113區
-
-
-螺絲H1~Hl3區
-
-
-螺絲Gl~G7區、G15
-
-
-螺絲G8~G14區、J區、不良品區
-
-
-鏈帶組裝區、研發工廠
-
-
-螺絲J區(螺絲成品)
-
-
-
-
-
-
-
-成品C區(C01-C15)
-
-
-
-
-
-
-成品D區(D01-D19)
-
-
-
-
-
-
-
-成品打板區
-
-
-
-BIT. 撞針
-
-
-
-包材棧板區
-
-
-
-
-
-
-
-包材暫存區
-
-
-
-
-包材塑袋區.配件區(X)、包材B區
-
-
-
-
-
-
-包材鐵架區(B區二側)及鐵架華司鍊帶區(Y)
-
-
-
-包材C區
-
-包材E區
-
-
-
-
-
-
-
-
-包材區(Hl-H6)
-
-
-
-
-
-
-
-包材區(H8-Hl5)&包材K區
-
-
-
-
-
-
-
-標籤
-
-尾數螺絲整理(區分留存與否)
-
-堆高機
-
-固定資產(北花廠.寬仕農場.沙崙廠)
-
-固定資產(品保課.測試室.樣品室.品保實驗室.會議室十一)
-
-固定資產(生產部.會議室十二.F. G. H. I區.倉管課.守衛室)
-
-固定資產(員工休息室.包裝部.包裝作業課.標籤組.會議室一)
-
-固定資產(資訊部.資訊部機房.管理部.會議室三.視覺攝影室)
-
-固定資產(現場A區.研發工廠.A區廚房.資料庫房.物料組.成品組)
-
-
-固定資產(研發部.研發測試室.研發樣品室.會議室四.實驗室.會議室七)
-
-固定資產(業務部.業務庫品室.財務部.會議室五)
-
-固定資產(婦幼室.總經理室.實驗餐廳.接特室.運動室.閱覽室)
-
-
-固定資產(子日宿舍)
-
-辦公室(協助接碼電話、飲料餐點分配)
-
-
-全廠廁所清潔
-一般食材、商品
-
-
-
-
-
-
-
-一般物料、包材
-
-
-
-
-
-
-
-固定資產(總公司)
-
-
-
-(五甲)調度
-(五甲)線材&搓牙成品
-
-
-
-
-
-(五甲)模具
-
-
-
-(五甲)牙板
-
-
-
-(五甲)沖棒
-
-(五甲)撞針
-
-(五甲)固定資產
-
-
-
-
+							<caption>寬仕盤點飲料</caption>
 
 							<tr>
 								<th style="width: 80px;"><font color="red" size="4" >上午</font>盤點區域：</th>
@@ -256,11 +81,11 @@ BIT. 撞針
                                     <font color="red" size="4" >上午飲料會分發到各位的盤點區域</font>
 									<select class="" id="area" >
 										<option area_num="0" value="0">請選擇盤點區域</option>
-										<option area_num="1" value="螺絲區I1～I13區、J區(堆高機+大地磅+天車)">螺絲區I1～I13區、J區(堆高機+大地磅+天車)</option>
-										<option area_num="2" value="螺絲區H1～H13(堆高機+大地磅+天車)">螺絲區H1～H13(堆高機+大地磅+天車)</option>
-										<option area_num="3" value="螺絲區G1～G15、不良品區(堆高機+大地磅+天車)">螺絲區G1～G15、不良品區(堆高機+大地磅+天車)</option>
-										<option area_num="4" value="五甲一廠">五甲一廠</option>
-										<option area_num="5" value="鏈袋組裝區、研發工廠(堆高機+大地磅+天車)">鏈袋組裝區、研發工廠(堆高機+大地磅+天車)</option>
+										<option area_num="1" value="螺絲I1~I13區">螺絲I1~I13區</option>
+										<option area_num="2" value="螺絲H1~Hl3區">螺絲H1~Hl3區</option>
+										<option area_num="3" value="螺絲Gl~G7區、G15">螺絲Gl~G7區、G15</option>
+										<option area_num="4" value="螺絲G8~G14區、J區、不良品區">螺絲G8~G14區、J區、不良品區</option>
+										<option area_num="5" value="鏈帶組裝區、研發工廠">鏈帶組裝區、研發工廠</option>
 										<option area_num="6" value="螺絲J區(螺絲成品)">螺絲J區(螺絲成品)</option>
 										<option area_num="7" value="螺絲F區(螺絲成品)">螺絲F區(螺絲成品)</option>
 										<option area_num="8" value="成品C區(C01-C15)">成品C區(C01-C15)</option>
@@ -278,19 +103,28 @@ BIT. 撞針
 										<option area_num="20" value="標籤">標籤</option>
 										<option area_num="21" value="尾數螺絲整理(區分輪流存與否)">尾數螺絲整理(區分輪流存與否)</option>
 										<option area_num="22" value="堆高機">堆高機</option>
-										<option area_num="23" value="固定資產-北花廠、寬仕農場、沙崙廠">固定資產-北花廠、寬仕農場、沙崙廠</option>
-										<option area_num="24" value="固定資產-品保課、測試室、樣品室、品保實驗室、會議室十、F.G.H.I區、倉管課">固定資產-品保課、測試室、樣品室、品保實驗室、會議室十、F.G.H.I區、倉管課</option>
-										<option area_num="25" value="固定資產-B區、B區餐廳、包裝部、會議室六、包裝作業課、標籤組、會議室十二">固定資產-B區、B區餐廳、包裝部、會議室六、包裝作業課、標籤組、會議室十二</option>
-										<option area_num="26" value="固定資產-財務部、會議室五、資訊部、資訊部機房、管理部、會議室三">固定資產-財務部、會議室五、資訊部、資訊部機房、管理部、會議室三</option>
-										<option area_num="27" value="固定資產-A區、研發工廠、A區廚房、資料庫房、領料組">固定資產-A區、研發工廠、A區廚房、資料庫房、領料組</option>
-										<option area_num="28" value="固定資產-研發部、研發測試室、研發樣品室、會議室四、實驗室、會議室七">固定資產-研發部、研發測試室、研發樣品室、會議室四、實驗室、會議室七</option>
-										<option area_num="29" value="固定資產-生產部、會議室一、業務部、業務庫品室、會議室二、會議室九、守衛室">固定資產-生產部、會議室一、業務部、業務庫品室、會議室二、會議室九、守衛室</option>
-										<option area_num="30" value="固定資產-婦幼室、總經理室、實驗餐廳、接待室、運動室、閱覽室、視覺攝影室">固定資產-婦幼室、總經理室、實驗餐廳、接待室、運動室、閱覽室、視覺攝影室</option>
-                                        <option area_num="31" value="五甲調度">五甲調度</option>
-                                        <option area_num="32" value="餐點分配">餐點分配</option>
-                                        <option area_num="33" value="深緣及水本廠-行政課">深緣及水本廠-行政課</option>
-                                        <option area_num="34" value="廁所清潔">廁所清潔</option>
-										<option area_num="35" value="關廟店">關廟店</option>
+										<option area_num="23" value="固定資產(北花廠、寬仕農場、沙崙廠)">固定資產-北花廠、寬仕農場、沙崙廠</option>
+										<option area_num="24" value="固定資產(品保課、測試室、樣品室、品保實驗室、會議室十一)">固定資產-品保課、測試室、樣品室、品保實驗室、會議室十、F.G.H.I區、倉管課</option>
+										<option area_num="25" value="固定資產(生產部.會議室十二.F. G. H. I區.倉管課.守衛室)">固定資產(生產部.會議室十二.F. G. H. I區.倉管課.守衛室)</option>
+										<option area_num="26" value="固定資產(員工休息室.包裝部.包裝作業課.標籤組.會議室一)">固定資產(員工休息室.包裝部.包裝作業課.標籤組.會議室一)</option>
+										<option area_num="27" value="固定資產(資訊部.資訊部機房.管理部.會議室三.視覺攝影室)">固定資產(資訊部.資訊部機房.管理部.會議室三.視覺攝影室)</option>
+										<option area_num="28" value="固定資產(現場A區.研發工廠.A區廚房.資料庫房.物料組.成品組)">固定資產(現場A區.研發工廠.A區廚房.資料庫房.物料組.成品組)</option>
+										<option area_num="29" value="固定資產(研發部、研發測試室、研發樣品室、會議室四、實驗室、會議室七)">固定資產-研發部、研發測試室、研發樣品室、會議室四、實驗室、會議室七</option>
+										<option area_num="30" value="固定資產(業務部.業務庫品室.財務部.會議室五)">固定資產(業務部.業務庫品室.財務部.會議室五)</option>
+										<option area_num="31" value="固定資產(婦幼室.總經理室.實驗餐廳.接特室.運動室.閱覽室)">固定資產(婦幼室.總經理室.實驗餐廳.接特室.運動室.閱覽室)</option>
+										<option area_num="32" value="固定資產(子日宿舍)">固定資產(子日宿舍)</option>
+										<option area_num="33" value="辦公室(協助接碼電話、飲料餐點分配)">辦公室(協助接碼電話、飲料餐點分配)</option>
+										<option area_num="34" value="全廠廁所清潔">全廠廁所清潔</option>
+										<option area_num="35" value="一般食材、商品">一般食材、商品</option>
+										<option area_num="36" value="一般物料、包材">一般物料、包材</option>
+										<option area_num="37" value="固定資產(總公司)">固定資產(總公司)</option>
+										<option area_num="38" value="(五甲)調度">(五甲)調度</option>
+										<option area_num="39" value="(五甲)線材&搓牙成品">(五甲)線材&搓牙成品</option>
+										<option area_num="40" value="(五甲)模具">(五甲)模具</option>
+										<option area_num="41" value="(五甲)牙板">(五甲)牙板</option>
+										<option area_num="42" value="(五甲)沖棒">(五甲)沖棒</option>
+										<option area_num="43" value="(五甲)撞針">(五甲)撞針</option>
+										<option area_num="44" value="(五甲)固定資產">(五甲)固定資產</option>
 									</select>
 									</td>
 							</tr>
