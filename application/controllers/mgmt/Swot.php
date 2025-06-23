ef6b2f1406a9e8f3e47b477ec39830d5b720e2df
pony <as3305191@gmail.com>
2025-06-23 14:04:23 +0800

    up

--- application/controllers/mgmt/Swot.php
+++ application/controllers/mgmt/Swot.php
@@ -194,7 +194,7 @@ class Swot extends MY_Mgmt_Controller {
 
 							// 先替換所有「。</span></p>」加標註
 							$tmp = preg_replace(
-								'/(。</span>)(<\/p>)/u',
+								'~(。</span>)(<\/p>)~u',
 								'。</span>(' . $each->d_or_c_name . ')</p>',
 								$tmp
 							);
