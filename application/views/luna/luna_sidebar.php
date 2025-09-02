959638d9c5339c193508d1ab5c37117a12cf1902
Pony0116 <as3305191@gmail.com>
2025-09-02 21:05:16 +0800

    up

--- application/views/luna/luna_sidebar.php
+++ application/views/luna/luna_sidebar.php
@@ -3,7 +3,7 @@
   <!-- User Image -->
   <div class="u-block-hover g-pos-rel">
     <figure>
-      <img class="img-fluid w-100 u-block-hover__main--zoom-v1" src="<?= base_url('img/luna/luna.jpg') ?>" alt="Image Description">
+      <img class="img-fluid w-100 u-block-hover__main--zoom-v1" src="<?= base_url('img/luna/luna01.png') ?>" alt="Image Description">
     </figure>
   </div>
 
